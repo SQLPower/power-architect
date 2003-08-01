104f88bd0a8acc25474d9e5c54e7cf0dbc22d9a6
fuerth <fuerth@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2003-08-01 21:33:21 +0000

    Updated to custom DnD support, which theoretiaclly works on java 1.2 and up.  I've only seen it work on 1.4.1 for windows (hangs after drop on 1.3.1 netbsd)

--- trunk/src/ca/sqlpower/architect/swingui/DBTree.java
+++ trunk/src/ca/sqlpower/architect/swingui/DBTree.java
@@ -3,41 +3,55 @@ package ca.sqlpower.architect.swingui;
 import javax.swing.*;
 import javax.swing.tree.*;
 import java.awt.datatransfer.*;
+import java.awt.dnd.*;
 
 import ca.sqlpower.architect.*;
 
-public class DBTree extends JTree {
+public class DBTree extends JTree implements DragSourceListener {
+
+	protected DragSource ds;
 
 	public DBTree(SQLDatabase root) throws ArchitectException {
 		super(new DBTreeModel(root));
-		setTransferHandler(new DBTreeTransferHandler());
-		setDragEnabled(true);
+		ds = new DragSource();
+		DragGestureRecognizer dgr = ds.createDefaultDragGestureRecognizer
+			(this, DnDConstants.ACTION_COPY, new DBTreeDragGestureListener());
+	}
+
+	// ---------- methods of DragSourceListener -----------
+	public void dragEnter(DragSourceDragEvent dsde) {
+		System.out.println("DBTree: got dragEnter event");
+	}
+
+	public void dragOver(DragSourceDragEvent dsde) {
+		System.out.println("DBTree: got dragOver event");
+	}
+	
+	public void dropActionChanged(DragSourceDragEvent dsde) {
+		System.out.println("DBTree: got dropActionChanged event");
+	}
+
+	public void dragExit(DragSourceEvent dse) {
+		System.out.println("DBTree: got dragExit event");
+	}
+	
+	public void dragDropEnd(DragSourceDropEvent dsde) {
+		System.out.println("DBTree: got dragDropEnd event");
 	}
 
 	/**
-	 * Exports the SQLObject under the pointer in a DBTree.  If the
-	 * tree contains non-SQLObject nodes, you'll get
-	 * ClassCastExceptions.
+	 * Exports the SQLObject which was under the pointer in a DBTree
+	 * when the drag gesture started.  If the tree contains
+	 * non-SQLObject nodes, you'll get ClassCastExceptions.
 	 */
-	public class DBTreeTransferHandler extends TransferHandler {
-		protected Transferable createTransferable(JComponent c) {
-			TreePath p = ((JTree) c).getSelectionPath();
-			SQLObject data = (SQLObject) p.getLastPathComponent();
-			return new SQLObjectTransferable(data);
-		}
-    
-		public int getSourceActions(JComponent c) {
-			return COPY;
-		}
-
-		public boolean importData(JComponent c, Transferable t) {
-			// TODO: import SQLObjects
-			return false;
-		}
-        
-		public boolean canImport(JComponent c, DataFlavor[] flavors) {
-			// TODO: import SQLObjects
-			return false;
-		} 
+ 	public static class DBTreeDragGestureListener implements DragGestureListener {
+		public void dragGestureRecognized(DragGestureEvent dge) {
+			System.out.println("Drag gesture event: "+dge);
+			DBTree t = (DBTree) dge.getComponent();
+  			TreePath p = t.getSelectionPath();
+  			SQLObject data = (SQLObject) p.getLastPathComponent();
+  			dge.getDragSource().startDrag
+				(dge, DragSource.DefaultCopyDrop, new SQLObjectTransferable(data), t);
+ 		}
 	}
 }
