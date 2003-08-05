a6eb57ba663357da476d2b57db9e8dbbb9445f35
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2003-08-05 22:09:59 +0000

    Changed the way tables are loaded: The SQLDatabase object now calls a static method SQLTable.  This method builds all necessary catalog and schema objects and populates them with the correct children. It also links them to their parents.
    In this way, all accessible table, catalog, and schema names are now
    retrieved in one JDBC call. Actual column names are still loaded lazily.

--- src/ca/sqlpower/architect/SQLSchema.java
+++ src/ca/sqlpower/architect/SQLSchema.java
@@ -4,7 +4,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.LinkedList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Collections;
 
@@ -17,87 +16,13 @@ public class SQLSchema extends SQLObject {
 	protected SQLObject parent;
 	protected String schemaName;
 
-	protected boolean populated;
-
 	public SQLSchema(SQLObject parent, String name) {
 		this.parent = parent;
 		this.schemaName = name;
 		this.children = new LinkedList();
-		this.populated = false;
 	}
-
-	/**
-	 * This one is a bit funny, so read the documentation.
-	 *
-	 * <p>Some RDBMSes have a Database-Catalog-Schema-(Table|View|...) 
-	 * hierarchy ("type 1", SQLServer), and others have
-	 * Database-Catalog-(Table|View|...) ("type 2", Access).  Still
-	 * others have Database-Schema-(Table|View|...) ("type 3",
-	 * Oracle). This method populates the catalogs and schemas for
-	 * types 1 and 3.
-	 *
-	 * <p>For type 1 databases, a new set of SQLCatalog objects will
-	 * be created.  The schemas will be added to the various catalogs,
-	 * and then the catalogs will be added to the database.
-	 *
-	 * <p>For type 3 databases, all schemas will be added directly to
-	 * the database.
-	 */
-	protected static boolean addSchemasToDatabase(SQLDatabase addTo)
-		throws SQLException, ArchitectException {
-
-		boolean addedSome = false;
-		HashMap catalogs = new HashMap();
-		ResultSet rs = null;
-		try {
-			rs = addTo.getConnection().getMetaData().getSchemas();
-			while (rs.next()) {
-				String schName = rs.getString(1);
-				String catName = "fake"; //rs.getString(2);
-				if (catName != null) {
-					SQLCatalog cat = (SQLCatalog) catalogs.get(catName);
-					if (cat == null) {
-						cat = new SQLCatalog(addTo, catName);
-						addTo.children.add(cat);
-						catalogs.put(catName, cat);
-					}
-					cat.children.add(new SQLSchema(cat, schName));
-					addedSome = true;
-				} else {
-					// for catalogless RDBMS (type 3)
-					addTo.children.add(new SQLSchema(addTo, schName));
-					addedSome = true;
-				}
-			}
-		} finally {
-			if (rs != null) rs.close();
-		}
-		return addedSome;
-	}
-
-	protected synchronized void populateTables() throws ArchitectException {
-		if (populated) return;
-		int oldSize = children.size();
-		try {
-			SQLTable.addTablesToSchema(this);
-		} catch (SQLException e) {
-			throw new ArchitectException("database.populate.fail", e);
-		} finally {
-			populated = true;
-			int newSize = children.size();
-			if (newSize > oldSize) {
-				int[] changedIndices = new int[newSize - oldSize];
-				for (int i = 0, n = newSize - oldSize; i < n; i++) {
-					changedIndices[i] = oldSize + i;
-				}
-				fireDbChildrenInserted(changedIndices, children.subList(oldSize, newSize));
-			}
-		}
-	}
-	
 
 	protected SQLTable getTableByName(String tableName) throws ArchitectException {
-		if (!populated) populate();
 		Iterator childit = children.iterator();
 		while (childit.hasNext()) {
 			SQLTable child = (SQLTable) childit.next();
@@ -126,12 +51,14 @@ public class SQLSchema extends SQLObject {
 		return true;
 	}
 
-	public void populate() throws ArchitectException {
-		populateTables();
+	/**
+	 * does nothing because schemas are pre-populated elsewhere.
+	 */
+	public void populate() {
 	}
 
 	public boolean isPopulated() {
-		return populated;
+		return true;
 	}
 
 	// ----------------- accessors and mutators -------------------
@@ -172,12 +99,4 @@ public class SQLSchema extends SQLObject {
 	protected void setChildren(List argChildren) {
 		this.children = argChildren;
 	}
-
-	/**
-	 * Don't use.
-	 */
-	private void setPopulated(boolean argPopulated) {
-		throw new UnsupportedOperationException("You can't do that");
-	}
-
 }
