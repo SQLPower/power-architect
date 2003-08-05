d29a25a6f44838fd1dc2b5e131dd1b14f535179b
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2003-08-05 18:18:06 +0000

    -Modified layout manager to avoid overlapping TablePanes -Added support for multi-table DnD import -removed green background

--- src/ca/sqlpower/architect/swingui/PlayPen.java
+++ src/ca/sqlpower/architect/swingui/PlayPen.java
@@ -5,11 +5,16 @@ import java.awt.*;
 import java.awt.datatransfer.*;
 import java.awt.dnd.*;
 import java.io.IOException;
+
 import java.util.Arrays;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Iterator;
 
 import ca.sqlpower.architect.*;
 
-public class PlayPen extends JPanel {
+public class PlayPen extends JPanel implements java.io.Serializable {
 
 	/**
 	 * Links this PlayPen with an instance of PlayPenDropListener so
@@ -18,21 +23,58 @@ public class PlayPen extends JPanel {
 	protected DropTarget dt;
 	
 	public PlayPen() {
-		super(new PlayPenLayout());
+		super();
+		setLayout(new PlayPenLayout(this));
 		setName("Play Pen");
 		setMinimumSize(new Dimension(200,200));
 		setOpaque(true);
-		setBackground(Color.green);
 		dt = new DropTarget(this, new PlayPenDropListener());
 	}
 	
-	public static class PlayPenLayout implements LayoutManager {
+	public static class PlayPenLayout implements LayoutManager2 {
 
 		/**
-		 * Does nothing.
+		 * This is the PlayPen that we are managing the layout for.
+		 */
+		PlayPen parent;
+
+		public PlayPenLayout(PlayPen parent) {
+			this.parent = parent;
+		}
+
+		/**
+		 * Does nothing.  Use the Object-style constraints, not String.
 		 */
 		public void addLayoutComponent(String name, Component comp) {
-			System.out.println("PlayPenLayout.addLayoutComponent");
+			System.out.println("PlayPenLayout.addLayoutComponent(String,Component)");
+		}
+
+		/**
+		 * Positions the new component near the given point.
+		 *
+		 * @param comp the component which has been added
+		 * @param position A java.awt.Point, near which the object
+		 * should be positioned.  It will not overlap existing
+		 * components in this play pen.
+		 */
+		public void addLayoutComponent(Component comp,
+									   Object position) {
+			System.out.println("PlayPenLayout.addLayoutComponent(Component,Object)");
+			Point pos = (Point) position;
+			comp.setSize(comp.getPreferredSize());
+
+			RangeList rl = new RangeList();
+			Rectangle cbounds = null;
+			for (int i = 0, n = parent.getComponentCount(); i < n; i++) {
+				Component c = parent.getComponent(i);
+				if (c.isVisible() && c != comp) {
+					cbounds = c.getBounds(cbounds);
+					rl.blockOut(cbounds.x, cbounds.width);
+				}
+			}
+			
+			pos.x = rl.findGapToRight(pos.x, comp.getWidth());
+			comp.setLocation(pos);
 		}
 
 		/**
@@ -61,7 +103,10 @@ public class PlayPen extends JPanel {
 					maxy = Math.max(cbounds.y + cbounds.height, maxy);
 				}
 			}
-			return new Dimension(maxx - minx, maxy - miny);
+
+			Dimension min = parent.getMinimumSize();
+			return new Dimension(Math.max(maxx - minx, min.width),
+								 Math.max(maxy - miny, min.height));
 		}
 
 		/**
@@ -73,10 +118,87 @@ public class PlayPen extends JPanel {
 		}
 
 		/**
+		 * Identical to {@link #preferredLayoutSize(Container)}.
+		 */
+		public Dimension maximumLayoutSize(Container target) {
+			return preferredLayoutSize(target);
+		}
+
+		public float getLayoutAlignmentX(Container target) {
+			return 0.5f;
+		}
+
+		public float getLayoutAlignmentY(Container target) {
+			return 0.5f;
+		}
+
+		/**
+		 * Discards cached layout information.  Currently this is a no-op.
+		 */
+		public void invalidateLayout(Container target) {
+			return;
+		}
+
+		/**
 		 * Does nothing!  Components will stay put.
 		 */
 		public void layoutContainer(Container parent) {
 			System.out.println("PlayPenLayout.layoutContainer");
+		}
+
+		protected static class RangeList {
+
+			List blocks;
+
+			public RangeList() {
+				blocks = new LinkedList();
+
+				// we need sentinel values because blockOut() requires non-empty list
+				blocks.add(new Block(Integer.MAX_VALUE, 0));
+			}
+
+			public void blockOut(int start, int length) {
+				Block block = new Block(start, length);
+				System.out.println("blockOut "+block+": before "+blocks);
+				ListIterator it = blocks.listIterator();
+				while (it.hasNext()) {
+					Block nextBlock = (Block) it.next();
+					if (nextBlock.start > start) {
+						it.previous();
+						it.add(block);
+						break;
+					}
+				}
+				System.out.println("blockOut "+block+": after  "+blocks);
+			}
+
+			public int findGapToRight(int start, int length) {
+				Iterator it = blocks.iterator();
+				while (it.hasNext()) {
+					Block block = (Block) it.next();
+
+					if ( (start + length) < block.start ) {
+						// current gap fits at right-hand side.. done!
+						return start;
+					} else {
+						start = block.start + block.length;
+					}
+
+				}
+				return start;
+			}
+
+			protected static class Block {
+				public int start;
+				public int length;
+				public Block(int start, int length) {
+					this.start = start;
+					this.length = length;
+				}
+				public String toString() {
+					return "("+start+","+length+")";
+				}
+			}
 		}
 	}
 	
@@ -85,14 +207,14 @@ public class PlayPen extends JPanel {
 	 * at the current mouse position.
 	 */
 	public static class PlayPenDropListener implements DropTargetListener {
-		public Point currentLocation = new Point(0,0);
-		
+
 		/**
 		 * Called while a drag operation is ongoing, when the mouse
 		 * pointer enters the operable part of the drop site for the
 		 * DropTarget registered with this listener.
 		 */
 		public void dragEnter(DropTargetDragEvent dtde) {
+			dragOver(dtde);
 		}
 		
 		/**
@@ -109,7 +231,8 @@ public class PlayPen extends JPanel {
 		 * the DropTarget registered with this listener.
 		 */
 		public void dragOver(DropTargetDragEvent dtde) {
-			currentLocation.setLocation(dtde.getLocation());
+			System.out.println("PlayPenDropTarget.dragOver()");
+			dtde.acceptDrag(DnDConstants.ACTION_COPY);
 		}
 		
 		/**
@@ -128,26 +251,38 @@ public class PlayPen extends JPanel {
 					Object someData = t.getTransferData(importFlavor);
 					System.out.println("MyJTreeTransferHandler.importData: got object of type "+someData.getClass().getName());
 					if (someData instanceof SQLTable) {
+						dtde.acceptDrop(DnDConstants.ACTION_COPY);
 						SQLTable table = (SQLTable) someData;
 						TablePane tp = new TablePane(table);
-						c.add(tp, null);
-						tp.setLocation(currentLocation);
-						System.out.println("Added "+table+" to playpen at "+tp.getLocation());
+						c.add(tp, dtde.getLocation());
+
 						tp.revalidate();
-						System.out.println("revalidated tablepane");
 						dtde.dropComplete(true);
-						System.out.println("signalled drop complete");
 						return;
 					} else if (someData instanceof SQLColumn) {
+						dtde.acceptDrop(DnDConstants.ACTION_COPY);
 						SQLColumn column = (SQLColumn) someData;
 						JLabel colName = new JLabel(column.getColumnName());
-						c.add(colName);
-						colName.setLocation(30,30);
 						colName.setSize(colName.getPreferredSize());
+						c.add(colName, dtde.getLocation());
 						System.out.println("Added "+column.getColumnName()+" to playpen (temporary, only for testing)");
 						colName.revalidate();
 						dtde.dropComplete(true);
 						return;
+					} else if (someData instanceof SQLObject[]) {
+						dtde.acceptDrop(DnDConstants.ACTION_COPY);
+						SQLObject[] objects = (SQLObject[]) someData;
+						for (int i = 0; i < objects.length; i++) {
+							if (objects[i] instanceof SQLTable) {
+								TablePane tp = new TablePane((SQLTable) objects[i]);
+								c.add(tp, dtde.getLocation());
+								tp.revalidate();
+							}
+						}
+						dtde.dropComplete(true);
+						return;
+					} else {
+						dtde.rejectDrop();
 					}
 				} catch (UnsupportedFlavorException ufe) {
 					ufe.printStackTrace();
@@ -191,7 +326,8 @@ public class PlayPen extends JPanel {
 				System.out.println("isLocalObject = "+flavors[i].getMimeType().equals(DataFlavor.javaJVMLocalObjectMimeType));
 
 
- 				if (flavors[i].equals(SQLObjectTransferable.flavor)) {
+ 				if (flavors[i].equals(SQLObjectTransferable.flavor)
+					|| flavors[i].equals(SQLObjectListTransferable.flavor)) {
 					System.out.println("YES");
  					return flavors[i];
 				}
