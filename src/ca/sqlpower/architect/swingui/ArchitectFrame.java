08458bcc51ccad1f7ad14e652824df1d960deccd
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2004-01-28 21:36:34 +0000

    First implementation of the actual main architect frame, including user settings save/restore using Commons Digester.

---
+++ src/ca/sqlpower/architect/swingui/ArchitectFrame.java
@@ -0,0 +1,98 @@
+package ca.sqlpower.architect.swingui;
+
+import ca.sqlpower.architect.*;
+
+import org.apache.log4j.Logger;
+
+import javax.swing.*;
+import java.awt.Rectangle;
+import java.awt.Container;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class ArchitectFrame extends JFrame {
+
+	private static Logger logger = Logger.getLogger(ArchitectFrame.class);
+
+	protected ConfigFile configFile = null;
+	protected UserSettings prefs = null;
+	protected SwingUserSettings sprefs = null;
+	protected JToolBar toolBar = null;
+	protected JMenuBar menuBar = null;
+	protected JSplitPane splitPane = null;
+	protected JPanel playpen = null;
+	protected JTree dbTree = null;
+	
+	/**
+	 * Updates the swing settings and then writes all settings to the
+	 * config file whenever actionPerformed is invoked.
+	 */
+	protected Action saveSettingsAction = new AbstractAction("Save Settings") {
+			public void actionPerformed(ActionEvent e) {
+				if (configFile == null) configFile = ConfigFile.getDefaultInstance();
+				try {
+					sprefs.setInt(SwingUserSettings.DIVIDER_LOCATION, splitPane.getDividerLocation());
+					sprefs.setInt(SwingUserSettings.MAIN_FRAME_X, getLocation().x);
+					sprefs.setInt(SwingUserSettings.MAIN_FRAME_Y, getLocation().y);
+					sprefs.setInt(SwingUserSettings.MAIN_FRAME_WIDTH, getWidth());
+					sprefs.setInt(SwingUserSettings.MAIN_FRAME_HEIGHT, getHeight());
+
+					configFile.write(prefs);
+				} catch (ArchitectException ex) {
+					logger.error("Couldn't save settings", ex);
+				}
+			}
+		};
+
+	public ArchitectFrame() throws ArchitectException {
+		try {
+			ConfigFile cf = ConfigFile.getDefaultInstance();
+			prefs = cf.read();
+			sprefs = prefs.getSwingSettings();
+		} catch (IOException e) {
+			throw new ArchitectException("prefs.read", e);
+		}
+
+		Container cp = getContentPane();
+		cp.setLayout(new BorderLayout());
+
+		toolBar = new JToolBar();
+		toolBar.add(new JButton(saveSettingsAction));
+		cp.add(toolBar, BorderLayout.NORTH);
+
+		menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.add(new JMenuItem(saveSettingsAction));
+		menuBar.add(fileMenu);
+		setJMenuBar(menuBar);
+
+		playpen = new PlayPen(SQLDatabase.getPlayPenInstance());
+		dbTree = new DBTree(prefs.getConnections());
+		((SQLObject) dbTree.getModel().getRoot()).addChild(SQLDatabase.getPlayPenInstance());
+		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+								   new JScrollPane(dbTree),
+								   new JScrollPane(playpen));
+		cp.add(splitPane, BorderLayout.CENTER);
+		splitPane.setDividerLocation
+			(sprefs.getInt(SwingUserSettings.DIVIDER_LOCATION,
+						   dbTree.getPreferredSize().width));
+		Rectangle bounds = new Rectangle();
+		bounds.x = sprefs.getInt(SwingUserSettings.MAIN_FRAME_X, 40);
+		bounds.y = sprefs.getInt(SwingUserSettings.MAIN_FRAME_Y, 40);
+		bounds.width = sprefs.getInt(SwingUserSettings.MAIN_FRAME_WIDTH, 600);
+		bounds.height = sprefs.getInt(SwingUserSettings.MAIN_FRAME_HEIGHT, 440);
+		setBounds(bounds);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	/**
+	 * Creates an ArchitectFrame and sets is visible.  This method is
+	 * an acceptable way to launch the Architect application.
+	 */
+	public static void main(String args[]) throws Exception {
+		ArchitectFrame frame = new ArchitectFrame();
+		frame.setVisible(true);
+	}
+	
+}
---
+++ src/ca/sqlpower/architect/swingui/SwingUserSettings.java
@@ -0,0 +1,110 @@
+package ca.sqlpower.architect.swingui;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+import org.apache.log4j.Logger;
+
+public class SwingUserSettings {
+
+	private static Logger logger = Logger.getLogger(SwingUserSettings.class);
+
+	public static final String DIVIDER_LOCATION 
+		= "ca.sqlpower.architect.swing.SwingUserSettings.DIVIDER_LOCATION";
+
+	public static final String MAIN_FRAME_X
+		= "ca.sqlpower.architect.swing.SwingUserSettings.MAIN_FRAME_X";
+
+	public static final String MAIN_FRAME_Y
+		= "ca.sqlpower.architect.swing.SwingUserSettings.MAIN_FRAME_Y";
+
+	public static final String MAIN_FRAME_WIDTH
+		= "ca.sqlpower.architect.swing.SwingUserSettings.MAIN_FRAME_WIDTH";
+
+	public static final String MAIN_FRAME_HEIGHT
+		= "ca.sqlpower.architect.swing.SwingUserSettings.MAIN_FRAME_HEIGHT";
+
+	protected Map settings;
+
+	public SwingUserSettings() {
+		super();
+		settings = new HashMap();
+	}
+
+	/**
+	 * Gets the named property from the settings map.  If the value in
+	 * the map is a Number, the value is obtained by calling
+	 * <code>intValue()</code> on it.  If it is a String, it is
+	 * converted with <code>Integer.parseInt()</code>. Otherwise, the
+	 * default value is returned a warning is logged using
+	 * <code>logger</code>.  If there is no such value in the map,
+	 * the default is returned without logging a warning.
+	 */
+	public int getInt(String propName, int defaultValue) {
+		Object result = settings.get(propName);
+		if (result == null) {
+			return defaultValue;
+		} else if (result instanceof Number) {
+			return ((Number) result).intValue();
+		} else if (result instanceof String) {
+			try {
+				return Integer.parseInt((String) result);
+			} catch (NumberFormatException e) {
+				logger.warn("Couldn't parse value '"+result
+							+"' as integer for property '"+propName+"'");
+			}
+		}			
+		logger.warn("Using default value for integer property '"+propName+"' because of unknown data type for existing value");
+		return defaultValue;
+	}
+
+	public void setInt(String propName, int value) {
+		settings.put(propName, new Integer(value));
+	}
+
+	public void setObject(String propName, Object value) {
+		settings.put(propName, value);
+	}
+
+	public void putSetting(String propName, String propClassName, String propValue) {
+		Object prop = null;
+		if (propClassName.equals("java.lang.Integer")) {
+			try {
+				prop = new Integer(propValue);
+			} catch (NumberFormatException e) {
+				logger.warn("Invalid integer setting "+propName+"="+propValue);
+				return;
+			}
+		} else {
+			logger.warn("Unknown property class "+propClassName);
+			return;
+		}
+		settings.put(propName, prop);
+	}
+
+	public String getString(String propName, String defaultValue) {
+		Object result = settings.get(propName);
+		if (result == null) {
+			return defaultValue;
+		} else {
+			return result.toString();
+		}	
+	}
+
+	public Object getObject(String propName, Object defaultValue) {
+		Object result = settings.get(propName);
+		if (result == null) {
+			return defaultValue;
+		} else {
+			return result;
+		}	
+	}
+
+	/**
+	 * Returns the names of all settings currently held by this
+	 * SwingUserSettings object.  They will all be Strings.
+	 */
+	public Set getSettingNames() {
+		return settings.keySet();
+	}
+}
