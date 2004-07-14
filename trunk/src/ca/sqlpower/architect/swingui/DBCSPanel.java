80c14439facec90a7ac641b27706217e487280d4
fuerth <fuerth@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2004-07-14 18:48:35 +0000

    Removed the history list from the DBCSPanel and implemented history in the DBTree popup menu instead.

--- trunk/src/ca/sqlpower/architect/swingui/DBCSPanel.java
+++ trunk/src/ca/sqlpower/architect/swingui/DBCSPanel.java
@@ -20,8 +20,6 @@ public class DBCSPanel extends JPanel implements ArchitectPanel {
 	protected DBConnectionSpec dbcs;
 	protected TextPanel form;
 
-	protected Vector history;
-	protected JComboBox historyBox;
 	protected JTextField dbNameField;
 	protected String dbNameTemp;
 	protected JComboBox dbDriverField;
@@ -35,36 +33,27 @@ public class DBCSPanel extends JPanel implements ArchitectPanel {
 		setLayout(new BorderLayout());
 		ArchitectFrame af = ArchitectFrame.getMainInstance();
 
-		List connectionHistory = af.getUserSettings().getConnections();
-		history = new Vector();
-		history.add(ASUtils.lvb("(New Connection)", null));
-		Iterator it = connectionHistory.iterator();
-		while (it.hasNext()) {
-			DBConnectionSpec spec = (DBConnectionSpec) it.next();
-			history.add(ASUtils.lvb(spec.getDisplayName(), spec));
-		}
-		historyBox = new JComboBox(history);
-		historyBox.addActionListener(new HistoryBoxListener());
-
 		dbDriverField = new JComboBox(getDriverClasses());
 		dbDriverField.insertItemAt("", 0);
 		dbNameField = new JTextField();
-		JComponent[] fields = new JComponent[] {historyBox,
-												dbNameField,
+		JComponent[] fields = new JComponent[] {dbNameField,
 												dbDriverField,
 												dbUrlField = new JTextField(),
 												dbUserField = new JTextField(),
 												dbPassField = new JPasswordField()};
-		String[] labels = new String[] {"History",
-										"Connection Name",
+		String[] labels = new String[] {"Connection Name",
 										"JDBC Driver",
 										"JDBC URL",
 										"Username",
 										"Password"};
 
-		char[] mnemonics = new char[] {'h', 'n', 'd', 'u', 'r', 'p'};
-		int[] widths = new int[] {30, 30, 30, 40, 20, 20};
-		String[] tips = new String[] {"A list of connections you have made in the past", "The name of this database", "The class name of the JDBC Driver", "Vendor-specific JDBC URL", "Username for this database", "Password for this database"};
+		char[] mnemonics = new char[] {'n', 'd', 'u', 'r', 'p'};
+		int[] widths = new int[] {30, 30, 40, 20, 20};
+		String[] tips = new String[] {"Your nickname for this database",
+									  "The class name of the JDBC Driver",
+									  "Vendor-specific JDBC URL",
+									  "Username for this database",
+									  "Password for this database"};
 		
 		// update url field when user picks new driver
 		dbDriverField.addActionListener(new ActionListener() {
@@ -94,42 +83,16 @@ public class DBCSPanel extends JPanel implements ArchitectPanel {
 	}
 	
 	private void setupDriverMap() {
-		jdbcDrivers = new HashMap();
-		jdbcDrivers.put("oracle.jdbc.driver.OracleDriver",
-						"jdbc:oracle:thin:@<hostname>:1521:<instance>");
-		jdbcDrivers.put("com.microsoft.jdbc.sqlserver.SQLServerDriver",
-						"jdbc:microsoft:sqlserver://<hostname>:1433;SelectMethod=cursor");
-		jdbcDrivers.put("org.postgresql.Driver",
-						"jdbc:postgresql://<hostname>:5432");
-		jdbcDrivers.put("ibm.sql.DB2Driver",
-						"jdbc:db2:<hostname>");
-	}
-
-	public class HistoryBoxListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			ASUtils.LabelValueBean lvb = (ASUtils.LabelValueBean) historyBox.getSelectedItem();
-			if (lvb.getValue() != null) {
-				setDbcs((DBConnectionSpec) lvb.getValue());
-			}
-		}
-	}
-
-	/**
-	 * Returns the index of findMe in the DBCS history vector.  0
-	 * means not found because 0 is the reserved index for new
-	 * connections not in the list.
-	 */
-	public int findHistoryConnection(DBConnectionSpec findMe) {
-		int i = 0;
-		Iterator it = history.iterator();
-		while (it.hasNext()) {
-			ASUtils.LabelValueBean lvb = (ASUtils.LabelValueBean) it.next();
-			if (lvb.getValue() == findMe) {
-				return i;
-			}
-			i++;
-		}
-		return 0;
+		Map drivers = new HashMap();
+		drivers.put("oracle.jdbc.driver.OracleDriver",
+					"jdbc:oracle:thin:@<hostname>:1521:<instance>");
+		drivers.put("com.microsoft.jdbc.sqlserver.SQLServerDriver",
+					"jdbc:microsoft:sqlserver://<hostname>:1433;SelectMethod=cursor");
+		drivers.put("org.postgresql.Driver",
+					"jdbc:postgresql://<hostname>:5432");
+		drivers.put("ibm.sql.DB2Driver",
+					"jdbc:db2:<hostname>");
+		jdbcDrivers = drivers;
 	}
 
 	// -------------------- ARCHITECT PANEL INTERFACE -----------------------
@@ -147,10 +110,6 @@ public class DBCSPanel extends JPanel implements ArchitectPanel {
 		dbcs.setUrl(dbUrlField.getText());
 		dbcs.setUser(dbUserField.getText());
 		dbcs.setPass(new String(dbPassField.getPassword())); // completely defeats the purpose for JPasswordField.getText() being deprecated, but we're saving passwords to the config file so it hardly matters.
-		if (historyBox.getSelectedIndex() == 0) {
-			ArchitectFrame.getMainInstance().getUserSettings().getConnections().add(dbcs);
-			history.add(ASUtils.lvb(name, dbcs));
-		}
 	}
 
 	public void applyChanges(DBConnectionSpec dbcs ) {
@@ -161,7 +120,6 @@ public class DBCSPanel extends JPanel implements ArchitectPanel {
 		dbcs.setUrl(dbUrlField.getText());
 		dbcs.setUser(dbUserField.getText());
 		dbcs.setPass(new String(dbPassField.getPassword())); // completely defeats the purpose for JPasswordField.getText() being deprecated, but we're saving passwords to the config file so it hardly matters.
-		
 	}
 	
 	
@@ -179,7 +137,6 @@ public class DBCSPanel extends JPanel implements ArchitectPanel {
 	 * when the applyChanges() method is called.
 	 */
 	public void setDbcs(DBConnectionSpec dbcs) {
-		historyBox.setSelectedIndex(findHistoryConnection(dbcs));
 		dbNameField.setText(dbcs.getName());
 		dbDriverField.removeItemAt(0);
 		if (dbcs.getDriverClass() != null) {
