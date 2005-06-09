9ee5a301196c0772a28ed0a64accb9e6d6ebfdee
fuerth <fuerth@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2005-06-09 16:12:41 +0000

    Turned on antialiasing in playpen

--- trunk/src/ca/sqlpower/architect/swingui/PlayPen.java
+++ trunk/src/ca/sqlpower/architect/swingui/PlayPen.java
@@ -427,6 +427,7 @@ public class PlayPen extends JPanel
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(getBackground());
 		g2.fillRect(0, 0, getWidth(), getHeight());
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 		if (logger.isDebugEnabled()) {
 			Rectangle clip = g2.getClipBounds();
