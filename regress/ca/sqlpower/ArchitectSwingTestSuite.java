8a5531f1eb151dbaa5442da6cce05b9b8264eb7e
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-01-30 20:01:17 +0000

    SwingUIProject: split the I/O portion of save() into its own method for testing; pass PrintWriter to all the methods it calls. Add Test case for it that tests load, save, and XML validity of saved files; add this to the ArchitectSwingTestSuite.

--- regress/ca/sqlpower/ArchitectSwingTestSuite.java
+++ regress/ca/sqlpower/ArchitectSwingTestSuite.java
@@ -8,6 +8,7 @@ import regress.ca.sqlpower.architect.swingui.TestArchitectFrame;
 import regress.ca.sqlpower.architect.swingui.TestAutoLayoutAction;
 import regress.ca.sqlpower.architect.swingui.TestColumnEditPanel;
 import regress.ca.sqlpower.architect.swingui.TestPlayPen;
+import regress.ca.sqlpower.architect.swingui.TestSwingUIProject;
 import regress.ca.sqlpower.architect.swingui.TestUndoManager;
 
 public class ArchitectSwingTestSuite extends TestCase {
@@ -15,7 +16,8 @@ public class ArchitectSwingTestSuite extends TestCase {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for Architect's Swing GUI");
 		//$JUnit-BEGIN$
-		suite.addTestSuite(SaveLoadTest.class);
+		suite.addTestSuite(SaveLoadTest.class);			// could be merged into TestSwingUIProject
+		suite.addTestSuite(TestSwingUIProject.class);
 		suite.addTestSuite(TestArchitectFrame.class);
 		suite.addTestSuite(TestAutoLayoutAction.class);
 		suite.addTestSuite(TestPlayPen.class);
