80fb7c5333dc1fa7bcb9103b32a90236d5932618
matthew <matthew@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-02-18 00:02:18 +0000

    progress on compare DM

--- trunk/src/ca/sqlpower/architect/swingui/CompareDMFrame.java
+++ trunk/src/ca/sqlpower/architect/swingui/CompareDMFrame.java
@@ -2,20 +2,27 @@ package ca.sqlpower.architect.swingui;
 
 import java.awt.BorderLayout;
 import java.awt.Toolkit;
+import java.awt.Window;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
+import java.sql.SQLData;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 import javax.swing.text.AbstractDocument;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
 
 import org.apache.log4j.Logger;
 
+import ca.sqlpower.architect.SQLDatabase;
 import ca.sqlpower.architect.SQLObject;
 
 import com.jgoodies.forms.builder.ButtonBarBuilder;
@@ -25,6 +32,7 @@ public class CompareDMFrame extends JFrame{
 	private static Logger logger = Logger.getLogger(CompareDMFrame.class);
 	private AbstractDocument outputText;
 	private String sourceName;
+	private JPanel panel;
 	private SQLObject source;
 	private String targetName;
 	private String outputType;
@@ -37,18 +45,21 @@ public class CompareDMFrame extends JFrame{
 	{
 		super();
 		
+		
 		this.outputText = outputDocument;
-		outputArea = new JTextArea(outputText);
+		outputArea = new JTextArea("test");	
 		outputArea.setEditable(false);
 		this.source = source;
 		this.sourceName = source.getName();
 		this.targetName = targetName;
 		this.outputType = outputType;
+		mainFrame();
 	}
 	
-	public void mainFrame(){
-				
-		setLayout (new BorderLayout());
+	public void mainFrame() {
+		
+		panel = new JPanel();
+		panel.setLayout (new BorderLayout());
 		builder = new ButtonBarBuilder();	
 		Action copy = new CopyAction(outputText);
 		Action execute = new AbstractAction(){
@@ -68,13 +79,17 @@ public class CompareDMFrame extends JFrame{
 								
 		title = new JLabel("Comparing " + sourceName + " to " +  
 				targetName + " using " + outputType );
-		add(title, BorderLayout.NORTH);
-		add(outputArea,BorderLayout.CENTER);
-		builder.add (new JButton(copy));
+		panel.add(title, BorderLayout.NORTH);
+		JScrollPane sp = new JScrollPane();
+		sp.add(outputArea);
+		panel.add(sp,BorderLayout.CENTER);
+/*		builder.add (new JButton(copy));		
 		builder.add (new JButton(execute));
 		builder.add (new JButton(save));
-		builder.add (new JButton (close));	
-		add(builder.getPanel(),BorderLayout.SOUTH);
+		builder.add (new JButton (close));*/	
+		panel.add(builder.getPanel(),BorderLayout.SOUTH);
+		add(panel);
+		pack();
 	}
 	
 	public class CopyAction extends AbstractAction{
@@ -107,4 +122,31 @@ public class CompareDMFrame extends JFrame{
 			localframe.setVisible(false);
 		}						
 	}
+	
+	/**
+	 * Just for testing the form layout without running the whole Architect.
+	 * 
+	 * <p>The frame it makes is EXIT_ON_CLOSE, so you should never use this
+	 * in a real app.
+	 */
+	public static void main(String[] args) {
+		final JFrame f = new JFrame("Testing compare dm panel");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				f.add(new CompareDMFrame( new DefaultStyledDocument(), new SQLDatabase(), "target", "SQL" ).getPanel());
+				f.pack();
+				f.setVisible(true);
+			};
+		});
+	}
+
+	public JPanel getPanel() {
+		return panel;
+	}
+
+	public void setPanel(JPanel panel) {
+		this.panel = panel;
+	}
+	
 }
