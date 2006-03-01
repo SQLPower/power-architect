33bacd31bc5650cfda5156aa5bdef49a738cc63d
benjamin <benjamin@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-03-01 19:17:55 +0000

    Simplified comparison code and made it pass the unit tests

--- src/ca/sqlpower/architect/diff/SQLObjectComparator.java
+++ src/ca/sqlpower/architect/diff/SQLObjectComparator.java
@@ -7,27 +7,17 @@ import ca.sqlpower.architect.SQLObject;
 public class SQLObjectComparator implements Comparator<SQLObject> {
 
 	public int compare(SQLObject t1, SQLObject t2) {
-		if (t1 instanceof SQLObject) {
-			if (t2 instanceof SQLObject) {
-				if (t1 != null && t2 != null) {
-					return (((SQLObject) t1).getName())
-							.compareTo(((SQLObject) t2).getName());
-				} else {
-					// if t1 is null t2 is greater
-					if (t1 == null) {
-						return -1;
-					} else {
-						// if t2 is null t1 is greater
-						return 1;
-					}
-				}
-
-			} else {
-				return 1;
-			}
-		} else {
-			return -1;
+		// if t1 and t2 refer to the same object, or are both null, then they're equal
+		if (t1 == t2) return 0;
+		else if (t1 == null) return -1;
+		else if (t2 == null) return 1;
+		else {
+			String n1 = t1.getName();
+			String n2 = t2.getName();
+			if (n1 == n2) return 0;
+			else if (n1 == null) return -1;
+			else if (n2 == null) return 1;
+			else return n1.compareTo(n2);
 		}
 	}
-
 }
