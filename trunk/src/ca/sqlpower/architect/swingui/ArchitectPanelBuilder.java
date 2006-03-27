33dfc586adc4e1fe90b11c69d033f7af1c54b8b9
benjamin <benjamin@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-03-27 19:35:42 +0000

    Set defaultButton to OK. Try to get escape handling working (not done). Revise test class to instantiate inner class each time, to avoid problems with Swing containment (Ian & Johnson)

--- trunk/src/ca/sqlpower/architect/swingui/ArchitectPanelBuilder.java
+++ trunk/src/ca/sqlpower/architect/swingui/ArchitectPanelBuilder.java
@@ -2,19 +2,21 @@ package ca.sqlpower.architect.swingui;
 
 import java.awt.BorderLayout;
 import java.awt.Dialog;
+import java.awt.Event;
 import java.awt.Frame;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.InputMap;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 import com.jgoodies.forms.factories.Borders;
 import com.jgoodies.forms.factories.ButtonBarFactory;
@@ -56,14 +58,15 @@ public class ArchitectPanelBuilder {
 		});
 		cancelButton.setText("Cancel");
 		
-		d.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				int key = e.getKeyCode();
-				System.out.println("Pressed key " + key);
-				super.keyTyped(e);
-			}
-		});
+		// Handle if the user presses Enter in the dialog - do OK action
+		d.getRootPane().setDefaultButton(okButton);
+		
+		// Handle if the user presses <ESCAPE> in the dialog - do CANCEL action
+		// XXX BUG for now this (<ESCAPE>) does NOT work
+		KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, Event.SHIFT_MASK);
+		// System.out.println("KEYSTROKE = " + keyStroke);
+		InputMap inputMap = d.getRootPane().getInputMap();
+		inputMap.put(keyStroke, cancelAction);
 
 		JPanel cp = new JPanel(new BorderLayout());
 		cp.add(panel, BorderLayout.CENTER);
