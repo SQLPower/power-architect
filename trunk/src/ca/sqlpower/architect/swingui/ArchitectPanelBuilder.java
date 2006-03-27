ddae9bc4c19188a05b5235f5a792136cdbf3bab3
benjamin <benjamin@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-03-27 17:57:13 +0000

    New: ArchitectPanel builder, with methods for creating the Dialog and buttons around a provided ArchitectPanel/JPanel. Default Actions just call the relevant methods in ArchitectPanel, or you can pass your own Action objects (especially useful if your Action code should run in a Thread, you get to do that part). Suggested by Jonathan, designed & coded by Ian & Johnson, further comments by Jonathan.

---
+++ trunk/src/ca/sqlpower/architect/swingui/ArchitectPanelBuilder.java
@@ -0,0 +1,102 @@
+package ca.sqlpower.architect.swingui;
+
+import java.awt.BorderLayout;
+import java.awt.Dialog;
+import java.awt.Frame;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+
+import com.jgoodies.forms.factories.Borders;
+import com.jgoodies.forms.factories.ButtonBarFactory;
+
+public class ArchitectPanelBuilder {
+	
+	public static JDialog createArchitectPanelDialog(
+			final ArchitectPanel arch,
+			final Window dialogParent, 
+			final String dialogTitle,
+			final String actionButtonTitle,
+			final Action okAction,
+			final Action cancelAction) {
+		final JDialog d;
+		if (dialogParent instanceof Frame) {
+			d = new JDialog((Frame) dialogParent, dialogTitle);
+		} else if (dialogParent instanceof Dialog) {
+			d = new JDialog((Dialog) dialogParent, dialogTitle);
+		} else {
+			throw new IllegalArgumentException("The dialogParent you gave me is not a " +
+					"Frame or Dialog (it is a "+dialogParent.getClass().getName()+")");
+		}
+		JComponent panel = arch.getPanel();
+		JButton okButton = new JButton(okAction);
+		okButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				d.setVisible(false);
+                d.dispose();
+			}		
+		});
+		okButton.setText(actionButtonTitle);
+		
+		JButton cancelButton = new JButton(cancelAction);
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				d.setVisible(false);
+                d.dispose();
+			}
+		});
+		cancelButton.setText("Cancel");
+		
+		JPanel cp = new JPanel(new BorderLayout());
+		cp.add(panel, BorderLayout.CENTER);		
+		
+		cp.add(ButtonBarFactory.buildOKCancelBar(okButton, cancelButton),
+				BorderLayout.SOUTH);
+		cp.setBorder(Borders.DIALOG_BORDER);
+		
+		d.add(cp);
+		
+		d.pack();
+		return d;
+	}
+	
+	/**
+	 * Build a JDialog around an object that implements
+	 * ArchitectPanel, to provide consistent behaviours
+	 * such as Cancel button, <ESC> to close, and so on.
+	 * @param arch The ArchitectPanel implementation
+	 * @param dialogParent A Window class to be the parent, or null
+	 * @param dialogTitle The display title.
+	 * @return The build JDialog
+	 */
+	public static JDialog createArchitectPanelDialog(
+		final ArchitectPanel arch,
+		final Window dialogParent, 
+		final String dialogTitle,
+		final String actionButtonTitle) {
+		
+		Action okAction = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				arch.applyChanges();
+			}			
+		};
+		Action cancelAction = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				arch.discardChanges();
+			}			
+		};
+		return createArchitectPanelDialog(arch,
+				dialogParent,
+				dialogTitle,
+				actionButtonTitle,
+				okAction,
+				cancelAction);
+	}
+}
