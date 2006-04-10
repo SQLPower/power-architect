f67470e9ba1eb9d19e90d9ff1fc3d3ed140e26d0
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-04-10 21:01:08 +0000

    javadoc what this does.

--- src/ca/sqlpower/architect/PrefsUtils.java
+++ src/ca/sqlpower/architect/PrefsUtils.java
@@ -4,8 +4,9 @@ import java.util.prefs.Preferences;
 
 public class PrefsUtils {
 
-	/**
-	 * @param mainClassInstance
+	/** Gets the Preferences node for a package, given one class in the package
+	 * (either by reference or by reference to its Class object).
+	 * @param mainClassInstance Either the object, or the object's Class object.
 	 */
 	public static Preferences getUserPrefsNode(Object mainClassInstance) {
 		Class clazz;
