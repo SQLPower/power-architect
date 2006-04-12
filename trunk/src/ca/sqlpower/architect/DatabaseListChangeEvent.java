43353465cb915b7a81aa5af648c57777d926ca2b
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-04-12 22:46:48 +0000

    First stab at DatabaseListChange{Event,Listener} for PlDotInit (which should probably be renamed DataBaseList). NOT TESTED; have to commit & run because the office closes up...

---
+++ trunk/src/ca/sqlpower/architect/DatabaseListChangeEvent.java
@@ -0,0 +1,19 @@
+package ca.sqlpower.architect;
+
+import java.util.EventObject;
+
+public class DatabaseListChangeEvent extends EventObject implements java.io.Serializable {
+	
+	int listIndex;
+	ArchitectDataSource dbcs;
+	
+	/**
+	 * @param dbcs
+	 * @param index
+	 */
+	public DatabaseListChangeEvent(Object source, int index, ArchitectDataSource dbcs) {
+		super(source);
+		this.dbcs = dbcs;
+		listIndex = index;
+	}
+}
