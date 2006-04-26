54583c0d27c49ef64ec3ae5b38c51768e5736b27
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-04-26 22:41:23 +0000

    Add test method that can't be used yet due to poor choice of superclass.

--- trunk/regress/prefs/MemoryPreferencesTest.java
+++ trunk/regress/prefs/MemoryPreferencesTest.java
@@ -1,7 +1,9 @@
 package prefs;
 
+import java.io.ByteArrayInputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.prefs.Preferences;
 
 import junit.framework.TestCase;
 
@@ -29,5 +31,22 @@ public class MemoryPreferencesTest extends TestCase {
 		assertEquals(123, mp.getInt("goo", 123));
 		mp.putInt("foo", 456);
 		assertEquals(456, mp.getInt("foo", 42));
+	}
+	
+	/**
+	 * You can NOT use the importPreferences() method with this implementation because we
+	 * subclass Preferences directly, whereas Sun's implementation of Preferences requires
+	 * you do subclass it via AbstractPreferences (this requirement is not explicitly documented, Sun).
+	 */
+	public void DONOTtestLoad() throws Exception {
+		// The Import code is shared static, so it should work even with our implementation
+		String xml = "<?xml version='1.0' encoding='UTF-8'?>" +
+			"<!DOCTYPE map SYSTEM 'http://java.sun.com/dtd/preferences.dtd'>" +
+			"<map MAP_XML_VERSION='1.0'><entry key='displayFontName' value='helvetica'/>" +
+			"<entry key='textFontName' value='times-roman'/></map>";
+		ByteArrayInputStream rdr = new ByteArrayInputStream(xml.getBytes());
+		Preferences.importPreferences(rdr);
+		String val = Preferences.userRoot().get("helvetica", "I give up");
+		System.out.println(val);
 	}
 }
