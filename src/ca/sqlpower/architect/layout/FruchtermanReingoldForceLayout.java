1c79c723aa0b0ef5ea0eff306360e23cfe8d56e4
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-06-12 15:23:02 +0000

    Organize imports to remove warnings.

--- src/ca/sqlpower/architect/layout/FruchtermanReingoldForceLayout.java
+++ src/ca/sqlpower/architect/layout/FruchtermanReingoldForceLayout.java
@@ -3,7 +3,6 @@ package ca.sqlpower.architect.layout;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
