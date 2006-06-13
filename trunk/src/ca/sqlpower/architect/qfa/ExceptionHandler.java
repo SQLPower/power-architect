3c95e096bd8090359c8294f34a41b38decc3aac2
fuerth <fuerth@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-06-13 21:38:52 +0000

    Created exception handler for unhandled exceptions that escape the stacks of threads (including the awt event dispatch thread), and also added a remarks field to the error report.

---
+++ trunk/src/ca/sqlpower/architect/qfa/ExceptionHandler.java
@@ -0,0 +1,55 @@
+/*
+ * Created on Jun 13, 2006
+ *
+ * This code belongs to SQL Power Group Inc.
+ */
+package ca.sqlpower.architect.qfa;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+import javax.swing.tree.TreeModel;
+
+import ca.sqlpower.architect.swingui.ArchitectFrame;
+import ca.sqlpower.architect.swingui.DBTree;
+import ca.sqlpower.architect.swingui.PlayPen;
+import ca.sqlpower.architect.swingui.SwingUIProject;
+
+public class ExceptionHandler implements UncaughtExceptionHandler {
+
+    public void uncaughtException(Thread t, Throwable e) {
+        ExceptionReport r = new ExceptionReport(e);
+        ArchitectFrame af = ArchitectFrame.getMainInstance();
+        StringBuffer remarks = new StringBuffer();
+        if (af != null) {
+            SwingUIProject p = af.getProject();
+            if (p != null) {
+                PlayPen pp = p.getPlayPen();
+                if (pp != null) {
+                    r.setNumObjectsInPlayPen(pp.getPPComponentCount());
+                } else {
+                    remarks.append("[playpen was null]");
+                }
+                DBTree dbt = p.getSourceDatabases();
+                if (dbt != null) {
+                    TreeModel dbtm = dbt.getModel();
+                    if (dbtm != null) {
+                        r.setNumSourceConnections(dbtm.getChildCount(dbtm.getRoot()));
+                    } else {
+                        remarks.append("[dbtree's model was null]");
+                    }
+                } else {
+                    remarks.append("[dbtree was null]");
+                }
+            } else {
+                remarks.append("[architect frame's project was null]");
+            }
+        } else {
+            remarks.append("[architect frame's main instance was null]");
+        }
+        
+        r.setRemarks(remarks.toString());
+        
+        r.postReportToSQLPower();
+    }
+
+}
