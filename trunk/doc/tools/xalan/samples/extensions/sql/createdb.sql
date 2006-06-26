30da63eed085edb93815649f6dfddb36472c2f2e
fuerth <fuerth@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-06-26 20:54:38 +0000

    Removed xalan source code and examples (we don't need them)

--- trunk/doc/tools/xalan/samples/extensions/sql/createdb.sql
+++
@@ -1,8 +0,0 @@
-connect 'sampleDB;create=true';
-
-create table import1(  ProductID int,  Name varchar(40));
-
-insert into import1 (ProductID, Name) values (1, 'toothpaste');
-insert into import1 (ProductID, Name) values (2, 'milk');
-insert into import1 (ProductID, Name) values (3, 'bread');
-insert into import1 (ProductID, Name) values (4, 'cereal');
