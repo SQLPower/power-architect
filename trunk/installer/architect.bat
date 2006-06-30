4f22b971bbba617b28488cdac1afcb57311624d7
matthew <matthew@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-06-30 00:07:28 +0000

    windows installer files and some bug fixes

--- trunk/installer/architect.bat
+++ trunk/installer/architect.bat
@@ -1,3 +1,4 @@
-REM This is a temporary way to launch the architect.  We will migrate to WebStart ASAP!
-set CLASSPATH=.;architect.jar;commons-beanutils-bean-collections.jar;commons-beanutils-core.jar;commons-beanutils.jar;commons-collections-3.1.jar;commons-dbcp-1.2.1.jar;commons-digester.jar;commons-logging.jar;commons-pool-1.3.jar;forms-1.0.6.jar;jakarta-regexp-1.2.jar;jfcunit.jar;jlfgr-1_0.jar;junit.jar;log4j.jar;sqlpower.jar
-javaw -Dlog4j.configuration=file:log4j.properties -jar architect.jar
+REM Launches the architect using the newest available Java JDK Thingy
+${INSTALL_DISK}
+cd ${INSTALL_PATH}
+"C:\Program Files\JavaSoft\JDK-1.5.0_06\bin\javaw" -Xmx600M -Dlog4j.configuration=file:log4j.properties -jar architect.jar
