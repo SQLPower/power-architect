d6322bc5249c7cf0bd96f5010e59ec9f71e9bf64
matthew <matthew@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-08-15 19:24:15 +0000

    Modified the ui to default to a table view

--- src/ca/sqlpower/architect/swingui/ProfilePanel.java
+++ src/ca/sqlpower/architect/swingui/ProfilePanel.java
@@ -56,31 +56,26 @@ import com.jgoodies.forms.layout.FormLayout;
  */
 public class ProfilePanel extends JPanel {
     private static Logger logger = Logger.getLogger(ProfilePanel.class);
+    public enum ChartTypes { BAR, PIE }
+
 
     private JComboBox tableSelector;
     private JList columnSelector;
     private int rowCount;
-    private JLabel title;
-    private JLabel rowCountDisplay;
-    private JPanel controlsArea, displayArea;
-    private JLabel nullableLabel;
-    private JLabel minValue;
-    private JLabel maxValue;
-    private JLabel avgValue;
-    private JLabel nullCountLabel;
-    private JLabel nullPercentLabel;
-    private JLabel minLengthLabel;
-    private JLabel maxLengthLabel;
-    private final JProgressBar progressBar = new JProgressBar();
-    private ChartPanel chartPanel;
-
-    public enum ChartTypes { BAR, PIE }
-
     private ChartTypes chartType = ChartTypes.PIE;
-
-    private ProfileManager pm = new ProfileManager();
-
-    public ProfilePanel() {
+    private JLabel rowCountDisplay;
+    private JPanel controlsArea;
+    private DisplayPanel displayPanel;
+    
+    private final JProgressBar progressBar = new JProgressBar();
+    private final ProfileManager pm;
+    
+    public ProfilePanel(ProfileManager pm) {
+        this.pm = pm;
+        displayPanel = new DisplayPanel(0,pm);
+        setup();
+    }
+    private void setup() {
         progressBar.setVisible(false);
         FormLayout controlsLayout = new FormLayout(
                 "4dlu,fill:min(150dlu;default):grow, 4dlu", // columns
@@ -114,7 +109,9 @@ public class ProfilePanel extends JPanel {
                         public void run() {
                             try {
                                 progressBar.setVisible(true);
-                                pm.createProfiles(Collections.nCopies(1, t));
+                                if (pm.getResult(t) == null) {
+                                    pm.createProfiles(Collections.nCopies(1, t));
+                                }
                                 TableProfileResult r = (TableProfileResult) pm.getResult(t);
                                 setRowCount(r.getRowCount());
                                 progressBar.setVisible(false);
@@ -151,8 +148,8 @@ public class ProfilePanel extends JPanel {
                     logger.debug("Null selection in columnSelector.ListSelectionListener");
                     return;
                 }
-                displayProfile((SQLTable) tableSelector.getSelectedItem(), col);
-            }
+                displayPanel.displayProfile((SQLTable) tableSelector.getSelectedItem(), col);              
+            }           
         });
         rowCountDisplay = new JLabel("Row Count: --");
 
@@ -164,8 +161,8 @@ public class ProfilePanel extends JPanel {
         pb.add(new JScrollPane(columnSelector), cc.xy(2, 5));
         pb.add(progressBar,cc.xy(2,7));
         this.add(controlsArea, BorderLayout.WEST);
-        createDisplayPanel();
-        this.add(displayArea, BorderLayout.CENTER);
+        displayPanel.setChartType(chartType);
+        this.add(displayPanel.getDisplayArea(), BorderLayout.CENTER);
 
     }
 
@@ -177,144 +174,212 @@ public class ProfilePanel extends JPanel {
     public void setTables(List<SQLTable> tables) {
         tableSelector.setModel(new DefaultComboBoxModel(tables.toArray()));
     }
-
-    public void setTitle(String title) {
-        this.title.setText(title);
-    }
-
-    private void createDisplayPanel() {
-        FormLayout displayLayout = new FormLayout(
-                "4dlu, default, 4dlu, 100dlu, 4dlu, fill:default:grow, 4dlu", // columns
-                "4dlu, default, 6dlu"); // rows
-        CellConstraints cc = new CellConstraints();
-
-        displayArea = logger.isDebugEnabled()  ? new FormDebugPanel(displayLayout) : new JPanel(displayLayout);
-        displayArea.setBorder(BorderFactory.createEtchedBorder());
-
-        Font bodyFont = getFont();
-        Font titleFont = bodyFont.deriveFont(Font.BOLD, bodyFont.getSize() * 1.25F);
-
-        title = new JLabel("Column Name");
-        title.setFont(titleFont);
-
-        PanelBuilder pb = new PanelBuilder(displayLayout, displayArea);
-        pb.add(title, cc.xyw(2, 2, 5));
-
-        int row = 4;
-        nullableLabel = makeInfoRow(pb, "Nullable", row); row += 2;
-        nullCountLabel = makeInfoRow(pb, "Null Count", row); row += 2;
-        nullPercentLabel = makeInfoRow(pb, "% Null Records", row); row += 2;
-        minLengthLabel = makeInfoRow(pb, "Minimum Length", row); row += 2;
-        maxLengthLabel = makeInfoRow(pb, "Maximum Length", row); row += 2;
-        minValue = makeInfoRow(pb, "Minimum Value", row); row += 2;
-        maxValue = makeInfoRow(pb, "Maximum Value", row); row += 2;
-        avgValue = makeInfoRow(pb, "Average Value", row); row += 2;
-
-        pb.appendRow("fill:4dlu:grow");
-        pb.appendRow("4dlu");
-        // Now add something to represent the chart
-        JFreeChart createPieChart = ChartFactory.createPieChart("",new DefaultPieDataset(new DefaultKeyedValues()),false,false,false);
-        chartPanel = new ChartPanel(createPieChart);
-        pb.add(chartPanel, cc.xywh(6, 4, 1, 17));
-    }
-
-    private void displayProfile(SQLTable t, SQLColumn c) {
-        ColumnProfileResult cr = (ColumnProfileResult) pm.getResult(c);
-        StringBuffer sb = new StringBuffer();
-        sb.append(c);
-        if (c.isPrimaryKey()) {
-            sb.append(' ').append("[PK]");
+    
+    /**
+     *  Creates and handles a specific display panel
+     *
+     */
+    class DisplayPanel {
+        
+        private JLabel title;
+        private JLabel nullableLabel;
+        private JLabel minValue;
+        private JLabel maxValue;
+        private JLabel avgValue;
+        private JLabel nullCountLabel;
+        private JLabel nullPercentLabel;
+        private JLabel minLengthLabel;
+        private JLabel uniqueCountLabel;
+        private JLabel uniquePercentLabel;
+        private JLabel maxLengthLabel;
+        private ChartTypes chartType = ChartTypes.PIE;
+        private JPanel displayArea;
+        private int rowCount;
+        private ChartPanel chartPanel;
+        private ProfileManager pm;
+        
+        
+        public DisplayPanel(int rowCount, ProfileManager pm) {
+            this.rowCount = rowCount;
+            this.pm = pm;
+            displayArea = new JPanel();
+            FormLayout displayLayout = new FormLayout(
+                    "4dlu, default, 4dlu, 100dlu, 4dlu, fill:default:grow, 4dlu", // columns
+                    "4dlu, default, 6dlu"); // rows
+            CellConstraints cc = new CellConstraints();
+            
+            displayArea = logger.isDebugEnabled()  ? new FormDebugPanel(displayLayout) : new JPanel(displayLayout);
+            displayArea.setBorder(BorderFactory.createEtchedBorder());
+            
+            Font bodyFont = displayArea.getFont();
+            Font titleFont = bodyFont.deriveFont(Font.BOLD, bodyFont.getSize() * 1.25F);
+            
+            title = new JLabel("Column Name");
+            title.setFont(titleFont);
+            
+            PanelBuilder pb = new PanelBuilder(displayLayout, displayArea);
+            pb.add(title, cc.xyw(2, 2, 5));
+            
+            int row = 4;
+            nullableLabel = makeInfoRow(pb, "Nullable", row); row += 2;
+            nullCountLabel = makeInfoRow(pb, "Null Count", row); row += 2;
+            nullPercentLabel = makeInfoRow(pb, "% Null Records", row); row += 2;
+            minLengthLabel = makeInfoRow(pb, "Minimum Length", row); row += 2;
+            maxLengthLabel = makeInfoRow(pb, "Maximum Length", row); row += 2;
+            uniqueCountLabel = makeInfoRow(pb,"Unique Values",row); row+=2;
+            uniquePercentLabel = makeInfoRow(pb,"% Unique", row); row +=2;
+            minValue = makeInfoRow(pb, "Minimum Value", row); row += 2;
+            maxValue = makeInfoRow(pb, "Maximum Value", row); row += 2;
+            avgValue = makeInfoRow(pb, "Average Value", row); row += 2;
+            
+            pb.appendRow("fill:4dlu:grow");
+            pb.appendRow("4dlu");
+            // Now add something to represent the chart
+            JFreeChart createPieChart = ChartFactory.createPieChart("",new DefaultPieDataset(new DefaultKeyedValues()),false,false,false);
+            chartPanel = new ChartPanel(createPieChart);
+          
+            pb.add(chartPanel, cc.xywh(6, 4, 1, row-3));
         }
-        setTitle(sb.toString());
-        nullableLabel.setText(Boolean.toString(c.isDefinitelyNullable()));
-        if (cr == null) {
-            logger.error("displayProfile called but unable to get ColumnProfileResult for column: "+c);
-            cr = new ColumnProfileResult();
-            cr.setCreateStartTime(0);
-            chartPanel.setChart(ChartFactory.createPieChart("", new DefaultPieDataset(), false,false,false));
-        } else {
-            chartPanel.setChart(createTopNChart(c));
+        
+        public void setTitle(String title) {
+            this.title.setText(title);
         }
-            nullCountLabel.setText(Integer.toString(cr.getNullCount()));
-            int nullsInRecords = cr.getNullCount();
-            double ratio = rowCount > 0 ? nullsInRecords * 100D / rowCount : 0;
-            nullPercentLabel.setText(format(ratio));
+        
+        
+        public JPanel getDisplayArea() {
+            return displayArea;
+        }
 
-            minLengthLabel.setText(Integer.toString(cr.getMinLength()));
-            maxLengthLabel.setText(Integer.toString(cr.getMaxLength()));
+        public void setDisplayArea(JPanel displayArea) {
+            this.displayArea = displayArea;
+        }
 
-            minValue.setText(cr.getMinValue() == null ? "" : cr.getMinValue().toString());
-            maxValue.setText(cr.getMaxValue() == null ? "" : cr.getMaxValue().toString());
-            Object o = cr.getAvgValue();
-            if (o == null) {
-                avgValue.setText("");
-            } else if (o instanceof BigDecimal) {
-                double d = ((BigDecimal)o).doubleValue();
-                avgValue.setText(format(d));
+        public void displayProfile(SQLTable t, SQLColumn c) {
+            ColumnProfileResult cr = (ColumnProfileResult) pm.getResult(c);
+            TableProfileResult tr = (TableProfileResult) pm.getResult(t);
+            if (tr instanceof TableProfileResult) {
+                rowCount = tr.getRowCount();
+            }
+            StringBuffer sb = new StringBuffer();
+            sb.append(c);
+            if (c.isPrimaryKey()) {
+                sb.append(' ').append("[PK]");
+            }
+            setTitle(sb.toString());
+            nullableLabel.setText(Boolean.toString(c.isDefinitelyNullable()));
+            if (cr == null) {
+                logger.error("displayProfile called but unable to get ColumnProfileResult for column: "+c);
+                cr = new ColumnProfileResult();
+                cr.setCreateStartTime(0);
+                chartPanel.setChart(ChartFactory.createPieChart("",new DefaultPieDataset(),false,false,false));
             } else {
-                logger.debug("Got avgValue of type: " + o.getClass().getName());
-                avgValue.setText(cr.getAvgValue().toString());
+                chartPanel.setChart(createTopNChart(c));
             }
-
-
-    }
-    private JFreeChart createTopNChart(SQLColumn sqo){
-        JFreeChart chart;
-        ColumnProfileResult cr = (ColumnProfileResult) pm.getResult(sqo);
-        List<ColumnValueCount> valueCounts = cr.getValueCount();
-        DefaultCategoryDataset catDataset = new DefaultCategoryDataset();
-
-        long otherDataCount = rowCount;
-        for (ColumnValueCount vc: valueCounts){
-            catDataset.addValue(vc.getCount(),sqo.getName(),vc.getValue()==null ? "null" : vc.getValue().toString());
-            otherDataCount -= vc.getCount();
+                nullCountLabel.setText(Integer.toString(cr.getNullCount()));
+                int nullsInRecords = cr.getNullCount();
+                double ratio = rowCount > 0 ? nullsInRecords * 100D / rowCount : 0;
+                nullPercentLabel.setText(format(ratio));
+                uniqueCountLabel.setText(Integer.toString(cr.getDistinctValueCount()));
+                double uniqueRatio = rowCount > 0 ? nullsInRecords * 100D / rowCount : 0;
+                uniquePercentLabel.setText(format(uniqueRatio));
+                minLengthLabel.setText(Integer.toString(cr.getMinLength()));
+                maxLengthLabel.setText(Integer.toString(cr.getMaxLength()));
+                
+                minValue.setText(cr.getMinValue() == null ? "" : cr.getMinValue().toString());
+                maxValue.setText(cr.getMaxValue() == null ? "" : cr.getMaxValue().toString());
+                Object o = cr.getAvgValue();
+                if (o == null) {
+                    avgValue.setText("");
+                } else if (o instanceof BigDecimal) {
+                    double d = ((BigDecimal)o).doubleValue();
+                    avgValue.setText(format(d));
+                } else {
+                    logger.debug("Got avgValue of type: " + o.getClass().getName());
+                    avgValue.setText(cr.getAvgValue().toString());  
+                }
         }
-        int numberOfTopValues = catDataset.getColumnCount();
-        if (otherDataCount > 0){
-            catDataset.addValue(otherDataCount,sqo.getName(),"Other Values");
-        }
-
-        if ( chartType == ChartTypes.BAR ){
-            chart = ChartFactory.createBarChart(
-                    "Top "+numberOfTopValues+" most common non-unique values",
-                    "","",catDataset,PlotOrientation.VERTICAL,
-                    false,true,false);
-
-            final CategoryAxis domainAxis = chart.getCategoryPlot().getDomainAxis();
-            domainAxis.setCategoryLabelPositions(CategoryLabelPositions.DOWN_90);
-        } else if (chartType == ChartTypes.PIE) {
-
-            chart = ChartFactory.createPieChart(
-                    "Top "+numberOfTopValues+" most common values",
-                    new CategoryToPieDataset(catDataset,TableOrder.BY_ROW,0),
-                    false,true,false);
-            if (chart.getPlot() instanceof PiePlot) {
-                ((PiePlot)chart.getPlot()).setLabelGenerator(new StandardPieSectionLabelGenerator("{0} [{1}]"));
+        
+        private JFreeChart createTopNChart(SQLColumn sqo){
+            JFreeChart chart;
+            ColumnProfileResult cr = (ColumnProfileResult) pm.getResult(sqo);
+            List<ColumnValueCount> valueCounts = cr.getValueCount();
+            DefaultCategoryDataset catDataset = new DefaultCategoryDataset();
+         
+            long otherDataCount = rowCount;
+            for (ColumnValueCount vc: valueCounts){    
+                catDataset.addValue(vc.getCount(),sqo.getName(),vc.getValue()==null ? "null" : vc.getValue().toString());
+                otherDataCount -= vc.getCount();
             }
-        } else {
-            throw new IllegalStateException("chart type "+chartType+" not recognized");
+            int numberOfTopValues = catDataset.getColumnCount();
+            if (otherDataCount > 0){
+                catDataset.addValue(otherDataCount,sqo.getName(),"Other Values");
+            }
+            
+            if ( chartType == ChartTypes.BAR ){
+                String chartTitle;
+                if (numberOfTopValues == 10 ) {
+                    chartTitle = "Top "+numberOfTopValues+" most common values";
+                } else {
+                    chartTitle = "All "+numberOfTopValues+" values";
+                }
+               
+                chart = ChartFactory.createBarChart(
+                        chartTitle,
+                        "","",catDataset,PlotOrientation.VERTICAL,
+                        false,true,false);
+                    
+                final CategoryAxis domainAxis = chart.getCategoryPlot().getDomainAxis();
+                domainAxis.setCategoryLabelPositions(CategoryLabelPositions.DOWN_90);
+            } else if (chartType == ChartTypes.PIE) {
+            
+                String chartTitle;
+                if (numberOfTopValues == 10 ) {
+                    chartTitle = "Top "+numberOfTopValues+" most common values";
+                } else {
+                    chartTitle = "All "+numberOfTopValues+" values";
+                }
+                
+                chart = ChartFactory.createPieChart(
+                        chartTitle,
+                        new CategoryToPieDataset(catDataset,TableOrder.BY_ROW,0),
+                        false,true,false);
+                if (chart.getPlot() instanceof PiePlot) {
+                    ((PiePlot)chart.getPlot()).setLabelGenerator(new StandardPieSectionLabelGenerator("{0} [{1}]"));
+                }
+            } else {
+
+                throw new IllegalStateException("chart type "+chartType+" not recognized");
+            }
+            return chart;
         }
-        return chart;
+        public ChartTypes getChartType() {
+            return chartType;
+        }
+    
+        public void setChartType(ChartTypes chartType) {
+            this.chartType = chartType;
+        }
+    
+        private String format(double d) {
+            return String.format("%6.2f", d);
+        }
+
+        
+        private JLabel makeInfoRow(PanelBuilder pb, String title, int row) {
+            CellConstraints cc = new CellConstraints();
+            pb.appendRow("default");
+            pb.appendRow("2dlu");
+            pb.add(new JLabel(title), cc.xy(2, row));
+            JLabel label = new JLabel("--");
+            pb.add(label, cc.xy(4, row));
+            return label;
+        }
     }
+
     public ChartTypes getChartType() {
         return chartType;
     }
-
     public void setChartType(ChartTypes chartType) {
         this.chartType = chartType;
-    }
-
-    private String format(double d) {
-        return String.format("%6.2f", d);
-    }
-
-    private JLabel makeInfoRow(PanelBuilder pb, String title, int row) {
-        CellConstraints cc = new CellConstraints();
-        pb.appendRow("default");
-        pb.appendRow("2dlu");
-        pb.add(new JLabel(title), cc.xy(2, row));
-        JLabel label = new JLabel("--");
-        pb.add(label, cc.xy(4, row));
-        return label;
     }
 }
---
+++ src/ca/sqlpower/architect/swingui/action/ProfilePanelAction.java
@@ -0,0 +1,147 @@
+package ca.sqlpower.architect.swingui.action;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JDialog;
+import javax.swing.JEditorPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.tree.TreePath;
+
+import org.apache.log4j.Logger;
+
+import ca.sqlpower.architect.ArchitectException;
+import ca.sqlpower.architect.SQLObject;
+import ca.sqlpower.architect.SQLTable;
+import ca.sqlpower.architect.profile.ProfileManager;
+import ca.sqlpower.architect.profile.ProfileResultFormatter;
+import ca.sqlpower.architect.swingui.ASUtils;
+import ca.sqlpower.architect.swingui.ArchitectFrame;
+import ca.sqlpower.architect.swingui.ArchitectPanelBuilder;
+import ca.sqlpower.architect.swingui.CommonCloseAction;
+import ca.sqlpower.architect.swingui.DBTree;
+import ca.sqlpower.architect.swingui.JDefaultButton;
+import ca.sqlpower.architect.swingui.ProfilePanel;
+import ca.sqlpower.architect.swingui.SwingUserSettings;
+import ca.sqlpower.architect.swingui.ProfilePanel.ChartTypes;
+
+public class ProfilePanelAction extends AbstractAction {
+    private static final Logger logger = Logger.getLogger(ProfileAction.class);
+
+    protected DBTree dbTree;
+    protected ProfileManager profileManager;
+    protected JDialog d;
+            
+    
+    public ProfilePanelAction() {
+        super("Profile...", ASUtils.createJLFIcon( "general/Information",
+                        "Information", 
+                        ArchitectFrame.getMainInstance().getSprefs().getInt(SwingUserSettings.ICON_SIZE, 24)));
+        
+        putValue(SHORT_DESCRIPTION, "Profile Tables");
+    }
+    
+    /**
+     * Called to pop up the ProfilePanel
+     */
+    public void actionPerformed(ActionEvent e) {
+        if (dbTree == null) {
+            logger.debug("dbtree was null when actionPerformed called");
+            return;
+        }
+        try {
+            if ( dbTree.getSelectionPaths() == null )
+                return;
+            
+            final Set <SQLTable> tables = new HashSet();
+            for ( TreePath p : dbTree.getSelectionPaths() ) {
+                SQLObject so = (SQLObject) p.getLastPathComponent();
+                Collection<SQLTable> tablesUnder = tablesUnder(so);
+                System.out.println("Tables under "+so+" are: "+tablesUnder);
+                tables.addAll(tablesUnder);
+            }
+            
+            final JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+            Action okAction = new AbstractAction() {
+                public void actionPerformed(ActionEvent evt) {
+                    d.setVisible(false);
+                }
+            };
+            okAction.putValue(Action.NAME, "OK");
+            final JDefaultButton okButton = new JDefaultButton(okAction);
+            buttonPanel.add(okButton);
+            d = new JDialog(ArchitectFrame.getMainInstance(), "Table Profiles");
+            profileManager.createProfiles(tables);
+            JTabbedPane tabPane = new JTabbedPane();
+            JEditorPane editorPane = new JEditorPane();
+            editorPane.setEditable(false);
+            editorPane.setContentType("text/html");
+            ProfileResultFormatter prf = new ProfileResultFormatter();
+            editorPane.setText(prf.format(tables,profileManager) );
+
+            JScrollPane editorScrollPane = new JScrollPane(editorPane);
+            editorScrollPane.setVerticalScrollBarPolicy(
+                            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+            editorScrollPane.setPreferredSize(new Dimension(800, 600));
+            editorScrollPane.setMinimumSize(new Dimension(10, 10));
+            
+            
+            
+            tabPane.addTab("Table View", editorScrollPane );
+            ProfilePanel p = new ProfilePanel(profileManager);
+            tabPane.addTab("Graph View",p);
+            
+            List<SQLTable> list = new ArrayList(tables);
+            p.setTables(list);
+            p.setChartType(ChartTypes.PIE);
+
+            ArchitectPanelBuilder.makeJDialogCancellable(
+                    d, new CommonCloseAction(d));
+            d.setContentPane(tabPane);
+            d.pack();
+            d.setLocationRelativeTo(ArchitectFrame.getMainInstance());
+            d.setVisible(true);
+    
+        } catch (Exception ex) {
+            logger.error("Error in Profile Action ", ex);
+            ASUtils.showExceptionDialog(dbTree, "Error during profile run", ex);
+        }
+    }
+
+
+    private Collection<SQLTable> tablesUnder(SQLObject so) throws ArchitectException {
+        List<SQLTable> tables = new ArrayList<SQLTable>();
+        if (so instanceof SQLTable) {
+            tables.add((SQLTable) so);
+        } else if (so.allowsChildren()) {
+            for (SQLObject child : (List<SQLObject>) so.getChildren()) {
+                tables.addAll(tablesUnder(child));
+            }
+        }
+        return tables;
+    }
+
+    public void setDBTree(DBTree dbTree) {
+        this.dbTree = dbTree;
+    }
+
+    public ProfileManager getProfileManager() {
+        return profileManager;
+    }
+
+    public void setProfileManager(ProfileManager profileManager) {
+        this.profileManager = profileManager;
+    }
+    
+
+}
