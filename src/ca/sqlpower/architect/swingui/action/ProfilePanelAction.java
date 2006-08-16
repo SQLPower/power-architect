645b483c6b6a38ab02749ffb116f1c40c89af786
johnson <johnson@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-08-16 14:23:01 +0000

    added progress bar

--- src/ca/sqlpower/architect/swingui/action/ProfilePanelAction.java
+++ src/ca/sqlpower/architect/swingui/action/ProfilePanelAction.java
@@ -8,6 +8,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
@@ -20,8 +21,10 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.tree.TreePath;
@@ -43,6 +46,7 @@ import ca.sqlpower.architect.swingui.CommonCloseAction;
 import ca.sqlpower.architect.swingui.DBTree;
 import ca.sqlpower.architect.swingui.JDefaultButton;
 import ca.sqlpower.architect.swingui.ProfilePanel;
+import ca.sqlpower.architect.swingui.ProgressWatcher;
 import ca.sqlpower.architect.swingui.SwingUserSettings;
 import ca.sqlpower.architect.swingui.ProfilePanel.ChartTypes;
 
@@ -82,113 +86,165 @@ public class ProfilePanelAction extends AbstractAction {
                 tables.addAll(tablesUnder);
             }
             
-            final JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-            Action okAction = new AbstractAction() {
+            
+            d = new JDialog(ArchitectFrame.getMainInstance(), "Table Profiles");
+            
+            
+            Action closeAction = new AbstractAction() {
                 public void actionPerformed(ActionEvent evt) {
                     d.setVisible(false);
                 }
             };
-            okAction.putValue(Action.NAME, "OK");
-            final JDefaultButton okButton = new JDefaultButton(okAction);
-            buttonPanel.add(okButton);
-            d = new JDialog(ArchitectFrame.getMainInstance(), "Table Profiles");
-            profileManager.createProfiles(tables);
-            JTabbedPane tabPane = new JTabbedPane();
-            JEditorPane editorPane = new JEditorPane();
-            editorPane.setEditable(false);
-            editorPane.setContentType("text/html");
-            ProfileResultFormatter prf = new ProfileResultFormatter();
-            editorPane.setText(prf.format(tables,profileManager) );
-
-            JScrollPane editorScrollPane = new JScrollPane(editorPane);
-            editorScrollPane.setVerticalScrollBarPolicy(
-                            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-            editorScrollPane.setPreferredSize(new Dimension(800, 600));
-            editorScrollPane.setMinimumSize(new Dimension(10, 10));
+            closeAction.putValue(Action.NAME, "Close");
+            final JDefaultButton closeButton = new JDefaultButton(closeAction);
             
-            JPanel htmlPane = new JPanel(new BorderLayout());
-            htmlPane.add(editorScrollPane,BorderLayout.CENTER);
-            ButtonBarBuilder buttonBuilder = new ButtonBarBuilder();
-            JButton save = new JButton(new AbstractAction("Save") {
-            
-                public void actionPerformed(ActionEvent e) {
-
-                    JFileChooser chooser = new JFileChooser();
-                    chooser.addChoosableFileFilter(ASUtils.PDF_FILE_FILTER);
-                    int response = chooser.showSaveDialog(d);
-                    if (response != JFileChooser.APPROVE_OPTION) {
-                        return;
-                    } else {
-                        File file = chooser.getSelectedFile();
-                        if (!file.getPath().endsWith(".pdf")) {
-                            file = new File(file.getPath()+".pdf");
-                        }
-                        if (file.exists()) {
-                            response = JOptionPane.showConfirmDialog(
-                                    d,
-                                    "The file\n\n"+file.getPath()+"\n\nalready exists. Do you want to overwrite it?",
-                                    "File Exists", JOptionPane.YES_NO_OPTION);
-                            if (response == JOptionPane.NO_OPTION) {
-                                actionPerformed(e);
-                                return;
-                            }
-                        }
-                        
-                        final File file2 = new File(file.getPath());
-                        Runnable saveTask = new Runnable() {
-                            public void run() {
-                                List tabList = new ArrayList(tables);
-                                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-                                FileOutputStream PDFFile = null;
-                                try {
-                                    PDFFile = new FileOutputStream(file2);
-                                    new ProfilePDFFormat().createPdf(buffer,tabList,profileManager);
-                                    buffer.writeTo(PDFFile);
-                                } catch (Exception ex) {
-                                    ASUtils.showExceptionDialog(d,"Could not save PDF File", ex);
-                                } finally {
-                                    if ( PDFFile != null ) {
-                                        try {
-                                            PDFFile.close();
-                                        } catch (IOException ex) {
-                                            ASUtils.showExceptionDialog(d,"Could not close PDF File", ex);
-                                        }
-                                    }
-                                }
-                            }
-                        };
-                        new Thread(saveTask).start();
-                    }
-                }
-            
-            });
-            
-            JButton print = new JButton("Print");
-            JButton close = new JButton("close");
-            JButton[] buttonArray = {save,print,close};
-            buttonBuilder.addGriddedButtons(buttonArray);
-            htmlPane.add(buttonBuilder.getPanel(),BorderLayout.SOUTH);
-            tabPane.addTab("Table View", htmlPane );
-            ProfilePanel p = new ProfilePanel(profileManager);
-            tabPane.addTab("Graph View",p);
-            
-            JPanel empty = new JPanel();
-            tabPane.addTab("Profile Explorer",empty);
-            ProfileAction profileAction = new ProfileAction();
-            empty.add(new JButton(profileAction));
-            profileAction.setDBTree(dbTree);
-            profileAction.setProfileManager(profileManager);
-            
-            List<SQLTable> list = new ArrayList(tables);
-            p.setTables(list);
-            p.setChartType(ChartTypes.PIE);
+            final JPanel progressViewPanel = new JPanel(new BorderLayout());
+            final JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+            buttonPanel.add(closeButton);
+            progressViewPanel.add(buttonPanel, BorderLayout.SOUTH);
+            final JProgressBar progressBar = new JProgressBar();
+            progressBar.setPreferredSize(new Dimension(450,20));
+            progressViewPanel.add(progressBar, BorderLayout.CENTER);
+            final JLabel workingOn = new JLabel("Profiling:");
+            progressViewPanel.add(workingOn, BorderLayout.NORTH);
 
             ArchitectPanelBuilder.makeJDialogCancellable(
                     d, new CommonCloseAction(d));
-            d.setContentPane(tabPane);
+            d.getRootPane().setDefaultButton(closeButton);
+            d.setContentPane(progressViewPanel);
             d.pack();
             d.setLocationRelativeTo(ArchitectFrame.getMainInstance());
             d.setVisible(true);
+
+            new ProgressWatcher(progressBar,profileManager,workingOn);
+
+            new Thread( new Runnable() {
+
+                public void run() {
+                    try {
+                        profileManager.createProfiles(tables, workingOn);
+
+
+
+
+                        progressViewPanel.remove(progressBar);
+                        progressBar.setVisible(false);
+                        workingOn.setVisible(false);
+                        
+                        
+                        JTabbedPane tabPane = new JTabbedPane();
+                        JEditorPane editorPane = new JEditorPane();
+                        editorPane.setEditable(false);
+                        editorPane.setContentType("text/html");
+                        ProfileResultFormatter prf = new ProfileResultFormatter();
+                        editorPane.setText(prf.format(tables,profileManager) );
+
+                        JScrollPane editorScrollPane = new JScrollPane(editorPane);
+                        editorScrollPane.setVerticalScrollBarPolicy(
+                                        JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+                        editorScrollPane.setPreferredSize(new Dimension(800, 600));
+                        editorScrollPane.setMinimumSize(new Dimension(10, 10));
+                        
+                        JPanel htmlPane = new JPanel(new BorderLayout());
+                        htmlPane.add(editorScrollPane,BorderLayout.CENTER);
+                        ButtonBarBuilder buttonBuilder = new ButtonBarBuilder();
+                        JButton save = new JButton(new AbstractAction("Save") {
+                        
+                            public void actionPerformed(ActionEvent e) {
+
+                                JFileChooser chooser = new JFileChooser();
+                                chooser.addChoosableFileFilter(ASUtils.PDF_FILE_FILTER);
+                                int response = chooser.showSaveDialog(d);
+                                if (response != JFileChooser.APPROVE_OPTION) {
+                                    return;
+                                } else {
+                                    File file = chooser.getSelectedFile();
+                                    if (!file.getPath().endsWith(".pdf")) {
+                                        file = new File(file.getPath()+".pdf");
+                                    }
+                                    if (file.exists()) {
+                                        response = JOptionPane.showConfirmDialog(
+                                                d,
+                                                "The file\n\n"+file.getPath()+"\n\nalready exists. Do you want to overwrite it?",
+                                                "File Exists", JOptionPane.YES_NO_OPTION);
+                                        if (response == JOptionPane.NO_OPTION) {
+                                            actionPerformed(e);
+                                            return;
+                                        }
+                                    }
+                                    
+                                    final File file2 = new File(file.getPath());
+                                    Runnable saveTask = new Runnable() {
+                                        public void run() {
+                                            List tabList = new ArrayList(tables);
+                                            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                                            FileOutputStream PDFFile = null;
+                                            try {
+                                                PDFFile = new FileOutputStream(file2);
+                                                new ProfilePDFFormat().createPdf(buffer,tabList,profileManager);
+                                                buffer.writeTo(PDFFile);
+                                            } catch (Exception ex) {
+                                                ASUtils.showExceptionDialog(d,"Could not save PDF File", ex);
+                                            } finally {
+                                                if ( PDFFile != null ) {
+                                                    try {
+                                                        PDFFile.close();
+                                                    } catch (IOException ex) {
+                                                        ASUtils.showExceptionDialog(d,"Could not close PDF File", ex);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    };
+                                    new Thread(saveTask).start();
+                                }
+                            }
+                        
+                        });
+                        
+                        JButton print = new JButton("Print");
+                        
+                        JButton[] buttonArray = {save,print,closeButton};
+                        buttonBuilder.addGriddedButtons(buttonArray);
+                        htmlPane.add(buttonBuilder.getPanel(),BorderLayout.SOUTH);
+                        tabPane.addTab("Table View", htmlPane );
+                        ProfilePanel p = new ProfilePanel(profileManager);
+                        tabPane.addTab("Graph View",p);
+                        
+                        JPanel empty = new JPanel();
+                        tabPane.addTab("Profile Explorer",empty);
+                        ProfileAction profileAction = new ProfileAction();
+                        empty.add(new JButton(profileAction));
+                        profileAction.setDBTree(dbTree);
+                        profileAction.setProfileManager(profileManager);
+                        
+                        List<SQLTable> list = new ArrayList(tables);
+                        p.setTables(list);
+                        p.setChartType(ChartTypes.PIE);
+
+                        ArchitectPanelBuilder.makeJDialogCancellable(
+                                d, new CommonCloseAction(d));
+                        d.setContentPane(tabPane);
+                        d.pack();
+                        d.setLocationRelativeTo(ArchitectFrame.getMainInstance());
+                        d.setVisible(true);
+
+                    } catch (SQLException e) {
+                        logger.error("Error in Profile Action ", e);
+                        ASUtils.showExceptionDialogNoReport(dbTree, "Error during profile run", e);
+                    } catch (ArchitectException e) {
+                        logger.error("Error in Profile Action", e);
+                        ASUtils.showExceptionDialog(dbTree, "Error during profile run", e);
+                    }
+                }
+
+            }).start();
+
+            
+   
+
+
+            
     
         } catch (Exception ex) {
             logger.error("Error in Profile Action ", ex);
