3169912eb78798cb0f0f6324d7d0e14b01e357c1
fuerth <fuerth@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-08-16 19:57:19 +0000

    Implemented Comparable and overrode equals and hashCode to match.

--- trunk/src/ca/sqlpower/architect/profile/ProfileResult.java
+++ trunk/src/ca/sqlpower/architect/profile/ProfileResult.java
@@ -1,8 +1,14 @@
 package ca.sqlpower.architect.profile;
 
+import ca.sqlpower.architect.ArchitectUtils;
+import ca.sqlpower.architect.SQLCatalog;
+import ca.sqlpower.architect.SQLColumn;
+import ca.sqlpower.architect.SQLDatabase;
 import ca.sqlpower.architect.SQLObject;
+import ca.sqlpower.architect.SQLSchema;
+import ca.sqlpower.architect.SQLTable;
 
-public abstract class ProfileResult<T extends SQLObject> {
+public abstract class ProfileResult<T extends SQLObject> implements Comparable<ProfileResult> {
 
     private T profiledObject;
     private long createEndTime = -1L;
@@ -58,5 +64,131 @@ public abstract class ProfileResult<T extends SQLObject> {
 
     public void setException(Exception ex) {
         this.ex = ex;
+    }
+
+    /**
+     * Compares this Profile Result based on the following attributes, in the following
+     * priority:
+     * 
+     * <ol>
+     *  <li>The profiled object's database name
+     *  <li>The profiled object's catalog name
+     *  <li>The profiled object's schema name
+     *  <li>The profiled object's table name
+     *  <li>The profiled object's column name
+     *  <li>The profile's createEndTime
+     *  <li>The profile's createStartTime
+     * </ol>
+     * 
+     * If any of those attributes are null or not applicable, they will count as
+     * coming before any non-null value.
+     * 
+     * @param o Another ProfileResult to compare with.
+     * @return -1 if this comes before o; 0 if this and o are the same; 1 if this comes after o.
+     */
+    public final int compareTo(ProfileResult o) {
+        
+        SQLObject po = this.getProfiledObject();
+        SQLObject opo = o.getProfiledObject();
+        if (po == opo) {
+            return 0;
+        }
+
+        int diff;
+        SQLObject so1, so2;
+        
+        // database name
+        so1 = ArchitectUtils.getAncestor(po, SQLDatabase.class);
+        so2 = ArchitectUtils.getAncestor(opo, SQLDatabase.class);
+        if (so1 == null && so2 != null) diff = -1;
+        else if (so1 != null && so2 == null) diff = 1;
+        else if (so1 != null && so2 != null) diff = so1.getName().compareTo(so2.getName());
+        else diff = 0;
+        if (diff != 0) return diff;
+
+        // catalog name
+        so1 = ArchitectUtils.getAncestor(po, SQLCatalog.class);
+        so2 = ArchitectUtils.getAncestor(opo, SQLCatalog.class);
+        if (so1 == null && so2 != null) diff = -1;
+        else if (so1 != null && so2 == null) diff = 1;
+        else if (so1 != null && so2 != null) diff = so1.getName().compareTo(so2.getName());
+        else diff = 0;
+        if (diff != 0) return diff;
+
+        // schema name
+        so1 = ArchitectUtils.getAncestor(po, SQLSchema.class);
+        so2 = ArchitectUtils.getAncestor(opo, SQLSchema.class);
+        if (so1 == null && so2 != null) diff = -1;
+        else if (so1 != null && so2 == null) diff = 1;
+        else if (so1 != null && so2 != null) diff = so1.getName().compareTo(so2.getName());
+        else diff = 0;
+        if (diff != 0) return diff;
+        
+        // table name
+        so1 = ArchitectUtils.getAncestor(po, SQLTable.class);
+        so2 = ArchitectUtils.getAncestor(opo, SQLTable.class);
+        if (so1 == null && so2 != null) diff = -1;
+        else if (so1 != null && so2 == null) diff = 1;
+        else if (so1 != null && so2 != null) diff = so1.getName().compareTo(so2.getName());
+        else diff = 0;
+        if (diff != 0) return diff;
+        
+        // column name
+        so1 = ArchitectUtils.getAncestor(po, SQLColumn.class);
+        so2 = ArchitectUtils.getAncestor(opo, SQLColumn.class);
+        if (so1 == null && so2 != null) diff = -1;
+        else if (so1 != null && so2 == null) diff = 1;
+        else if (so1 != null && so2 != null) diff = so1.getName().compareTo(so2.getName());
+        else diff = 0;
+        if (diff != 0) return diff;
+
+        // profile create date
+        if (this.createEndTime > o.createEndTime) return 1;
+        else if (this.createEndTime < o.createEndTime) return -1;
+
+        if (this.createStartTime > o.createStartTime) return 1;
+        else if (this.createStartTime < o.createStartTime) return -1;
+        else return 0;
+    }
+    
+    /**
+     * Tests for equality with obj.  To be considered equal, obj must be a subtype
+     * of ProfileResult and compareTo(obj) must return 0.
+     */
+    @Override
+    public final boolean equals(Object obj) {
+        ProfileResult o = (ProfileResult) obj;
+        return (compareTo(o) == 0);
+    }
+    
+    /**
+     * Generates a hash code consistent with the equals() method.
+     */
+    @Override
+    public final int hashCode() {
+        int hash = 17;
+        SQLObject po = getProfiledObject();
+        SQLObject so;
+        
+        so = ArchitectUtils.getAncestor(po, SQLDatabase.class);
+        if (so != null) hash *= so.getName().hashCode();
+        
+        so = ArchitectUtils.getAncestor(po, SQLCatalog.class);
+        if (so != null) hash *= so.getName().hashCode();
+
+        so = ArchitectUtils.getAncestor(po, SQLSchema.class);
+        if (so != null) hash *= so.getName().hashCode();
+        
+        so = ArchitectUtils.getAncestor(po, SQLTable.class);
+        if (so != null) hash *= so.getName().hashCode();
+        
+        so = ArchitectUtils.getAncestor(po, SQLColumn.class);
+        if (so != null) hash *= so.getName().hashCode();
+
+        hash *= createEndTime;
+
+        hash *= createStartTime;
+        
+        return hash;
     }
 }
