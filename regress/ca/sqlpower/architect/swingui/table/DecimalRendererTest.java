bcfb5e057ce6ab6ffe2a0af81c5bf6995d1d26c6
johnson <johnson@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-08-23 19:52:59 +0000

    Split ProfileTableCellRenderer into a series of RendererFactory classes. Provide an enum class ProfileColumn listing the column names. This commit includes the Render Factory tests.

---
+++ regress/ca/sqlpower/architect/swingui/table/DecimalRendererTest.java
@@ -0,0 +1,25 @@
+package ca.sqlpower.architect.swingui.table;
+
+import java.math.BigDecimal;
+
+import javax.swing.JLabel;
+
+public class DecimalRendererTest extends BaseRendererTest {
+
+    public void test1() {
+
+        DecimalRendererFactory fmt = new DecimalRendererFactory();
+        JLabel renderer = (JLabel) fmt.getTableCellRendererComponent(table, 1.2345, false, false, 0, 0);
+        String renderedValue = renderer.getText();
+        assertEquals("renderer formatted OK", "1.2", renderedValue);
+
+        renderer = (JLabel) fmt.getTableCellRendererComponent(table, new BigDecimal(111111111.11), false, false, 0, 0);
+        renderedValue = renderer.getText();
+        // This test is not Locale-specific because the format is hard-coded in DecimalRenderer
+        assertEquals("renderer formatted OK", "111,111,111.1", renderedValue);
+
+        renderer = (JLabel) fmt.getTableCellRendererComponent(table, null, false, false, 0, 0);
+        renderedValue = renderer.getText();
+        assertEquals("renderer formatted OK", "null", renderedValue);
+    }
+}
---
+++ regress/ca/sqlpower/architect/swingui/table/PercentRendererTest.java
@@ -0,0 +1,22 @@
+package ca.sqlpower.architect.swingui.table;
+
+import javax.swing.JLabel;
+
+public class PercentRendererTest extends BaseRendererTest {
+
+    public void test1() {
+
+        PercentRendererFactory fmt = new PercentRendererFactory();
+        JLabel renderer = (JLabel) fmt.getTableCellRendererComponent(table, 1.2345, false, false, 0, 0);
+        String renderedValue = renderer.getText();
+        assertEquals("renderer formatted OK", "123%", renderedValue);
+
+        renderer = (JLabel) fmt.getTableCellRendererComponent(table, 0.8777, false, false, 0, 0);
+        renderedValue = renderer.getText();
+        assertEquals("renderer formatted OK", "88%", renderedValue);
+
+        renderer = (JLabel) fmt.getTableCellRendererComponent(table, null, false, false, 0, 0);
+        renderedValue = renderer.getText();
+        assertEquals("renderer formatted OK", "N/A", renderedValue);
+    }
+}
