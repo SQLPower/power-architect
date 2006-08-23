481806ce7b075f85e14d9c1e55ba717c50eee9cf
johnson <johnson@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-08-23 17:10:30 +0000

    Test that the ProfileTableCellRenderer formats the various columns correctly, prior to refactoring that into several subclasses.

--- regress/ca/sqlpower/ArchitectSwingTestSuite.java
+++ regress/ca/sqlpower/ArchitectSwingTestSuite.java
@@ -4,6 +4,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import ca.sqlpower.architect.layout.TestFruchtermanReingoldForceLayout;
+import ca.sqlpower.architect.swingui.ProfileTableCellRenderer;
 import ca.sqlpower.architect.swingui.TestArchitectFrame;
 import ca.sqlpower.architect.swingui.TestAutoLayoutAction;
 import ca.sqlpower.architect.swingui.TestBasicRelationshipUI;
@@ -24,18 +25,19 @@ public class ArchitectSwingTestSuite extends TestCase {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for Architect's Swing GUI");
-		//$JUnit-BEGIN$		
+		//$JUnit-BEGIN$
 		suite.addTestSuite(TestArchitectFrame.class);
 		suite.addTestSuite(TestAutoLayoutAction.class);
 		suite.addTestSuite(TestBasicRelationshipUI.class);
 		suite.addTestSuite(TestDBTree.class);
-		suite.addTestSuite(TestColumnEditPanel.class);		
+		suite.addTestSuite(TestColumnEditPanel.class);
 		suite.addTestSuite(TestDeleteSelectedAction.class);
 		suite.addTestSuite(TestCompareDMPanel.class);
 		suite.addTestSuite(TestFruchtermanReingoldForceLayout.class);
 		suite.addTestSuite(TestRelationship.class);
 		suite.addTestSuite(TestPlayPen.class);
 		suite.addTestSuite(TestPlayPenComponent.class);
+        suite.addTestSuite(ProfileTableCellRenderer.class);
 		suite.addTestSuite(TestSwingUIProject.class);
 		suite.addTestSuite(TestSQLObjectUndoableEventAdapter.class);
 		suite.addTestSuite(TestTableEditPane.class);
