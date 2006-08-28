12bfbc277051ccc48eb49e36ca5d2d7bc942d440
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-08-28 22:09:00 +0000

    Un-static the Formatter objects as these are quasi-singletons anyway (the methods were non-static so there should be no ripple effect). Provide a FormatFactory interface to provide a uniform way of retrieving the Format object from a given RenderFactory so they can be re-used.

--- src/ca/sqlpower/architect/swingui/table/DateRendererFactory.java
+++ src/ca/sqlpower/architect/swingui/table/DateRendererFactory.java
@@ -2,20 +2,28 @@ package ca.sqlpower.architect.swingui.table;
 
 import java.awt.Component;
 import java.text.DateFormat;
+import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
-public class DateRendererFactory extends DefaultTableCellRenderer {
+public class DateRendererFactory extends DefaultTableCellRenderer implements FormatFactory {
 
-    static DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
             int row, int column) {
 
         String formattedValue = df.format(new Date((Long) value));
         return super.getTableCellRendererComponent(table, formattedValue, isSelected, hasFocus, row, column);
+    }
+
+    /* (non-Javadoc)
+     * @see ca.sqlpower.architect.swingui.table.FormatFactory#getFormat()
+     */
+    public Format getFormat() {
+        return df;
     }
 }
--- src/ca/sqlpower/architect/swingui/table/ValueRendererFactory.java
+++ src/ca/sqlpower/architect/swingui/table/ValueRendererFactory.java
@@ -2,22 +2,23 @@ package ca.sqlpower.architect.swingui.table;
 
 import java.awt.Component;
 import java.text.DecimalFormat;
+import java.text.Format;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
 public class ValueRendererFactory extends DefaultTableCellRenderer {
 
-    static DecimalFormat aldf;
+    DecimalFormat aldf;
+
+    public ValueRendererFactory() {
+        aldf = new DecimalFormat("#,##0.0");
+        aldf.setMaximumFractionDigits(1);
+        aldf.setMinimumFractionDigits(0);
+    }
 
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
             int row, int column) {
-
-        if (aldf == null) {
-            aldf = new DecimalFormat("#,##0.0");
-            aldf.setMaximumFractionDigits(1);
-            aldf.setMinimumFractionDigits(0);
-        }
 
         String formattedValue;
 
@@ -29,5 +30,9 @@ public class ValueRendererFactory extends DefaultTableCellRenderer {
             formattedValue = value.toString();
         }
          return super.getTableCellRendererComponent(table, formattedValue, isSelected, hasFocus, row, column);
+    }
+
+    public Format getFormat() {
+        return aldf;
     }
 }
