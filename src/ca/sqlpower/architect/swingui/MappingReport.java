d609037b8ed4eafef2bfb70c4e68277e935ef87d
johnson <johnson@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-08-28 17:43:38 +0000

    fixed the bug1197, visual mapping report throws null pointer exception

--- src/ca/sqlpower/architect/swingui/MappingReport.java
+++ src/ca/sqlpower/architect/swingui/MappingReport.java
@@ -21,11 +21,11 @@ public class MappingReport {
     private int unrelatedSourcesGap = 10;
     private int arrowheadLength = 10;
     private double arrowheadAngle = Math.PI / 6.0;
-    Map<SQLTable, Collection<SQLTable>> mappings; 
+    Map<SQLTable, Collection<SQLTable>> mappings;
     Map<SQLTable, TablePane> panes = new HashMap<SQLTable, TablePane>();
     int maxSourceWidth = 0;
     int maxTargetWidth = 0;
-    
+
     public MappingReport(Collection<SQLTable> targetTables) throws ArchitectException {
         PlayPen pp = new PlayPen();
         mappings = ETLUtils.findTableLevelMappings(targetTables);
@@ -36,14 +36,14 @@ public class MappingReport {
             panes.put(sourceTable, stp);
             maxSourceWidth = Math.max(maxSourceWidth, stp.getPreferredSize().width);
         }
-        
+
         for (SQLTable targetTable : targetTables) {
             TablePane ttp = new TablePane(targetTable, pp);
             panes.put(targetTable, ttp);
             maxTargetWidth = Math.max(maxTargetWidth, ttp.getPreferredSize().width);
         }
     }
-    
+
     public Dimension drawHighLevelReport(Graphics2D g, Dimension pageSize) throws ArchitectException {
 
         int y = 0;
@@ -53,20 +53,21 @@ public class MappingReport {
             if (pageSize != null) {
                 int clusterSize = drawSourceTargetCluster(null, panes, maxSourceWidth, maxTargetWidth, y, st, targets);
                 if ((y % pageSize.height)+(clusterSize%pageSize.height) >= pageSize.height){
-                    y += pageSize.height - (y % pageSize.height); 
+                    y += pageSize.height - (y % pageSize.height);
                 }
             }
             y = drawSourceTargetCluster(g, panes, maxSourceWidth, maxTargetWidth, y, st, targets);
         }
-        return new Dimension(maxSourceWidth + sourceTargetGap + maxTargetWidth, y - unrelatedSourcesGap);
+        return new Dimension(maxSourceWidth + sourceTargetGap + maxTargetWidth, y );
     }
 
     private int drawSourceTargetCluster(Graphics2D g, Map<SQLTable, TablePane> panes, int maxSourceWidth, int maxTargetWidth,  int sy,  SQLTable st, Collection<SQLTable> targets) {
         int sx = 0;
         int tx = maxSourceWidth + sourceTargetGap;
         int ty = sy;
+
         for (SQLTable targetTable : targets) {
-            
+
             TablePane ttp = panes.get(targetTable);
             Dimension tpsize = ttp.getPreferredSize();
             ttp.setBounds(
@@ -77,7 +78,7 @@ public class MappingReport {
                 ttp.paint(g);
                 g.translate(-ttp.getX(), -ttp.getY());
             }
-            ty += ttp.getHeight();
+            ty += ttp.getHeight()+unrelatedSourcesGap;
         }
         int targetsHeight = ty - sy;
         TablePane stp = panes.get(st);
@@ -87,7 +88,7 @@ public class MappingReport {
                     sx + maxSourceWidth/2 - stpsize.width/2, Math.max(sy, sy + targetsHeight/2 - stpsize.height/2),
                     stpsize.width, stpsize.height);
             if (g != null ) {
-                if (g.hitClip(stp.getX(),stp.getY(),stp.getWidth(),stp.getHeight())) {                    
+                if (g.hitClip(stp.getX(),stp.getY(),stp.getWidth(),stp.getHeight())) {
                     g.translate(stp.getX(), stp.getY());
                     stp.paint(g);
                     g.translate(-stp.getX(), -stp.getY());
@@ -98,32 +99,36 @@ public class MappingReport {
                             panes.get(targetTable).getBounds());
                 }
             }
+            sy += unrelatedSourcesGap;
+            sy += stp.getHeight();
         }
-        ty += unrelatedSourcesGap ;
-        sy = Math.max(ty,sy+stp.getHeight()+unrelatedSourcesGap);
+        else {
+            sy += unrelatedSourcesGap;
+        }
+        sy = Math.max(ty, sy);
         return sy;
     }
 
     public Dimension getRequiredSize() throws ArchitectException {
         return drawHighLevelReport(null,null);
     }
-    
+
     private void drawArrow(Graphics2D g, Rectangle from, Rectangle to) {
         GeneralPath arrow = new GeneralPath();
         Point2D.Float p1 = new Point2D.Float(from.width + from.x, from.height/2 + from.y);
         Point2D.Float p2 = new Point2D.Float(to.x, to.y + to.height/2);
-        
+
         float length = (float) Math.sqrt(Math.pow(p2.x-p1.x, 2) + Math.pow(p2.y-p1.y, 2));
-        
+
         // now do everything with respect to p1 and length
-        
+
         Point2D.Float a = new Point2D.Float(p1.x + length - arrowheadLength , (float) (p1.y + arrowheadLength * Math.tan(arrowheadAngle)));
         arrow.moveTo(p1.x, p1.y);
         arrow.lineTo(p1.x + length, p1.y);
         arrow.moveTo(a.x, a.y);
         arrow.lineTo(p1.x + length, p1.y);
         arrow.lineTo(a.x, -(a.y - p1.y) + p1.y);
-        
+
         arrow.transform(AffineTransform.getRotateInstance(Math.atan((p2.y-p1.y)/(p2.x-p1.x)), p1.x, p1.y));
         Stroke backupStroke = g.getStroke();
         g.setStroke(new BasicStroke(2f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER));
