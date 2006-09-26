2e8709de384a27add5d679fa7ac4c7bcdac0e6be
johnson <johnson@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-09-26 20:49:38 +0000

    added getTreeObject to search JTree for given type

--- src/ca/sqlpower/architect/ArchitectUtils.java
+++ src/ca/sqlpower/architect/ArchitectUtils.java
@@ -8,6 +8,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.JTree;
+import javax.swing.tree.TreePath;
+
 import org.apache.log4j.Logger;
 
 import ca.sqlpower.architect.ddl.DB2DDLGenerator;
@@ -20,7 +23,7 @@ import ca.sqlpower.architect.undo.UndoCompoundEventListener;
  * Collection of static utility methods for Architect.
  */
 public class ArchitectUtils {
-	
+
 	private static final Logger logger = Logger.getLogger(ArchitectUtils.class);
     public static final String APP_VERSION_MAJOR = "1";
     public static final String APP_VERSION_MINOR = "1";
@@ -32,7 +35,7 @@ public class ArchitectUtils {
      * The System.currentTimeMillis when this class was loaded.
      */
     private static final long startupTimeMillis = System.currentTimeMillis();
-	
+
 	/**
 	 * This class is just a container for utility routines; you do not
 	 * need to instantiate it.
@@ -40,7 +43,7 @@ public class ArchitectUtils {
 	private ArchitectUtils() {
 		// never gets called
 	}
-	
+
     /**
      * Performs startup tasks for the architect system.  You should call
      * this when starting the Architect.
@@ -48,10 +51,10 @@ public class ArchitectUtils {
     public static void startup() {
         // By virtue of referencing this class, calling this method early in the app's
         // startup sequence will cause the startupTimeMillis to init itself.
-        
+
         // there's nothing else to do, really.
     }
-    
+
 	/**
 	 * Sets up the log4j logging framework.
 	 */
@@ -66,7 +69,7 @@ public class ArchitectUtils {
 			logger.info("Log4J configured successfully");
 		}
 	}
-	
+
 	/**
 	 * Adds listener to source's listener list and all of source's
 	 * children's listener lists recursively.
@@ -81,9 +84,9 @@ public class ArchitectUtils {
 				listenToHierarchy(listener, (SQLObject) it.next());
 			}
 		}
-		
+
 	}
-	
+
 	/**
 	 * Adds listener to source's listener list and all of source's
 	 * children's listener lists recursively.
@@ -98,9 +101,9 @@ public class ArchitectUtils {
 				addUndoListenerToHierarchy(listener, (SQLObject) it.next());
 			}
 		}
-		
+
 	}
-	
+
 	/**
 	 * Calls listenToHierarchy on each element in the sources array.
 	 * Does nothing if sources is null.
@@ -112,7 +115,7 @@ public class ArchitectUtils {
 			addUndoListenerToHierarchy(listener, sources[i]);
 		}
 	}
-	
+
 	/**
 	 * Calls listenToHierarchy on each element in the sources array.
 	 * Does nothing if sources is null.
@@ -124,7 +127,7 @@ public class ArchitectUtils {
 			listenToHierarchy(listener, sources[i]);
 		}
 	}
-	
+
 	/**
 	 * Removes listener from source's listener list and all of source's
 	 * children's listener lists recursively.
@@ -141,7 +144,7 @@ public class ArchitectUtils {
 			}
 		}
 	}
-	
+
 	/**
 	 * Calls unlistenToHierarchy on each element in the sources array.
 	 * Does nothing if sources is null.
@@ -153,7 +156,7 @@ public class ArchitectUtils {
 			undoUnlistenToHierarchy(listener, sources[i]);
 		}
 	}
-	
+
 	/**
 	 * Removes listener from source's listener list and all of source's
 	 * children's listener lists recursively.
@@ -171,7 +174,7 @@ public class ArchitectUtils {
 			}
 		}
 	}
-	
+
 	/**
 	 * Calls unlistenToHierarchy on each element in the sources array.
 	 * Does nothing if sources is null.
@@ -183,11 +186,11 @@ public class ArchitectUtils {
 			unlistenToHierarchy(listener, sources[i]);
 		}
 	}
-	
+
 	/**
 	 * Does a generic object comparison where one or both of the objects could
-	 * be null.  If both objects are null, they are considered equal; if only 
-	 * one is null, they are not equal; otherwise they are compared using 
+	 * be null.  If both objects are null, they are considered equal; if only
+	 * one is null, they are not equal; otherwise they are compared using
 	 * <code>o1.equals(o2)</code>.
 	 */
 	public static boolean areEqual(Object o1, Object o2) {
@@ -195,11 +198,11 @@ public class ArchitectUtils {
 		else if (o1 == null || o2 == null) return false;
 		else return o1.equals(o2);
 	}
-	
+
 	/**
 	 * Searches for all columns in the target database which are marked as having
 	 * source columns in the given source database.
-	 * 
+	 *
 	 * @param target The database to search.  All columns of all tables in this database are searched.
 	 * @param source The database to look for in the target database's columns.
 	 * @return A list of all columns in the target database whose source database is the same
@@ -227,12 +230,12 @@ public class ArchitectUtils {
 		}
 		return matches;
 	}
-	
+
 	/**
 	 * Recursively poke the SQLDatabase until we find at least one leaf node
-	 * (SQLColumn). 
-	 * 
-	 * @param source the source object (usually the database) 
+	 * (SQLColumn).
+	 *
+	 * @param source the source object (usually the database)
 	 */
 	public static boolean pokeDatabase(SQLObject source) throws ArchitectException {
 		if (logger.isDebugEnabled()) logger.debug("HELLO my class is " + source.getClass().getName() + ", my name is + " + source.getName());
@@ -260,13 +263,13 @@ public class ArchitectUtils {
 	/**
 	 * Recursively count tables in the project, including ones that have not been
 	 * expanded in the DBTree.
-	 * 
-	 * @param source the source object (usually the database) 
+	 *
+	 * @param source the source object (usually the database)
 	 */
 	public static int countTablesSnapshot(SQLObject so) throws ArchitectException {
 		if (so instanceof SQLTable) {
 			return 1;
-		} else {			
+		} else {
 			int count = 0;
 			Iterator it = so.getChildren().iterator();
 			while (it.hasNext()) {
@@ -277,16 +280,16 @@ public class ArchitectUtils {
 	}
 
 	/**
-	 * Keep in mind that if you go after anything lower than 
-	 * SQLTable, you will invoke a potentially expensive 
+	 * Keep in mind that if you go after anything lower than
+	 * SQLTable, you will invoke a potentially expensive
 	 * populate() method multiple times.
-	 * 
-	 * @param source the source object (usually the database) 
+	 *
+	 * @param source the source object (usually the database)
 	 */
 	public static List findDescendentsByClass(SQLObject so, java.lang.Class clazz, List list) throws ArchitectException {
 		if (clazz.isAssignableFrom(so.getClass())) {
 			list.add(so);
-		} else {			
+		} else {
 			Iterator it = so.getChildren().iterator();
 			while (it.hasNext()) {
 				findDescendentsByClass((SQLObject) it.next(), clazz, list);
@@ -295,10 +298,10 @@ public class ArchitectUtils {
 		return list;
 	}
 
-	
+
 	/**
 	 * Chop long strings down to size for display purposes
-	 * 
+	 *
 	 * @param s the input string
 	 * @return the truncated string
 	 */
@@ -307,17 +310,17 @@ public class ArchitectUtils {
 			return s;
 		} else {
 			return s.substring(27) + "...";
-		}		
+		}
 	}
-	
+
 	/**
 	 * Recursively count tables in the project, but only consider tables that
 	 * have been expanded.
-	 * 
+	 *
 	 * This might be undercounting a little bit because I think this suppresses
 	 * the Target Database (playpen) entries.
-	 * 
-	 * @param source the source object (usually the database) 
+	 *
+	 * @param source the source object (usually the database)
 	 */
 	public static int countTables(SQLObject so) throws ArchitectException {
 		if (so instanceof SQLTable) {
@@ -333,7 +336,7 @@ public class ArchitectUtils {
 			return myCount;
 		}
 	}
-	
+
 	/**
 	 * Map from JDBC Driver Class name to the template used to create database URLs
 	 * XXX: look thise up from somewhere (i.e. don't hard code them)
@@ -350,13 +353,13 @@ public class ArchitectUtils {
 					"jdbc:db2:<Hostname>");
         drivers.put("org.apache.derby.jdbc.EmbeddedDriver",
                     "jdbc:derby:<Database>;create=true");
-        drivers.put("org.hsqldb.jdbcDriver", 
+        drivers.put("org.hsqldb.jdbcDriver",
                     "jdbc:hsqldb:<Database>");
 		drivers.put("ca.sqlpower.architect.MockJDBCDriver",
 					"jdbc:mock:catalogTerm=<Catalog Term:Catalog>&schemaTerm=<Schema Term:Schema>");
 		return drivers;
 	}
-	
+
 	/**
 	 * Map from driver class name to a short name for the database.
 	 * XXX: look thise up from somewhere (i.e. don't hard code them)
@@ -372,11 +375,11 @@ public class ArchitectUtils {
 		driverSystems.put("ca.sqlpower.architect.MockJDBCDriver", "OTHER");
 		return driverSystems;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * XXX: look thise up from somewhere (i.e. don't hard code them)
-	 * 
+	 *
 	 */
 	public static Map getDriverDDLGeneratorMap () {
 		Map drivers = new HashMap();
@@ -388,18 +391,18 @@ public class ArchitectUtils {
 				PostgresDDLGenerator.class);
 		drivers.put("ibm.sql.DB2Driver",
 				DB2DDLGenerator.class);
-		return drivers;		
+		return drivers;
 	}
-	
+
 	/**
 	 * Replaces double quotes, ampersands, and less-than signs with
 	 * their character reference equivalents.  This makes the returned
-	 * string be safe for use as an XML content data or as an attribute value 
+	 * string be safe for use as an XML content data or as an attribute value
 	 * enclosed in double quotes. From the XML Spec at http://www.w3.org/TR/REC-xml/#sec-predefined-ent:
 	 * 4.6 Predefined Entities
-	 * "Definition: Entity and character references may both be used to escape the left angle bracket, ampersand, 
+	 * "Definition: Entity and character references may both be used to escape the left angle bracket, ampersand,
 	 * and other delimiters. A set of general entities (amp, lt, gt, apos, quot) is specified for this purpose...]
-	 * All XML processors must recognize these entities whether they are declared or not. For interoperability, 
+	 * All XML processors must recognize these entities whether they are declared or not. For interoperability,
 	 * valid XML documents should declare these entities..."
 	 */
 	public static String escapeXML(String src) {
@@ -411,23 +414,23 @@ public class ArchitectUtils {
 			case '\'':
 				sb.append("&apos;");
 				break;
-				
+
 			case '"':
 				sb.append("&quot;");
 				break;
-				
+
 			case '&':
 				sb.append("&amp;");
 				break;
-				
+
 			case '<':
 				sb.append("&lt;");
 				break;
-				
+
 			case '>':
 				sb.append("&gt;");
 				break;
-				
+
 			default:
 				sb.append(ch);
 				break;
@@ -435,13 +438,13 @@ public class ArchitectUtils {
 		}
 		return sb.toString();
 	}
-    
+
       /**
      * Pulls out all SQLTable objects which exist under the start object
      * and adds them to the given list.
-     * 
+     *
      * TODO make this generic so we can ask for any object type
-     * 
+     *
      * @param db The database to extract the tables from
      * @return A flat List consisting of all the SQLTable objects in db.
      * @throws ArchitectException
@@ -466,11 +469,11 @@ public class ArchitectUtils {
     public static long getAppUptime() {
         return System.currentTimeMillis() - startupTimeMillis;
     }
-    
+
     /**
      * Returns the parent database of <tt>so</tt> or <tt>null</tt> if <tt>so</tt>
      * doesn't have an ancestor whose class is <tt>ancestorType</tt>.
-     * 
+     *
      * @param so The object for whose ancestor to look. (Thanks, Winston).
      * @return The nearest ancestor of type ancestorType, or null if no such ancestor exists.
      */
@@ -481,12 +484,12 @@ public class ArchitectUtils {
         }
         return null;
     }
-    
+
     /**
      * returns the tables under the given SQLObject
-     * 
+     *
      */
-    
+
     public static Collection<SQLTable> tablesUnder(SQLObject so) throws ArchitectException {
         List<SQLTable> tables = new ArrayList<SQLTable>();
         if (so instanceof SQLTable) {
@@ -497,5 +500,25 @@ public class ArchitectUtils {
             }
         }
         return tables;
+    }
+
+    /**
+     * Returns the first object of given type on JTree if there is any
+     * on the selected path
+     *
+     * @param so The object for whose ancestor to look. (Thanks, Winston).
+     * @return The nearest ancestor of type ancestorType, or null if no such ancestor exists.
+     */
+    public static <T extends Object> T getTreeObject(JTree tree, Class<T> type) {
+        TreePath [] paths = tree.getSelectionPaths();
+        if ( paths == null || paths.length == 0 )
+            return null;
+        for ( int i=0; i<paths.length; i++ ) {
+            TreePath path = paths[i];
+            for ( Object o : path.getPath() ) {
+                if (o.getClass().equals(type)) return (T) o;
+            }
+        }
+        return null;
     }
 }
