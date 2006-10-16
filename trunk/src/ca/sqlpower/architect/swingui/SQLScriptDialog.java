0a5ca701b87e06c9b53959c4bb2dd32778c8c35c
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-10-16 22:27:42 +0000

    Guard against null-parented modal dialogs.

--- trunk/src/ca/sqlpower/architect/swingui/SQLScriptDialog.java
+++ trunk/src/ca/sqlpower/architect/swingui/SQLScriptDialog.java
@@ -58,7 +58,7 @@ import com.jgoodies.forms.layout.FormLayout;
 public class SQLScriptDialog extends JDialog {
 
 	private static final Logger logger = Logger.getLogger(SQLScriptDialog.class);
-	
+
 	private List<DDLStatement> statements;
 	private ArrayList <LabelValueBean> statementResultList;
 	private JProgressBar progressBar = new JProgressBar();
@@ -69,7 +69,7 @@ public class SQLScriptDialog extends JDialog {
 	private JLabel statusLabel;
 
 	private ArchitectDataSource targetDataSource;
-	
+
 	private JTextPane sqlScriptArea;
 	private AbstractDocument sqlDoc;
 
@@ -82,6 +82,10 @@ public class SQLScriptDialog extends JDialog {
 			boolean closeParent )
 			throws HeadlessException {
 		super(owner, title, modal);
+        if (modal && owner == null) {
+            JOptionPane.showMessageDialog(null,
+                "Debug: This action tried to create a null-parented modal dialog");
+        }
 		statementResultList = new ArrayList();
 		statusLabel = new JLabel();
 		parent = owner;
@@ -91,7 +95,7 @@ public class SQLScriptDialog extends JDialog {
 		this.closeParent = closeParent;
 		add(buildPanel());
 		pack();
-		setLocationRelativeTo(parent);		
+		setLocationRelativeTo(parent);
 	}
 
 	public SQLScriptDialog(Dialog owner, String title, String header, boolean modal,
@@ -99,6 +103,10 @@ public class SQLScriptDialog extends JDialog {
 			boolean closeParent )
 			throws HeadlessException {
 		super(owner, title, modal);
+        if (modal && owner == null) {
+            JOptionPane.showMessageDialog(null,
+                "Debug: This action tried to create a null-parented modal dialog");
+        }
 		statementResultList = new ArrayList();
 		statusLabel = new JLabel();
 		parent = owner;
@@ -115,15 +123,15 @@ public class SQLScriptDialog extends JDialog {
 	private JPanel buildPanel() {
 		FormLayout sqlLayout = new FormLayout(
 				"4dlu, min:grow, 4dlu", //columns
-				"pref, 4dlu, pref, 6dlu, fill:300dlu:grow,6dlu, pref, 6dlu, pref"); //rows		
-		
+				"pref, 4dlu, pref, 6dlu, fill:300dlu:grow,6dlu, pref, 6dlu, pref"); //rows
+
 		CellConstraints cc = new CellConstraints();
-		
+
 		sqlDoc = new DefaultStyledDocument();
-		
+
 		SimpleAttributeSet att = new SimpleAttributeSet();
 		StyleConstants.setForeground(att, Color.black);
-		
+
 		for (DDLStatement ddl : statements){
 			try {
 				sqlDoc.insertString(sqlDoc.getLength(), ddl.getSQLText()+ddl.getSqlTerminator(), att);
@@ -135,72 +143,71 @@ public class SQLScriptDialog extends JDialog {
 		}
 		sqlScriptArea = new JTextPane();
 		sqlScriptArea.setMargin(new Insets(6, 10, 4, 6));
-		sqlScriptArea.setDocument(sqlDoc);		
+		sqlScriptArea.setDocument(sqlDoc);
 		sqlScriptArea.setEditable(false);
 		sqlScriptArea.setAutoscrolls(true);
 		JScrollPane sp = new JScrollPane(sqlScriptArea);
-		
+
 		Action copy = new CopyAction(sqlDoc);
-		Action execute = null;		
-		
+		Action execute = null;
+
 		if ( targetDataSource != null ) {
 			execute = new AbstractAction(){
 				public void actionPerformed(ActionEvent e) {
 					new Thread(executeTask).start();
-				}			
+				}
 			};
 		}
-		
+
 		Action save = new AbstractAction() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				logger.info( "SQL_FILE_FILTER:"+ ((FileExtensionFilter) ASUtils.SQL_FILE_FILTER).toString());
-				
+
 				new SaveDocument(parent,sqlDoc,
-						(FileExtensionFilter) ASUtils.SQL_FILE_FILTER );				
+						(FileExtensionFilter) ASUtils.SQL_FILE_FILTER );
 			}
 		};
 		CloseAction close = new CloseAction();
 		close.setWhatToClose(this);
-		
-		
+
+
 		ButtonBarBuilder barBuilder = new ButtonBarBuilder();
 		JButton copyButton = new JButton(copy);
 		copyButton.setText("Copy");
 		barBuilder.addGridded (copyButton);
 		barBuilder.addRelatedGap();
 		barBuilder.addGlue();
-		
-		
+
 		JButton executeButton = new JButton(execute);
 		executeButton.setText("Execute");
-		barBuilder.addGridded(executeButton);		
+		barBuilder.addGridded(executeButton);
 		barBuilder.addRelatedGap();
 		barBuilder.addGlue();
 
 		if ( execute == null ) {
 			executeButton.setEnabled(false);
 		}
-		
+
 		JButton saveButton = new JButton(save);
 		saveButton.setText("Save");
 		barBuilder.addGridded(saveButton);
 		barBuilder.addRelatedGap();
-		barBuilder.addGlue();		
-		
+		barBuilder.addGlue();
+
 		JButton closeButton = new JButton(close);
 		closeButton.setText("Close");
 		barBuilder.addGridded(closeButton);
-		
+
 		PanelBuilder pb;
 
 		JPanel panel = logger.isDebugEnabled() ? new FormDebugPanel(sqlLayout) : new JPanel(sqlLayout);
-		pb = new PanelBuilder(sqlLayout, panel);			
-		pb.setDefaultDialogBorder();	
+		pb = new PanelBuilder(sqlLayout, panel);
+		pb.setDefaultDialogBorder();
 		pb.add(new JLabel(header), cc.xy(2, 1));
-						
+
 		if (targetDataSource != null) {
-			pb.add(new JLabel("Your Target Database is "+ targetDataSource.getName() ), cc.xy(2, 3));			
+			pb.add(new JLabel("Your Target Database is "+ targetDataSource.getName() ), cc.xy(2, 3));
 		}
 		pb.add(sp, cc.xy(2, 5));
     		pb.add(barBuilder.getPanel(), cc.xy(2, 7, "c,c"));
@@ -208,71 +215,71 @@ public class SQLScriptDialog extends JDialog {
 
 		return pb.getPanel();
 	}
-	
+
 	public MonitorableWorker getExecuteTask() {
 		return executeTask;
 	}
-	
+
 	/**
 	 * Changes the task that will be invoked by the "execute" button on this
 	 * dialog.  If you want your task to run before the normal script execution,
 	 * you should call {@link #getExecuteTask()} and chain that task onto the
 	 * one you specify here.
-	 * 
+	 *
 	 * @param v The task to execute when the "execute" button is clicked.
 	 */
 	public void setExecuteTask(MonitorableWorker v) {
 		executeTask = v;
 	}
-	
+
 	// ============== Nested classes follow ================
-	
+
 	private class CopyAction extends AbstractAction {
 
 		AbstractDocument doc;
 		public CopyAction(AbstractDocument doc) {
 			this.doc = doc;
 		}
-		
+
 		public void actionPerformed(ActionEvent e) {
-			
+
 			try {
 				StringSelection selection = new StringSelection(doc.getText(0,doc.getLength()));
 				Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection,selection);
 			} catch (BadLocationException e1) {
 				logger.debug("Unable to get the text for copying"+ e1);
 			}
-			
-		}			
+
+		}
 	}
-	
-	public class CloseAction extends AbstractAction {	
+
+	public class CloseAction extends AbstractAction {
 
 		Component c;
-		
+
 		public void setWhatToClose(Component c){
-			this.c = c;				
+			this.c = c;
 		}
 		public void actionPerformed(ActionEvent e) {
 			c.setVisible(false);
 			if ( closeParent )
 				parent.setVisible(false);
-		}						
+		}
 	}
-	
+
 	private class ExecuteSQLScriptWorker extends MonitorableWorker {
-		
-		
+
+
 		private int stmtsTried = 0;
 		private int stmtsCompleted = 0;
 		private boolean finished = false;
 		private boolean hasStarted = false;
-		
+
 		/**
 		 * This method runs on a separate worker thread.
 		 */
 		public void doStuff() {
-			
+
 			finished = false;
 
 			hasStarted = true;
@@ -284,12 +291,12 @@ public class SQLScriptDialog extends JDialog {
 			//ProgressWatcher pw = new ProgressWatcher(progressBar, this, statusLabel);
 			stmtsTried = 0;
 			stmtsCompleted = 0;
-			
+
 			logger.debug("the Target Database is: " + target.getDataSource());
 
 			Connection con;
 			Statement stmt;
-			
+
 			try {
 				con = target.getConnection();
 			} catch (ArchitectException ex) {
@@ -302,9 +309,9 @@ public class SQLScriptDialog extends JDialog {
 				finished = true;
 				logger.error("Unexpected exception in DDL generation", ex);
 				throw new RuntimeException("You have to specify a target database connection"
-				+"\nbefore executing this script.");				
+				+"\nbefore executing this script.");
 			}
-			
+
 			try {
 				logger.debug("the connection thinks it is: " + con.getMetaData().getURL());
 				stmt = con.createStatement();
@@ -312,20 +319,20 @@ public class SQLScriptDialog extends JDialog {
 				finished = true;
 				throw new RuntimeException("Couldn't generate DDL statements: "
 						+ex.getMessage()+"\nThe problem was reported by " +
-								"the target database.");								
+								"the target database.");
 			}
-			
+
 			LogWriter logWriter = null;
-			
+
 			try {
 				logWriter = new LogWriter(ArchitectSession.getInstance().getUserSettings().getDDLUserSettings().getString(DDLUserSettings.PROP_DDL_LOG_PATH,""));
 			} catch (ArchitectException ex) {
 				finished = true;
 				final Exception fex = ex;
 				throw new RuntimeException("A problem with the DDL log file " +
-					"prevented\n DDL generation from running:\n\n"+fex.getMessage());								
+					"prevented\n DDL generation from running:\n\n"+fex.getMessage());
 			}
-			
+
 			try {
 				logWriter.info("Starting DDL Generation at " + new java.util.Date(System.currentTimeMillis()));
 				logWriter.info("Database Target: " + target.getDataSource());
@@ -337,18 +344,18 @@ public class SQLScriptDialog extends JDialog {
 					String status = "Unknown";
 					try {
 						stmtsTried++;
-						logWriter.info("executing: " + ddlStmt.getSQLText());					
+						logWriter.info("executing: " + ddlStmt.getSQLText());
 						stmt.executeUpdate(ddlStmt.getSQLText());
 						stmtsCompleted++;
 						status = "OK";
 					} catch (SQLException ex) {
 						final Exception fex = ex;
 						final String fsql = ddlStmt.getSQLText();
-						final LogWriter fLogWriter = logWriter; 
+						final LogWriter fLogWriter = logWriter;
 						logWriter.info("sql statement failed: " + ex.getMessage());
 						status = "SQL statement failed: " + ex.getMessage();
 						try {
-							SwingUtilities.invokeAndWait(new Runnable() {						
+							SwingUtilities.invokeAndWait(new Runnable() {
 								public void run() {
 									JTextArea jta = new JTextArea(fsql,25,40);
 									jta.setEditable(false);
@@ -377,10 +384,10 @@ public class SQLScriptDialog extends JDialog {
 							status = "Worker thread died: "
 									+fex2.getMessage();
 							throw new RuntimeException("Worker thread died: "
-									+fex2.getMessage());							
-						} 
+									+fex2.getMessage());
+						}
 
-						
+
 						if (isCanceled()) {
 							finished = true;
 							// don't return, we might as well display how many statements ended up being processed...
@@ -389,10 +396,10 @@ public class SQLScriptDialog extends JDialog {
 					finally {
 						statementResultList.add(new LabelValueBean(
 								ddlStmt.toString(),status));
-						
+
 					}
 				}
-				
+
 			} catch (Exception exc){
 				logWriter.info("Caught Unexpected Exception " + exc);
 				ASUtils.showExceptionDialog(
@@ -400,8 +407,8 @@ public class SQLScriptDialog extends JDialog {
 						"Couldn't finish running this SQL Script due to the following unexpected exception:",
 						exc);
 			} finally {
-				final String resultsMessage = 
-					(stmtsCompleted == 0 ? "Did not execute any out of " : 
+				final String resultsMessage =
+					(stmtsCompleted == 0 ? "Did not execute any out of " :
 						"Successfully executed " + stmtsCompleted + " out of ") +
 					stmtsTried + " statements.";
 				logWriter.info(resultsMessage);
@@ -420,11 +427,11 @@ public class SQLScriptDialog extends JDialog {
 					logger.error("Couldn't close connection", ex);
 				}
 			}
-			
+
 			finished = true;
-			
+
 		}
-		
+
 		/**
 		 * Displays error messages or invokes the next process in the chain on a new
 		 * thread. The run method asks swing to invoke this method on the event dispatch
@@ -433,46 +440,46 @@ public class SQLScriptDialog extends JDialog {
 		public void cleanup() {
 
 		}
-		
-	
-		
+
+
+
 		// ============= Monitorable Interface =============
-		
-		public Integer getJobSize() throws ArchitectException {			
+
+		public Integer getJobSize() throws ArchitectException {
 			if (statements != null) {
 				return new Integer(statements.size());
 			} else {
 				return null;
 			}
 		}
-		
+
 		public int getProgress() throws ArchitectException {
-			return stmtsTried;			
+			return stmtsTried;
 		}
-		
+
 		public boolean isFinished() throws ArchitectException {
 			return finished;
 		}
-		
+
 		public String getMessage() {
 			return null;
 		}
-		
+
 		public void cancelJob() {
 			this.setCancelled(true);
 			finished = true;
 		}
-		
-			
+
+
 		public boolean hasStarted() {
 			return hasStarted;
 		}
-		
+
 		public void setHasStarted(boolean hasStarted) {
 			this.hasStarted = hasStarted;
 		}
 
-	
+
 	}
 
 	public ArrayList<LabelValueBean> getStatementResultList() {
@@ -481,5 +488,5 @@ public class SQLScriptDialog extends JDialog {
 
 	public void setStatementResultList(ArrayList<LabelValueBean> statementResultList) {
 		this.statementResultList = statementResultList;
-	}	
+	}
 }
