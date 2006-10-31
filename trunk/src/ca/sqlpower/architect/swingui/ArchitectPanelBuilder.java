65a265f260cdc327381ec849a5620af65475500c
benjamin <benjamin@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-10-31 17:19:50 +0000

    Added in a static makeOwnedDialog method that tries to create a JDialog that is own by the parent component.

--- trunk/src/ca/sqlpower/architect/swingui/ArchitectPanelBuilder.java
+++ trunk/src/ca/sqlpower/architect/swingui/ArchitectPanelBuilder.java
@@ -1,6 +1,7 @@
 package ca.sqlpower.architect.swingui;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dialog;
 import java.awt.Frame;
 import java.awt.Window;
@@ -17,6 +18,7 @@ import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
 
 import org.apache.log4j.Logger;
 
@@ -27,6 +29,33 @@ public class ArchitectPanelBuilder {
 	static Logger logger = Logger.getLogger(ArchitectPanelBuilder.class);
 	public static final String OK_BUTTON_LABEL = "OK";
 	public static final String CANCEL_BUTTON_LABEL = "Cancel";
+    
+    /**
+     * Tries very hard to create a JDialog which is owned by the parent
+     * Window of the given component.  However, if the component does not
+     * have a Window ancestor, or the component has a Window ancestor that
+     * is not a Frame or Dialog, this method instead creates an unparented
+     * JDialog which is always-on-top.
+     *
+     * @param owningComponent The component that should own this dialog.
+     * @param title The title for the dialog.
+     * @return A JDialog that is
+     * @author Jonathan Fuerth (donated to SQL Power on October 31, 2006)
+     */
+    public static JDialog makeOwnedDialog(Component owningComponent,
+            String title) {
+        Window owner = SwingUtilities.getWindowAncestor(owningComponent);
+        if (owner instanceof Frame) {
+            return new JDialog((Frame) owner, title);
+        } else if (owner instanceof Dialog) {
+            return new JDialog((Dialog) owner, title);
+        } else {
+            JDialog d = new JDialog();
+            d.setTitle(title);
+            d.setAlwaysOnTop(true);
+            return d;
+        }
+    }
 
 	/**
 	 * Build a JDialog around an object that implements ArchitectPanel, to
@@ -292,4 +321,6 @@ public class ArchitectPanelBuilder {
         d.pack();
         return d;
     }
+    
+
 }
