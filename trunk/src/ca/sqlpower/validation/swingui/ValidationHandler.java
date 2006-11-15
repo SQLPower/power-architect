966ed64813c11a413e2ede4f2674365ea95dc98c
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-11-15 22:53:53 +0000

    Add missing javadoc.

--- trunk/src/ca/sqlpower/validation/swingui/ValidationHandler.java
+++ trunk/src/ca/sqlpower/validation/swingui/ValidationHandler.java
@@ -2,6 +2,16 @@ package ca.sqlpower.validation.swingui;
 
 import ca.sqlpower.validation.Validator;
 
+/**
+ * A ValidationHandler is a non-visual component that ties a given
+ * Validator (which in turn probably depends on a given
+ * JComponent, e.g., a JTextComponent) into the StatusComponent;
+ * upon events from the JComponent (such as KeyEvents
+ * for the TextComponentValidationHandler) the ValidationHandler
+ * invokes the Validator's validate() method and updates the
+ * JComponent's appearance accordingly.
+ * @see ca.sqlpower.validation.TextComponentValidationHandler
+ */
 public abstract class ValidationHandler {
     Validator validator;
     StatusComponent statusComponent;
