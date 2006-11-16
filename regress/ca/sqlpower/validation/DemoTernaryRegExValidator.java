382a85271438454aaa77586a9d99daa050ee827d
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-11-16 00:07:58 +0000

    Demonstrate the "Termary" matching!

---
+++ regress/ca/sqlpower/validation/DemoTernaryRegExValidator.java
@@ -0,0 +1,41 @@
+package ca.sqlpower.validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * A non-general demo Validator that uses RegEx matching and
+ * looks for strings like Pass, Warn or Fail.
+ */
+public class DemoTernaryRegExValidator implements Validator {
+
+    private Pattern pattern;
+    private String message;
+
+    /**
+     * Construct a Validator for regexes
+     * @param pattern The regex pattern
+     *
+     */
+    public DemoTernaryRegExValidator() {
+        super();
+        String pattern = "^(OK|WARN|FAIL)$";
+        this.pattern = Pattern.compile(pattern);
+        this.message = "Must match " + pattern;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Status validate(Object contents) {
+        String value = (String)contents;
+        Matcher matcher = pattern.matcher(value);
+        if (matcher.matches()) {
+            String match = matcher.group(1);
+            return Status.valueOf(match.toUpperCase());
+        } else
+            return Status.FAIL;
+    }
+
+}
