d2e381f349a657bc15b8479c3aac47c6b8036294
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2006-12-05 22:02:33 +0000

    reformatted to local conventions

--- src/ca/sqlpower/architect/layout/AbstractLayout.java
+++ src/ca/sqlpower/architect/layout/AbstractLayout.java
@@ -10,45 +10,40 @@ import ca.sqlpower.architect.swingui.TablePane;
 
 public abstract class AbstractLayout implements ArchitectLayoutInterface {
 
-	protected Rectangle frame;
-	
-	public void setup(List<TablePane> nodes, List<Relationship> edges, Rectangle rect) {
-		frame = rect;
-		
-	}
-	
-	HashMap<String,Object> properties;
-	
-	protected AbstractLayout()
-	{
-		properties = new HashMap<String,Object>();
-	}
-	
-	public void setProperty(String key, Object value) {
-		properties.put(key,value);
+    protected Rectangle frame;
 
-	}
+    public void setup(List<TablePane> nodes, List<Relationship> edges, Rectangle rect) {
+        frame = rect;
+    }
 
-	public Object getProperty(String key) {
-		return properties.get(key);
-	}
-	
-	public Dimension getNewArea(List<TablePane> nodes) {
-		Dimension d = new Dimension();
-        long area=0;
-		for (TablePane tp : nodes) {
-			Rectangle b = tp.getBounds();
-            area += b.width *b.height;
-		}
+    HashMap<String, Object> properties;
+
+    protected AbstractLayout() {
+        properties = new HashMap<String, Object>();
+    }
+
+    public void setProperty(String key, Object value) {
+        properties.put(key, value);
+
+    }
+
+    public Object getProperty(String key) {
+        return properties.get(key);
+    }
+
+    public Dimension getNewArea(List<TablePane> nodes) {
+        Dimension d = new Dimension();
+        long area = 0;
+        for (TablePane tp : nodes) {
+            Rectangle b = tp.getBounds();
+            area += b.width * b.height;
+        }
 
         final double areaFudgeFactor = 16.0;
-        double newWidth = Math.sqrt( (11.0/8.5) * area * areaFudgeFactor );
-        double newHeight = (8.5/11.0) * newWidth;
-		d.setSize((int) newWidth, (int) newHeight);
-		return d;
-	}
+        double newWidth = Math.sqrt((11.0 / 8.5) * area * areaFudgeFactor);
+        double newHeight = (8.5 / 11.0) * newWidth;
+        d.setSize((int) newWidth, (int) newHeight);
+        return d;
+    }
 
-	
-	
-	
 }
