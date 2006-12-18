1331529915a5e919a0ca2ac73e3072b356fe1dee
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2006-12-18 19:25:02 +0000

    Stop the Show/Hide details button in the showExceptionDialog from jumping around when you show/hide the details.  Add a second demo in its Demo program.

--- trunk/regress/ca/sqlpower/architect/swingui/ASUtilsDisplayDialogDemo.java
+++ trunk/regress/ca/sqlpower/architect/swingui/ASUtilsDisplayDialogDemo.java
@@ -1,8 +1,12 @@
 package ca.sqlpower.architect.swingui;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.sql.SQLException;
 
-import javax.swing.*;
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class ASUtilsDisplayDialogDemo {
 
@@ -12,16 +16,27 @@ public class ASUtilsDisplayDialogDemo {
     public static void main(String[] args) {
         final JFrame jf = new JFrame("test");
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        AbstractAction action = new AbstractAction("Test Now") {
+        jf.setLayout(new GridLayout(0, 1));
+        AbstractAction action = new AbstractAction("Test ISE") {
 
             public void actionPerformed(ActionEvent e) {
                 Throwable t = new IllegalStateException("Moo cow");
-                ASUtils.showExceptionDialogNoReport(jf, "Something went terribly wrong", t);
+                ASUtils.showExceptionDialogNoReport(jf, "Your cow mooed one too many times", t);
             }
         };
-        JButton button = new JButton(action);
-        jf.add(button);
-        jf.setBounds(200, 200, 200, 200);
+        jf.add(new JButton(action));
+
+        action = new AbstractAction("Test SQL") {
+
+            public void actionPerformed(ActionEvent e) {
+                Throwable t = new SQLException("Db42 Error detected");
+                ASUtils.showExceptionDialogNoReport(jf, "Your database doesn't like you!\nLine 2", t);
+            }
+        };
+        jf.add(new JButton(action));
+
+        jf.pack();
+        jf.setLocation(400, 300);
         jf.setVisible(true);
     }
 
