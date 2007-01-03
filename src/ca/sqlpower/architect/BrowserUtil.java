94abddaf8b5fa4387758924bff10b48b5c0724d2
johnson <johnson@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2007-01-03 20:40:08 +0000

    works on windows. print any error message coming out.

--- src/ca/sqlpower/architect/BrowserUtil.java
+++ src/ca/sqlpower/architect/BrowserUtil.java
@@ -1,29 +1,43 @@
 package ca.sqlpower.architect;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.InputStream;
 
 public class BrowserUtil {
 
     final static String OS_NAME = System.getProperty("os.name");
     final static String OS_VER = System.getProperty("os.version");
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        System.out.println(OS_NAME + OS_VER);
-        launch(new URI("http://www.sqlpower.ca/"));
+    public static void main(String[] args) throws IOException {
+        System.out.println(OS_NAME + " " + OS_VER);
+        launch("http://www.sqlpower.ca/");
     }
 
-    /** Launches the default browser to display a URI.
-     * @throws IOException */
-    public static void launch(URI uri) throws IOException {
+    /** Launches the default browser to display a URL.
+     * @throws IOException 
+     */
+    public static void launch(String uri) throws IOException {
         Runtime runtime = Runtime.getRuntime();
-        if (OS_NAME.equals("windows")) {
-            runtime.exec(uri.toString());
+        Process p = null;
+        if (OS_NAME.contains("Windows")) {
+            p = runtime.exec("cmd /C \"start " + uri + "\"");
         } else if (OS_NAME.equals("macos")) {
-            runtime.exec("open " + uri);
+            p = runtime.exec("open " + uri);
         } else {
-            runtime.exec("mozilla " + uri);
+            p = runtime.exec("mozilla " + uri);
         }
+        try {
+            p.waitFor();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Problem waiting for browser", e);
+        }
+        final InputStream errorStream = p.getErrorStream();
+        int c = -1;
+        do {
+            if (errorStream.available() > 0) {
+                c = errorStream.read();
+                System.out.print((char)c);
+            }
+        } while (c != -1);
     }
 }
