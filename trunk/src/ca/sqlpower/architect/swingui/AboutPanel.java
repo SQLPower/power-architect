ed6a19e35330e924ad6acae572341684d18ef33f
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2007-02-05 17:34:15 +0000

    Add a JTabbedPane to the About dialog; the first entry is System.properties, but there will be additional tabs later (Credits?). OK jonathan.

--- trunk/src/ca/sqlpower/architect/swingui/AboutPanel.java
+++ trunk/src/ca/sqlpower/architect/swingui/AboutPanel.java
@@ -1,10 +1,17 @@
 package ca.sqlpower.architect.swingui;
 
 import java.awt.FlowLayout;
+import java.util.Arrays;
+import java.util.Properties;
 
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 
 import ca.sqlpower.architect.ArchitectUtils;
 
@@ -13,11 +20,17 @@ public class AboutPanel extends JPanel implements ArchitectPanel {
 	public JLabel content;
 
 	public AboutPanel() {
-		initComponents();
+        JTabbedPane tabs = new JTabbedPane();
+        tabs.add("About", initAboutTab());
+        tabs.add("System Properties", new JScrollPane(initSysPropsTab()));
+
+        tabs.setSelectedIndex(0);
+        add(tabs);
 	}
 
-	public void initComponents() {
-		setLayout(new FlowLayout());
+    private JComponent initAboutTab() {
+        JPanel pan = new JPanel();
+		pan.setLayout(new FlowLayout());
 
         // Include the Power*Architect Icon!
         String realPath = "/icons/architect128.png";
@@ -25,23 +38,76 @@ public class AboutPanel extends JPanel implements ArchitectPanel {
 
         if (imgURL != null) {
             ImageIcon imageIcon = new ImageIcon(imgURL, "Architect Logo");
-            add(new JLabel(imageIcon));
+            pan.add(new JLabel(imageIcon));
         }
 
 		content = new JLabel("<html>Power*Architect "+
 		                    ArchitectUtils.APP_VERSION+"<br><br>" +
 							"Copyright 2003-2006 SQL Power Group Inc.<br>" +
 							"</html>");
-		add(content);
+		pan.add(content);
+        return pan;
 	}
+
+    /**
+     * A JTable model to display the system properties
+     */
+    private class SystemPropertiesTableModel extends AbstractTableModel {
+        private Properties props = System.getProperties();
+        private String[] keys;
+
+        SystemPropertiesTableModel() {
+            keys = props.keySet().toArray(new String[0]);
+            Arrays.sort(keys);
+        }
+
+        public int getRowCount() {
+            return keys.length;
+        }
+
+        public int getColumnCount() {
+            return 2;
+        }
+
+        public Object getValueAt(int row, int column) {
+            switch(column) {
+            case 0: return keys[row];
+            case 1: return props.get(keys[row]);
+            default: throw new IllegalArgumentException("Column count");
+            }
+        }
+
+        @Override
+        public String getColumnName(int column) {
+            switch(column) {
+            case 0: return "Property";
+            case 1: return "Value";
+            default: throw new IllegalArgumentException("Column count");
+            }
+        }
+    }
+
+    private JComponent initSysPropsTab() {
+        JTable table = new JTable(new SystemPropertiesTableModel());
+
+        // tailor column 1 width
+        table.getColumnModel().getColumn(0).setMinWidth(200);
+
+        // Want column 2 wide enough to show CLASSPATH
+        table.getColumnModel().getColumn(1).setMinWidth(2000);
+
+        table.setSize(table.getPreferredSize());
+
+        return table;
+    }
 
 	public boolean applyChanges() {
 		return true;
-        // nothing to apply
+        // read-only, nothing to apply
 	}
 
 	public void discardChanges() {
-        // nothing to discard
+        // read-only, nothing to discard
 	}
 
 	public JPanel getPanel() {
