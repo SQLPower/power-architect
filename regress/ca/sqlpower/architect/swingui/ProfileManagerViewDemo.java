46d6dc72c654fc8b473ffb51034930056d4dd266
ian <ian@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2007-03-01 22:46:34 +0000

    Add more mock methods, and mock up some of the listener methods too.

--- regress/ca/sqlpower/architect/swingui/ProfileManagerViewDemo.java
+++ regress/ca/sqlpower/architect/swingui/ProfileManagerViewDemo.java
@@ -5,24 +5,31 @@ import java.util.List;
 
 import javax.swing.*;
 
+import ca.sqlpower.architect.ArchitectException;
 import ca.sqlpower.architect.SQLTable;
+import ca.sqlpower.architect.profile.ProfileChangeEvent;
 import ca.sqlpower.architect.profile.ProfileManagerInterface;
 import ca.sqlpower.architect.profile.TableProfileResult;
 
 public class ProfileManagerViewDemo {
 
+    static ProfileManagerView view;
+    static SQLTable mockTable;
+    static TableProfileResult tableProfileResult;
+
     /**
      * @param args
+     * @throws Exception
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         JFrame jf = new JFrame("ProfileManagerViewDemo");
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         final List<TableProfileResult> mockData = new ArrayList<TableProfileResult>();
 
-        SQLTable mockTable = new SQLTable();
+        mockTable = new SQLTable();
         mockTable.setName("Customers");
-        TableProfileResult tableProfileResult = new TableProfileResult(mockTable) {
+        tableProfileResult = new TableProfileResult(mockTable) {
             @Override
             public boolean isFinished() {
                 return true;
@@ -30,6 +37,7 @@ public class ProfileManagerViewDemo {
         };
         tableProfileResult.setCreateStartTime(System.currentTimeMillis());
         mockData.add(tableProfileResult);
+
         mockTable = new SQLTable();
         mockTable.setName("Suppliers");
         tableProfileResult = new TableProfileResult(mockTable);
@@ -41,11 +49,32 @@ public class ProfileManagerViewDemo {
             public List<TableProfileResult> getTableResults() {
                 return mockData;
             }
+
+            public void remove(TableProfileResult victim) throws ArchitectException {
+                System.out.println("Mock Manager asked to remove " + victim);
+                mockData.remove(victim);
+                view.profileRemoved(new ProfileChangeEvent(mockTable, tableProfileResult));
+            }
+
+            public void clear() {
+                System.out.println("Mock Manager asked to clear");
+                mockData.clear();
+            }
         };
 
-        jf.setContentPane(new ProfileManagerView(mock));
+        view = new ProfileManagerView(mock);
+        jf.setContentPane(view);
         jf.pack();
         jf.setVisible(true);
+
+        Thread.sleep(4000);
+        System.out.println("Adding another table");
+        mockTable = new SQLTable();
+        mockTable.setName("Yet Another Table");
+        tableProfileResult = new TableProfileResult(mockTable);
+        tableProfileResult.setCreateStartTime(System.currentTimeMillis());
+
+        view.profileAdded(new ProfileChangeEvent(mockTable, tableProfileResult));
     }
 
 }
