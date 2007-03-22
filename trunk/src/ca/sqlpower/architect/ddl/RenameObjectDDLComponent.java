6477ab84ae30cad029b0d5bdc20fded4e0c9a6d9
ryan <ryan@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2007-03-22 21:10:56 +0000

    In the ddl warnings dialog you can now change the names of conflicting tables/columns from the dialog.

--- trunk/src/ca/sqlpower/architect/ddl/RenameObjectDDLComponent.java
+++ trunk/src/ca/sqlpower/architect/ddl/RenameObjectDDLComponent.java
@@ -1,27 +1,49 @@
 package ca.sqlpower.architect.ddl;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.*;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import ca.sqlpower.architect.SQLObject;
 
 public class RenameObjectDDLComponent extends GenericDDLWarningComponent {
 
-    DDLWarning warning;
+    final DDLWarning warning;
     JComponent component;
+    /**
+     * List of text fields that correspond to the name of each
+     * SQLObject in the list of involved objects for the warning
+     * this component holds.
+     */
+    final List<JTextField> textFields = new ArrayList<JTextField>();
     Runnable changeApplicator;
 
     public RenameObjectDDLComponent(DDLWarning warning, Runnable changeApplicator) {
         super(warning);
         this.warning = warning;
-        this.changeApplicator = changeApplicator;
+        this.changeApplicator = new Runnable() {
+
+            public void run() {
+                for (int i = 0; i < textFields.size(); i++) {
+                    SQLObject obj = 
+                        RenameObjectDDLComponent.this.warning.getInvolvedObjects().get(i);
+                    obj.setName(textFields.get(i).getText());
+                }
+            }
+            
+        };
         component = new JPanel();
         component.add(getQuickFixButton());                 // XXX anti-pattern
         component.add(new JLabel(warning.getMessage()));
         List<SQLObject> list = warning.getInvolvedObjects();
-        for (int i = 0; i < list.size(); i++) {
-            component.add(new JTextField(list.get(i).getName()));
+        for (SQLObject obj : list) {
+            JTextField jtf = new JTextField(obj.getName());
+            component.add(jtf);
+            textFields.add(jtf);
         }
     }
 
