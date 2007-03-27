b8dff41c0634b7aa6a7e25796b83f07e61a11735
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2007-03-27 23:11:02 +0000

    Got rid of unnecessary "message" attribtue in the architectVersion task
    Renamed "deploy" to "dist" in the build file.

--- src/ca/sqlpower/architect/antbuild/ArchitectVersionTask.java
+++ src/ca/sqlpower/architect/antbuild/ArchitectVersionTask.java
@@ -5,18 +5,17 @@ import org.apache.tools.ant.Task;
 
 import ca.sqlpower.architect.ArchitectVersion;
 
+/**
+ * Sets three properties in the ant project: app_ver_major, app_ver_minor, and
+ * app_ver_tiny. All values are based on the constants in the ArchitectVersion
+ * class.
+ */
 public class ArchitectVersionTask extends Task {
     
-    
-    
-    // The method executing the task
     public void execute() throws BuildException {
         getProject().setNewProperty("app_ver_major", ArchitectVersion.APP_VERSION_MAJOR );
         getProject().setNewProperty("app_ver_minor", ArchitectVersion.APP_VERSION_MINOR );
         getProject().setNewProperty("app_ver_tiny", ArchitectVersion.APP_VERSION_TINY );
     }
 
-    // The setter for the "message" attribute
-    public void setMessage(String msg) {
-    }
 }
