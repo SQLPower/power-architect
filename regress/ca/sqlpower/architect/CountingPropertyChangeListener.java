f9fa64757e56b1a34d031b2f0bcd7d05626739a4
thomas <thomas@sqlpower.ca>
2007-05-11 21:05:12 +0000

    Enhanced the counting property to count each property individually. Added more documentation.

--- regress/ca/sqlpower/architect/CountingPropertyChangeListener.java
+++ regress/ca/sqlpower/architect/CountingPropertyChangeListener.java
@@ -2,6 +2,8 @@ package ca.sqlpower.architect;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * The CountingPropertyChangeListener class helps with testing beans with
@@ -11,10 +13,37 @@ import java.beans.PropertyChangeListener;
  * $Id$
  */
 public class CountingPropertyChangeListener implements PropertyChangeListener {
+    
+    /**
+     * Individual counts of changes by property name.
+     */
+    private Map<String, Integer> propertyChangeCounts = new HashMap<String, Integer>();
+    
+    /**
+     * The overall number of property changes (summed across all property names).
+     */
 	private int propertyChangeCount;
+    
+    /**
+     * The most recent propery name to have changed.
+     */
 	private String lastPropertyChange;
+    
+    /**
+     * The most recent property change source object.
+     */
 	private Object lastSource;
+    
+    /**
+     * The most recent "old" property value (the previous value of the <i>lastPropertyChange</i>
+     * property of <i>lastSource</i>).
+     */
 	private Object lastOldValue;
+
+    /**
+     * The most recent "new" property value (the current value of the <i>lastPropertyChange</i>
+     * property of <i>lastSource</i>).
+     */
 	private Object lastNewValue;
 	
 	/**
@@ -26,6 +55,12 @@ public class CountingPropertyChangeListener implements PropertyChangeListener {
 		lastSource = evt.getSource();
 		lastOldValue = evt.getOldValue();
 		lastNewValue = evt.getNewValue();
+        
+        Integer oldCount = propertyChangeCounts.get(evt.getPropertyName());
+        if (oldCount == null) {
+            oldCount = 0;
+        }
+        propertyChangeCounts.put(evt.getPropertyName(), oldCount + 1);
 	}
 
 	public Object getLastNewValue() {
@@ -44,8 +79,25 @@ public class CountingPropertyChangeListener implements PropertyChangeListener {
 		return lastSource;
 	}
 
-	public int getPropertyChangeCount() {
-		return propertyChangeCount;
-	}
+    /**
+     * Returns the total number of property change events processed by
+     * this instance, regardless of property name.
+     */
+    public int getPropertyChangeCount() {
+        return propertyChangeCount;
+    }
+
+    /**
+     * Returns the number of property change events that applied to the
+     * given property name. 
+     */
+    public int getPropertyChangeCount(String propName) {
+        Integer count = propertyChangeCounts.get(propName);
+        if (count == null) {
+            return 0;
+        } else {
+            return count.intValue();
+        }
+    }
 
 }
