a70d89860f70a6974da29dc8ea56eb0a08347338
thomas <thomas@sqlpower.ca>
2007-05-11 21:02:29 +0000

    Removed unused diff and dump methods. Added the method copyFrom to copy the properties of one data source to this one.

--- src/ca/sqlpower/architect/ArchitectDataSource.java
+++ src/ca/sqlpower/architect/ArchitectDataSource.java
@@ -24,7 +24,6 @@ import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -192,52 +191,6 @@ public class ArchitectDataSource {
 	public static final String PL_DATABASE_NAME = "Database Name";
 	public static final String PL_IP = "IP";
 	public static final String PL_PORT = "PORT";
-
-    /**
-     * This is just for debugging purposes and prints out the difference between
-     * two ArchitectDataSource
-     * @param o2 the object containing the properties
-     */
-    public void diff(ArchitectDataSource o2) {
-        Map<String,String> p2 = o2.properties;
-        dump(properties, p2, DBCS_DRIVER_CLASS);
-        dump(properties, p2, DBCS_URL);
-        dump(properties, p2, PL_LOGICAL);
-        dump(properties, p2, PL_TYPE);
-        dump(properties, p2, PL_DSN);
-        dump(properties, p2, PL_SCHEMA_OWNER);
-        dump(properties, p2, PL_UID);
-        dump(properties, p2, PL_PWD);
-        dump(properties, p2, PL_TNS);
-        dump(properties, p2, PL_DATABASE_NAME);
-        dump(properties, p2, PL_IP);
-        dump(properties, p2, PL_PORT);
-        Iterator<String> iter = properties.keySet().iterator();
-        while (iter.hasNext()) {
-            System.out.print(iter.next() + ", ");
-        }
-        logger.debug("\n");
-        Iterator<String> iter2 = p2.keySet().iterator();
-        while (iter2.hasNext()) {
-            logger.debug(iter2.next() + ", ");
-        }
-        logger.debug("\n");
-    }
-
-    /**
-     * This method is for debugging purposes to print out the differences between two maps.
-     * 
-     * @param myMap first map to compare
-     * @param otherMap second map for comparaing
-     * @param key the property to compare
-     */
-	private void dump(Map<String, String> myMap, Map<String, String> otherMap, String key) {
-        String val1 = myMap.get(key);
-        String val2 = otherMap.get(key);
-        System.out.print("Property=" + key + "; me=" + val1 + "; other=" + val2 + ';');
-        if (val1 != null && val2 != null && !val1.equals(val2))
-            logger.debug(" <-- DIFFERENT");
-    }
 
     /**
 	 * This field is transient; don't access it directly becuase it
@@ -590,5 +543,27 @@ public class ArchitectDataSource {
 
     public boolean isParentSet() {
         return parentSet;
+    }
+
+    /**
+     * Copies all properties from the given data source into this one.
+     * After this method returns, this data source will specify the same
+     * target database as the given data source.
+     * 
+     * @param dbcs The connection spec to copy from (must not be null).
+     */
+    public void copyFrom(ArchitectDataSource dbcs) {
+        properties.clear();
+        
+        // This is extremely, stupidly cheap.  The tree doesn't notice the change unless there's
+        // a property change event for the data source's name.
+        setName(dbcs.getName());
+
+        for (Map.Entry<String, String> entry : dbcs.getPropertiesMap().entrySet()) {
+            // this is non-ideal, because the property change events will not have correct property names
+            put(entry.getKey(), entry.getValue());
+        }
+        
+        setParentType(dbcs.getParentType());
     }
 }
