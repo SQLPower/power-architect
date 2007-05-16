a47860a28517c715f7c78446851f9cc3f90e257d
fuerth <fuerth@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2007-05-16 21:33:20 +0000

    Bumped version to 0.9.5. This version has better support for non-XML characters in the project file.

--- trunk/src/ca/sqlpower/architect/ArchitectVersion.java
+++ trunk/src/ca/sqlpower/architect/ArchitectVersion.java
@@ -1,4 +1,5 @@
 package ca.sqlpower.architect;
+
 /**
  * It is extremly important that this class has no dependancies outside of the standard java libraries.
  */
@@ -6,7 +7,7 @@ public class ArchitectVersion {
     
     public static final String APP_VERSION_MAJOR = "0";
     public static final String APP_VERSION_MINOR = "9";
-    public static final String APP_VERSION_TINY  = "4";
+    public static final String APP_VERSION_TINY  = "5";
     public static final String APP_VERSION = APP_VERSION_MAJOR+"."+
                                             APP_VERSION_MINOR+"." +
                                             APP_VERSION_TINY;
