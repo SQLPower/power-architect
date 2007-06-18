7c511176974e3d67ea8de283c852eea47331bd4f
jeffrey <jeffrey@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2007-06-18 22:00:29 +0000

    Added a new package: ca.sqlpoewr.architect.etl.kettle
    Added a new class KettleOptions, which acts as a container for Kettle-related options.

---
+++ trunk/src/ca/sqlpower/architect/etl/kettle/KettleOptions.java
@@ -0,0 +1,19 @@
+package ca.sqlpower.architect.etl.kettle;
+
+/**
+ * A container for Kettle-related options.
+ */
+public class KettleOptions {
+
+    /**
+     * The key to use in an ArchitectDataSource for the repository login
+     * name.
+     */
+    public static final String KETTLE_REPOS_LOGIN_KEY = "ca.sqlpower.architect.etl.kettle.repos.login";
+
+    /**
+     * The key to use in an ArchitectDataSource for the repository password.
+     */
+    public static final String KETTLE_REPOS_PASSWORD_KEY = "ca.sqlpower.architect.etl.kettle.repos.password";
+
+}
