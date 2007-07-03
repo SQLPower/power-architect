9179285b0dbe7fb15a8b9e22ede5d87441711115
thomas <thomas@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2007-07-03 21:53:54 +0000

    Added documentation

--- trunk/src/ca/sqlpower/architect/FileValidator.java
+++ trunk/src/ca/sqlpower/architect/FileValidator.java
@@ -1,9 +1,34 @@
 package ca.sqlpower.architect;
 
-
+/**
+ *  This interface is used for handling when a file is to be created
+ *   but already exists. This class can be implemented to handle
+ *   file overwrites in different ways, such as overwriting or not. 
+ *   One example is the AlwaysAcceptFileValidator which always
+ *   returns a WRITE_OK response.
+ *   
+ *   This class is also used to handle the desired overwriting when
+ *   saving to a repository.
+ */
 public interface FileValidator {
 
-    public static enum FileValidationResponse {WRITE_OK, WRITE_NOT_OK, CANCEL, WRITE_OK_ALWAYS, WRITE_NOT_OK_ALWAYS}
-    
+    public static enum FileValidationResponse {WRITE_OK                 //This response denotes that the file should be overwritten
+        
+                                               , WRITE_NOT_OK           //This response denotes that the file should not be overwritten
+                                               
+                                               , CANCEL                 //This response denotes that the file should not be overwritten
+                                                                        //and no more files should be written to
+                                               
+                                               , WRITE_OK_ALWAYS        //This response denotes that the file and all remaining files
+                                                                        //should be overwritten
+                                               
+                                               , WRITE_NOT_OK_ALWAYS    //This response denotes that no further files should be overwritten
+                                                                        //including the current one
+                                               }
+
+    /**
+     * This method decides if a given file should be overwritten or not
+     * based on the string name and its path.
+     */
     public FileValidationResponse acceptFile (String name, String path);
 }
