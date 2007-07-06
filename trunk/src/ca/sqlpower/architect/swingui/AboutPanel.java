7648af4c77674e769cef998ea9422111e8795656
jfuerth <jfuerth@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2007-07-06 22:43:48 +0000

    Updated copyright date to this year (I know, it's already half over)

--- trunk/src/ca/sqlpower/architect/swingui/AboutPanel.java
+++ trunk/src/ca/sqlpower/architect/swingui/AboutPanel.java
@@ -74,7 +74,7 @@ public class AboutPanel extends JPanel implements ArchitectPanel {
 
 		content = new JLabel("<html>Power*Architect "+
 		                    ArchitectVersion.APP_VERSION+"<br><br>" +
-							"Copyright 2003-2006 SQL Power Group Inc.<br>" +
+							"Copyright 2003-2007 SQL Power Group Inc.<br>" +
 							"</html>");
 		pan.add(content);
         return pan;
