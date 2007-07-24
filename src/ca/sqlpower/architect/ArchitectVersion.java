239b0a4d15a589373f4dd3b708e03223837a2195
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2007-07-24 15:59:17 +0000

    Added javadoc comments for all variables to clarify their meaning and the conventions assoicated with Architect version numbering.
    Changed the full version string to omit the "-" when the suffix
    is the empty string.

--- src/ca/sqlpower/architect/ArchitectVersion.java
+++ src/ca/sqlpower/architect/ArchitectVersion.java
@@ -36,12 +36,47 @@ package ca.sqlpower.architect;
  */
 public class ArchitectVersion {
     
+    /**
+     * The major version number. Currently we're working toward the 1.0 release.
+     * This number will become 1 when we are satisfied that the API is feature
+     * complete and can remain stable for a period of time. Until then, we will
+     * leave the major version at 0 and all bets are off.
+     */
     public static final String APP_VERSION_MAJOR  = "0";
+    
+    /**
+     * Minor version number. This changes when new features appear that might
+     * break forward compatibility.
+     */
     public static final String APP_VERSION_MINOR  = "9";
+    
+    /**
+     * Tiny version number.  This number changes with each release, but resets
+     * back to 0 when the minor version changes.  All versions under the same
+     * minor version number are fully compatible with each other.
+     */
     public static final String APP_VERSION_TINY   = "7";
+    
+    /**
+     * Suffixes indicate pre-release builds.  They normally progress from "alpha"
+     * to "beta" to "rcN" (release candidate N).  This progression is common
+     * for a lot of software projects, and has the advantage of sorting alphabetically
+     * into the correct order from oldest to newest.
+     * <p>
+     * Full releases do not have a suffix.  In that case, the suffix is the empty
+     * string (not null).
+     */
     public static final String APP_VERSION_SUFFIX = "alpha";
-    public static final String APP_VERSION = APP_VERSION_MAJOR+"."+
-                                            APP_VERSION_MINOR+"." +
-                                            APP_VERSION_TINY+"-" +
-                                            APP_VERSION_SUFFIX;
+    
+    /**
+     * The full version number, formatted as Major.Minor.Tiny[-Suffix].  Note the square
+     * brackets are not part of the version string; they indicate that the hyphen and
+     * suffix are omitted when there is no suffix.
+     */
+    public static final String APP_VERSION = APP_VERSION_MAJOR + "." +
+                                            APP_VERSION_MINOR + "." +
+                                            APP_VERSION_TINY +
+                                            (APP_VERSION_SUFFIX.length() > 0
+                                              ? "-" + APP_VERSION_SUFFIX
+                                              : "");
 }
