ea1d04715950bf5668e331bd68b82c0fd6f88444
jfuerth <jfuerth@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2007-07-31 00:59:03 +0000

    Bumped version number since we've just released 0.9.7

--- trunk/src/ca/sqlpower/architect/ArchitectVersion.java
+++ trunk/src/ca/sqlpower/architect/ArchitectVersion.java
@@ -55,7 +55,7 @@ public class ArchitectVersion {
      * back to 0 when the minor version changes.  All versions under the same
      * minor version number are fully compatible with each other.
      */
-    public static final String APP_VERSION_TINY   = "7";
+    public static final String APP_VERSION_TINY   = "8";
     
     /**
      * Suffixes indicate pre-release builds.  They normally progress from "alpha"
@@ -66,7 +66,7 @@ public class ArchitectVersion {
      * Full releases do not have a suffix.  In that case, the suffix is the empty
      * string (not null).
      */
-    public static final String APP_VERSION_SUFFIX = "";
+    public static final String APP_VERSION_SUFFIX = "alpha";
     
     /**
      * The full version number, formatted as Major.Minor.Tiny[-Suffix].  Note the square
