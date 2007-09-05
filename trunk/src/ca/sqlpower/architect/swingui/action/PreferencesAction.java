19b7bf393be50719b4b5f42976e9e236ef4f0cd2
sean.oczkowski <sean.oczkowski@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2007-09-05 20:16:48 +0000

    Fixed the PreferencesAction so that it doesn't created another JDialog if one has already been created. This fixes the problem where clicking Preferences multiple times would open multiple Preference dialogs

--- trunk/src/ca/sqlpower/architect/swingui/action/PreferencesAction.java
+++ trunk/src/ca/sqlpower/architect/swingui/action/PreferencesAction.java
@@ -57,6 +57,11 @@ import com.jgoodies.forms.factories.ButtonBarFactory;
 
 public class PreferencesAction extends AbstractArchitectAction {
 	private static final Logger logger = Logger.getLogger(EditTableAction.class);
+	
+	/**
+	 * The JDialog where we display the User Preferences
+	 */
+	private JDialog d;
 
 	public PreferencesAction(ArchitectSwingSession session) {
         super(session, "User Preferences...", "User Preferences");
@@ -66,60 +71,65 @@ public class PreferencesAction extends AbstractArchitectAction {
 		showPreferencesDialog();
 	}
 
+	/**
+	 * If the JDialog hasn't been initialized, then initialize it
+	 * Otherwise, just set it to visible.
+	 */
 	public void showPreferencesDialog() {
 		logger.debug("showPreferencesDialog");
 		
 		// XXX Can't easily use ArchitectPanelBuilder since this
 		// contains a JTabbedPane which is not an ArchitectPanel.
-		final JDialog d = new JDialog(frame, "User Preferences");
-		
-		JPanel cp = new JPanel(new BorderLayout(12,12));
-		JTabbedPane tp = new JTabbedPane();
-		cp.add(tp, BorderLayout.CENTER);
-		cp.setBorder(BorderFactory.createEmptyBorder(12,12,12,12));
-
-		final PreferencesPanel prefPanel = new PreferencesPanel(session);
-		tp.add("General", prefPanel);
-
-        final DataSourceTypeEditor dsTypeEditor =
-            new DataSourceTypeEditor(session.getUserSettings().getPlDotIni());
-
-        // Add the Kettle Options Panel as a tab to the SPDataSourceTypePanel
-        
-        final KettleDataSourceTypeOptionPanel kettleOptsPanel = new KettleDataSourceTypeOptionPanel();
-        
-        dsTypeEditor.addTab("Kettle", kettleOptsPanel);
-        
-        tp.add("JDBC Drivers", dsTypeEditor.getPanel());
-
-	
-		JDefaultButton okButton = new JDefaultButton(DataEntryPanelBuilder.OK_BUTTON_LABEL);
-		okButton.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent evt) {
-					prefPanel.applyChanges();
-                    dsTypeEditor.applyChanges();
-					d.setVisible(false);
-				}
-			});
-	
-		Action cancelAction = new AbstractAction() {
-				public void actionPerformed(ActionEvent evt) {
-					prefPanel.discardChanges();
-                    dsTypeEditor.discardChanges();
-					d.setVisible(false);
-				}
-		};
-		cancelAction.putValue(Action.NAME, DataEntryPanelBuilder.CANCEL_BUTTON_LABEL);
-		JButton cancelButton = new JButton(cancelAction);
-
-        JPanel buttonPanel = ButtonBarFactory.buildOKCancelBar(okButton, cancelButton);
-
-		SPSUtils.makeJDialogCancellable(d, cancelAction);
-		d.getRootPane().setDefaultButton(okButton);
-		cp.add(buttonPanel, BorderLayout.SOUTH);
-		d.setContentPane(cp);
-		d.pack();
-		d.setLocationRelativeTo(frame);
+		if (d == null) {
+    		d = new JDialog(frame, "User Preferences");
+    		JPanel cp = new JPanel(new BorderLayout(12,12));
+    		JTabbedPane tp = new JTabbedPane();
+    		cp.add(tp, BorderLayout.CENTER);
+    		cp.setBorder(BorderFactory.createEmptyBorder(12,12,12,12));
+    
+    		final PreferencesPanel prefPanel = new PreferencesPanel(session);
+    		tp.add("General", prefPanel);
+    
+            final DataSourceTypeEditor dsTypeEditor =
+                new DataSourceTypeEditor(session.getUserSettings().getPlDotIni());
+    
+            // Add the Kettle Options Panel as a tab to the SPDataSourceTypePanel
+            
+            final KettleDataSourceTypeOptionPanel kettleOptsPanel = new KettleDataSourceTypeOptionPanel();
+            
+            dsTypeEditor.addTab("Kettle", kettleOptsPanel);
+            
+            tp.add("JDBC Drivers", dsTypeEditor.getPanel());
+    
+    	
+    		JDefaultButton okButton = new JDefaultButton(DataEntryPanelBuilder.OK_BUTTON_LABEL);
+    		okButton.addActionListener(new ActionListener() {
+    				public void actionPerformed(ActionEvent evt) {
+    					prefPanel.applyChanges();
+                        dsTypeEditor.applyChanges();
+    					d.setVisible(false);
+    				}
+    			});
+    	
+    		Action cancelAction = new AbstractAction() {
+    				public void actionPerformed(ActionEvent evt) {
+    					prefPanel.discardChanges();
+                        dsTypeEditor.discardChanges();
+    					d.setVisible(false);
+    				}
+    		};
+    		cancelAction.putValue(Action.NAME, DataEntryPanelBuilder.CANCEL_BUTTON_LABEL);
+    		JButton cancelButton = new JButton(cancelAction);
+    
+            JPanel buttonPanel = ButtonBarFactory.buildOKCancelBar(okButton, cancelButton);
+    
+    		SPSUtils.makeJDialogCancellable(d, cancelAction);
+    		d.getRootPane().setDefaultButton(okButton);
+    		cp.add(buttonPanel, BorderLayout.SOUTH);
+    		d.setContentPane(cp);
+    		d.pack();
+    		d.setLocationRelativeTo(frame);
+		}
 		d.setVisible(true);		
 	}
 }
