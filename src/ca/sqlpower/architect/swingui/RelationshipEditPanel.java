0165b317a3879676e4a483764ba5178b31bfaa5e
Jonathan Fuerth <jfuerth@users.noreply.github.com>
2007-10-29 21:21:09 +0000

    -Added FK constraint deferrability stuff to the UI -Redid the layout to be more in line with the other panels of the Architect (it was looking kind of old, and adding the deferrability stuff stretched it to the breaking point)

--- src/ca/sqlpower/architect/swingui/RelationshipEditPanel.java
+++ src/ca/sqlpower/architect/swingui/RelationshipEditPanel.java
@@ -31,7 +31,7 @@
  */
 package ca.sqlpower.architect.swingui;
 
-import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -40,22 +40,32 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
-import javax.swing.border.TitledBorder;
 
 import org.apache.log4j.Logger;
 
 import ca.sqlpower.architect.ArchitectException;
 import ca.sqlpower.architect.SQLRelationship;
+import ca.sqlpower.architect.SQLRelationship.Deferrability;
 import ca.sqlpower.architect.undo.UndoCompoundEvent;
 import ca.sqlpower.architect.undo.UndoCompoundEventListener;
 import ca.sqlpower.architect.undo.UndoCompoundEvent.EventTypes;
 import ca.sqlpower.swingui.DataEntryPanel;
 
-public class RelationshipEditPanel extends JPanel
-	implements DataEntryPanel {
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+
+public class RelationshipEditPanel implements DataEntryPanel {
 
 	private static final Logger logger = Logger.getLogger(RelationshipEditPanel.class);
 
+    /**
+     * The panel that contains the editor components.
+     */
+    private final JPanel panel;
+    
+    /**
+     * The relationship being edited.
+     */
 	private SQLRelationship relationship;
 
 	private JTextField relationshipName;
@@ -77,66 +87,79 @@ public class RelationshipEditPanel extends JPanel
 	private JRadioButton fkTypeOneToMany;
 	private JRadioButton fkTypeZeroOne;
 
+    private ButtonGroup deferrabilityGroup;
+    private JRadioButton notDeferrable;
+    private JRadioButton initiallyDeferred;
+    private JRadioButton initiallyImmediate;
+    
 	public RelationshipEditPanel(ArchitectSwingSession session) {
-		super(new BorderLayout());
-		addUndoEventListener(session.getArchitectFrame().getUndoManager().getEventAdapter());
-		JPanel topPanel = new JPanel(new FormLayout());
-		topPanel.add(new JLabel("Relationship Name"));
-		topPanel.add(relationshipName = new JTextField());
-		topPanel.add(new JLabel("Relationship Type"));
-		JPanel typePanel = new JPanel();
 
+        // XXX this looks suspicious. why does it work? shouldn't the events come
+        //     straight from the SQLRelationship object?
+        addUndoEventListener(session.getArchitectFrame().getUndoManager().getEventAdapter());
+        
+        FormLayout layout = new FormLayout("pref, 4dlu, pref:grow");
+        DefaultFormBuilder fb = new DefaultFormBuilder(layout);
+        
+        fb.append("Relationship Name", relationshipName = new JTextField());
+
+		JPanel typePanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 12, 0));
 		identifyingGroup = new ButtonGroup();
 		typePanel.add(identifyingButton = new JRadioButton("Identifying"));
 		identifyingGroup.add(identifyingButton);
 		typePanel.add(nonIdentifyingButton = new JRadioButton("Non-Identifying"));
 		identifyingGroup.add(nonIdentifyingButton);
-		topPanel.add(typePanel);
-		add(topPanel, BorderLayout.NORTH);
-		
-		JPanel pkPanel = new JPanel(new FormLayout());
-		pkPanel.setBorder(new TitledBorder("Primary Key End"));
-		pkPanel.add(new JLabel("PK Table"));
-		pkPanel.add(pkTableName = new JLabel("Unknown"));
-		pkTypeGroup = new ButtonGroup();
-		pkPanel.add(new JLabel(""));
-		pkPanel.add(pkTypeZeroToMany = new JRadioButton("Zero or More"));
-		pkTypeGroup.add(pkTypeZeroToMany);
-		pkPanel.add(new JLabel("Cardinality"));
-		pkPanel.add(pkTypeOneToMany = new JRadioButton("One or More"));
-		pkTypeGroup.add(pkTypeOneToMany);
-		pkPanel.add(new JLabel(""));
-		pkPanel.add(pkTypeZeroOne = new JRadioButton("Zero or One"));
-		pkTypeGroup.add(pkTypeZeroOne);
-		pkPanel.add(new JLabel(""));
-		pkPanel.add(pkTypeOne = new JRadioButton("Exactly One"));
-		pkTypeGroup.add(pkTypeOne);
-		add(pkPanel, BorderLayout.WEST);
+		fb.append("Relationship Type", typePanel);
 
-		JPanel fkPanel = new JPanel(new FormLayout());
-		fkPanel.setBorder(new TitledBorder("Foreign Key End"));
-		fkPanel.add(new JLabel("FK Table"));
-		fkPanel.add(fkTableName = new JLabel("Unknown"));
+		fb.nextLine();
+        fb.appendUnrelatedComponentsGapRow();
+        fb.nextLine();
+        fb.append("Cardinality", pkTableName = new JLabel("PK Table: Unknown"));
+		pkTypeGroup = new ButtonGroup();
+        fb.append("", pkTypeZeroToMany = new JRadioButton("Zero or More"));
+		pkTypeGroup.add(pkTypeZeroToMany);
+        fb.append("", pkTypeOneToMany = new JRadioButton("One or More"));
+		pkTypeGroup.add(pkTypeOneToMany);
+		fb.append("", pkTypeZeroOne = new JRadioButton("Zero or One"));
+		pkTypeGroup.add(pkTypeZeroOne);
+		fb.append("", pkTypeOne = new JRadioButton("Exactly One"));
+		pkTypeGroup.add(pkTypeOne);
+
+		fb.nextLine();
+        fb.appendRelatedComponentsGapRow();
+        fb.nextLine();
+		fb.append("", fkTableName = new JLabel("FK Table: Unknown"));
 		fkTypeGroup = new ButtonGroup();
-		fkPanel.add(new JLabel(""));
-		fkPanel.add(fkTypeZeroToMany = new JRadioButton("Zero or More"));
+		fb.append("", fkTypeZeroToMany = new JRadioButton("Zero or More"));
 		fkTypeGroup.add(fkTypeZeroToMany);
-		fkPanel.add(new JLabel("Cardinality"));
-		fkPanel.add(fkTypeOneToMany = new JRadioButton("One or More"));
+		fb.append("", fkTypeOneToMany = new JRadioButton("One or More"));
 		fkTypeGroup.add(fkTypeOneToMany);
-		fkPanel.add(new JLabel(""));
-		fkPanel.add(fkTypeZeroOne = new JRadioButton("Zero or One"));
+		fb.append("", fkTypeZeroOne = new JRadioButton("Zero or One"));
 		fkTypeGroup.add(fkTypeZeroOne);
-		add(fkPanel, BorderLayout.EAST);		
+
+		fb.nextLine();
+        fb.appendUnrelatedComponentsGapRow();
+        fb.nextLine();
+        deferrabilityGroup = new ButtonGroup();
+        fb.append("Deferrability", notDeferrable = new JRadioButton("Not Deferrable"));
+        deferrabilityGroup.add(notDeferrable);
+        fb.append("", initiallyDeferred = new JRadioButton("Deferrable, Initially Deferred"));
+        deferrabilityGroup.add(initiallyDeferred);
+        fb.append("", initiallyImmediate = new JRadioButton("Deferrable, Initially Immediate"));
+        deferrabilityGroup.add(initiallyImmediate);
+        
 		relationshipName.selectAll();
+        
+        fb.setDefaultDialogBorder();
+        panel = fb.getPanel();
 	}
 
 
 	public void setRelationship(SQLRelationship r) {
 		this.relationship = r;
 		relationshipName.setText(r.getName());
-		pkTableName.setText(relationship.getPkTable().getName());
-		fkTableName.setText(relationship.getFkTable().getName());
+		pkTableName.setText("PK Table: " + relationship.getPkTable().getName());
+		fkTableName.setText("FK Table: " + relationship.getFkTable().getName());
 		if ( r.isIdentifying()){
 			identifyingButton.setSelected(true);
 		} else {
@@ -160,6 +183,15 @@ public class RelationshipEditPanel extends JPanel
 		} else if (fkc == (SQLRelationship.ONE | SQLRelationship.MANY)){
 			fkTypeOneToMany.setSelected(true);
 		}
+        
+        if (r.getDeferrability() == Deferrability.NOT_DEFERRABLE) {
+            notDeferrable.setSelected(true);
+        } else if (r.getDeferrability() == Deferrability.INITIALLY_DEFERRED) {
+            initiallyDeferred.setSelected(true);
+        } else if (r.getDeferrability() == Deferrability.INITIALLY_IMMEDIATE) {
+            initiallyImmediate.setSelected(true);
+        }
+        
 		relationshipName.selectAll();
 	}
 
@@ -192,6 +224,15 @@ public class RelationshipEditPanel extends JPanel
 			} else if (fkTypeOneToMany.isSelected()) {
 				relationship.setFkCardinality(SQLRelationship.ONE | SQLRelationship.MANY);
 			}
+            
+            if (notDeferrable.isSelected()) {
+                relationship.setDeferrability(Deferrability.NOT_DEFERRABLE);
+            } else if (initiallyDeferred.isSelected()) {
+                relationship.setDeferrability(Deferrability.INITIALLY_DEFERRED);
+            } else if (initiallyImmediate.isSelected()) {
+                relationship.setDeferrability(Deferrability.INITIALLY_IMMEDIATE);
+            }
+            
 		} finally {
 			endCompoundEdit("Ending new compound edit event in relationship edit panel");
 		}
@@ -242,7 +283,7 @@ public class RelationshipEditPanel extends JPanel
 	}
 
 	public JPanel getPanel() {
-		return this;
+		return panel;
 	}
 	
 }
