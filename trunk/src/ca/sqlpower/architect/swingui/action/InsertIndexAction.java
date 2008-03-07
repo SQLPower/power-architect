e6d76b79ec969429235048da9a00333a9cadea67
oscanada12 <oscanada12@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2008-03-07 23:13:17 +0000

    Added the GUI components that will be able to add, remove and edit indices for tables. Proper support for index types will still have to be added to the current components.

--- trunk/src/ca/sqlpower/architect/swingui/action/InsertIndexAction.java
+++ trunk/src/ca/sqlpower/architect/swingui/action/InsertIndexAction.java
@@ -31,16 +31,19 @@
  */
 package ca.sqlpower.architect.swingui.action;
 
-import javax.swing.JOptionPane;
+import javax.swing.JDialog;
 
 import org.apache.log4j.Logger;
 
 import ca.sqlpower.architect.ArchitectException;
+import ca.sqlpower.architect.SQLIndex;
 import ca.sqlpower.architect.SQLObject;
 import ca.sqlpower.architect.SQLTable;
 import ca.sqlpower.architect.swingui.ArchitectSwingConstants;
 import ca.sqlpower.architect.swingui.ArchitectSwingSession;
+import ca.sqlpower.architect.swingui.IndexEditPanel;
 import ca.sqlpower.architect.swingui.TablePane;
+import ca.sqlpower.swingui.DataEntryPanelBuilder;
 
 public class InsertIndexAction extends AbstractTableTargetedAction {
 
@@ -82,8 +85,15 @@ public class InsertIndexAction extends AbstractTableTargetedAction {
         
         // (check the revision history for the old implementation)
         
-        JOptionPane.showMessageDialog(session.getArchitectFrame(),
-                "Index editing support will be available in\n" +
-                "an upcoming release of the Power*Architect.");
+        final JDialog d;
+        SQLIndex index = new SQLIndex();
+        final IndexEditPanel editPanel = new IndexEditPanel(index, parent, session);
+  
+        d = DataEntryPanelBuilder.createDataEntryPanelDialog(
+                editPanel, frame,
+                "Index Properties", "OK");
+        d.pack();
+        d.setLocationRelativeTo(frame);
+        d.setVisible(true);
     }
 }
