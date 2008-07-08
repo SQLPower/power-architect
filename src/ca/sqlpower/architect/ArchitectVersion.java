85a87cdecf0a61bb91453f4ef4f0463bc6d1deaa
jeffrey <jeffrey@sqlpower.ca>
2008-07-08 19:51:58 +0000

    The 0.9.11 release is finished so future builds should be 0.9.12-alpha.

--- src/ca/sqlpower/architect/ArchitectVersion.java
+++ src/ca/sqlpower/architect/ArchitectVersion.java
@@ -53,7 +53,7 @@ public class ArchitectVersion implements Comparable<ArchitectVersion> {
      * back to 0 when the minor version changes.  All versions under the same
      * minor version number are fully compatible with each other.
      */
-    public static final String APP_VERSION_TINY   = "11";
+    public static final String APP_VERSION_TINY   = "12";
     
     /**
      * Suffixes indicate pre-release builds.  They normally progress from "alpha"
@@ -64,7 +64,7 @@ public class ArchitectVersion implements Comparable<ArchitectVersion> {
      * Full releases do not have a suffix.  In that case, the suffix is the empty
      * string (not null).
      */
-    public static final String APP_VERSION_SUFFIX = "";
+    public static final String APP_VERSION_SUFFIX = "alpha";
     
     /**
      * The full version number, formatted as Major.Minor.Tiny[-Suffix].  Note the square
