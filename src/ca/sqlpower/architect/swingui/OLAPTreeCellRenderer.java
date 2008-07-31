b1cd56e7ef89dfbd015f27b985863ff79b418dd0
kaiyi4 <kaiyi4@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2008-07-31 16:28:46 +0000

    Created a treeModel for olap objects. Modified some of the xml definition file and the meta generator to fix allow GlazedLists to work properly.  Also, the mondrian.dtd was added, so now anyone can generate the MondrianDef =). We tested the treemodel with foodmart.xml using a main method included in OLAPTreeModel.java.

---
+++ src/ca/sqlpower/architect/swingui/OLAPTreeCellRenderer.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2008, SQL Power Group Inc.
+ *
+ * This file is part of Power*Architect.
+ *
+ * Power*Architect is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Power*Architect is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.architect.swingui;
+
+import java.awt.Component;
+
+import javax.swing.ImageIcon;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+import ca.sqlpower.architect.ArchitectSession;
+import ca.sqlpower.architect.olap.MondrianDef.Cube;
+import ca.sqlpower.architect.olap.MondrianDef.CubeDimension;
+import ca.sqlpower.architect.olap.MondrianDef.Measure;
+import ca.sqlpower.architect.olap.MondrianDef.Schema;
+import ca.sqlpower.architect.olap.MondrianDef.VirtualCube;
+import ca.sqlpower.architect.olap.MondrianDef.VirtualCubeMeasure;
+import ca.sqlpower.swingui.SPSUtils;
+
+public class OLAPTreeCellRenderer extends DefaultTreeCellRenderer {
+    public static final ImageIcon dbIcon = SPSUtils.createIcon("Database", "SQL Database", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon dbProfiledIcon = SPSUtils.createIcon("Database_profiled", "SQL Database", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon targetIcon = SPSUtils.createIcon("Database_target", "SQL Database", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon cataIcon = SPSUtils.createIcon("Catalog", "SQL Catalog", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon schemaIcon = SPSUtils.createIcon("Schema", "SQL Schema", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon tableIcon = SPSUtils.createIcon("Table", "SQL Table", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon tableProfiledIcon = SPSUtils.createIcon("Table_profiled", "SQL Table", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon exportedKeyIcon = SPSUtils.createIcon("ExportedKey", "Exported key", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon importedKeyIcon = SPSUtils.createIcon("ImportedKey", "Imported key", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon ownerIcon = SPSUtils.createIcon("Owner", "Owner", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon indexIcon = SPSUtils.createIcon("Index", "Index", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon pkIndexIcon = SPSUtils.createIcon("Index_key", "Primary Key Index", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon uniqueIndexIcon = SPSUtils.createIcon("Index_unique", "Unique Index", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+    public static final ImageIcon columnIcon = SPSUtils.createIcon("Column", "Column", ArchitectSwingSessionContext.ICON_SIZE); //$NON-NLS-1$ //$NON-NLS-2$
+//    private final ArchitectSession session;
+   
+
+    public OLAPTreeCellRenderer(ArchitectSession session) {
+        super();
+//        this.session = session;
+    }
+    
+    public OLAPTreeCellRenderer(){
+        super();
+    }
+
+
+    public Component getTreeCellRendererComponent(JTree tree,
+                                                  Object value,
+                                                  boolean sel,
+                                                  boolean expanded,
+                                                  boolean leaf,
+                                                  int row,
+                                                  boolean hasFocus) {
+        setText(value.toString());
+        if (value instanceof Schema) {
+            Schema schema = (Schema) value;
+            setText("Schema");
+            setIcon(schemaIcon);
+        } else if (value instanceof Cube) {
+            Cube cube = (Cube) value;
+            setText(cube.getInstanceName() + " (Cube)");
+            setIcon(tableIcon);
+        } else if (value instanceof VirtualCube) {
+            VirtualCube vCube = (VirtualCube) value;
+            setText(vCube.getInstanceName() + " (VirtualCube)");
+            setIcon(tableIcon);
+        } else if (value instanceof CubeDimension) {
+            CubeDimension dim = (CubeDimension) value;
+            setText(dim.getInstanceName() + " (Dimension)");
+            setIcon(columnIcon);
+        } else if (value instanceof Measure) {
+            Measure measure = (Measure) value;
+            setText(measure.getInstanceName() + " (Measure)");
+            setIcon(columnIcon);
+        } else if (value instanceof VirtualCubeMeasure) {
+            VirtualCubeMeasure measure = (VirtualCubeMeasure) value;
+            setText(measure.getInstanceName() + " (VitualCubeMeasure)");
+            setIcon(columnIcon);
+        } else {
+            setIcon(null);
+        }
+
+        this.selected = sel;
+        this.hasFocus = hasFocus;
+
+//        if (value instanceof SQLObject) {
+//            if (((SQLObject) value).isPopulated()) {
+//                setForeground(Color.black);
+//            } else {
+//                setForeground(Color.lightGray);
+//            }
+//        }
+//        setToolTipText(getText());
+        return this;
+    }
+}
