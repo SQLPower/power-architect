05054590b281103cf1172e09598673ae397b795d
kaiyi4 <kaiyi4@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2008-08-26 22:43:45 +0000

    Property edit field of top level name for levels, similar to top level name for hierarchies in the previous commit.

--- trunk/src/ca/sqlpower/architect/swingui/olap/HierarchyEditPanel.java
+++ trunk/src/ca/sqlpower/architect/swingui/olap/HierarchyEditPanel.java
@@ -50,6 +50,7 @@ public class HierarchyEditPanel implements ValidatableDataEntryPanel {
     private final JTextField captionField;
     private final JComboBox tableChooser;
     private final JCheckBox hasAll;
+    private final JTextField allLevelName;
     
     /**
      * Validation handler for errors in the dialog
@@ -77,6 +78,7 @@ public class HierarchyEditPanel implements ValidatableDataEntryPanel {
         builder.append("Caption", captionField = new JTextField(hierarchy.getCaption()));
         builder.append("Has All", hasAll = new JCheckBox("Has All", hierarchy.getHasAll() != null ? hierarchy.getHasAll() : true));
         builder.append("Table", tableChooser = new JComboBox(new Vector<SQLTable>(tables)));
+        builder.append("All Level Name", allLevelName = new JTextField("(All)"));
         tableChooser.setSelectedItem(OLAPUtil.tableForHierarchy(hierarchy)); // XXX this isn't quite right.. it would set the default as the local value
         
         handler = new FormValidationHandler(status);
@@ -105,6 +107,7 @@ public class HierarchyEditPanel implements ValidatableDataEntryPanel {
             table.setSchema(OLAPUtil.getQualifier(stable));
             hierarchy.setRelation(table);
         }
+        hierarchy.setAllLevelName(allLevelName.getText());
         hierarchy.endCompoundEdit();
         return true;
     }
