34e9faa1ce98cf4a8122988ef9d57daa58fd2f74
jfuerth <jfuerth@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2008-11-07 22:29:50 +0000

    Bumping version number for rc2

--- branches/release-0.9.12/src/ca/sqlpower/architect/ArchitectVersion.java
+++ branches/release-0.9.12/src/ca/sqlpower/architect/ArchitectVersion.java
@@ -64,7 +64,7 @@ public class ArchitectVersion implements Comparable<ArchitectVersion> {
      * Full releases do not have a suffix.  In that case, the suffix is the empty
      * string (not null).
      */
-    public static final String APP_VERSION_SUFFIX = "rc1";
+    public static final String APP_VERSION_SUFFIX = "rc2";
     
     /**
      * The full version number, formatted as Major.Minor.Tiny[-Suffix].  Note the square
