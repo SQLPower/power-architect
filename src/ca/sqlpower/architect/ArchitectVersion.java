9fbd29e147d8d9e55aa1f8342f7788f4d96b9c6e
jeffrey <jeffrey@sqlpower.ca>
2008-11-28 18:52:20 +0000

    Marked this version as Release Candidate 3

--- src/ca/sqlpower/architect/ArchitectVersion.java
+++ src/ca/sqlpower/architect/ArchitectVersion.java
@@ -64,7 +64,7 @@ public class ArchitectVersion implements Comparable<ArchitectVersion> {
      * Full releases do not have a suffix.  In that case, the suffix is the empty
      * string (not null).
      */
-    public static final String APP_VERSION_SUFFIX = "rc2";
+    public static final String APP_VERSION_SUFFIX = "rc3";
     
     /**
      * The full version number, formatted as Major.Minor.Tiny[-Suffix].  Note the square
