6094cd5f4131aeddd2ca5a8fa125f92a0dc3a17b
jeffrey <jeffrey@sqlpower.ca>
2008-12-03 22:26:05 +0000

    The trunk should be on 0.9.13, since we branched 0.9.12 earlier

--- src/ca/sqlpower/architect/ArchitectVersion.java
+++ src/ca/sqlpower/architect/ArchitectVersion.java
@@ -53,7 +53,7 @@ public class ArchitectVersion implements Comparable<ArchitectVersion> {
      * back to 0 when the minor version changes.  All versions under the same
      * minor version number are fully compatible with each other.
      */
-    public static final String APP_VERSION_TINY   = "12";
+    public static final String APP_VERSION_TINY   = "13";
     
     /**
      * Suffixes indicate pre-release builds.  They normally progress from "alpha"
