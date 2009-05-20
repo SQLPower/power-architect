115a5397aceb2bdc6f1779b745050ac8a7d5b35c
jfuerth <jfuerth@ef18fcdb-8a34-0410-ae7e-c1cd3e483e87>
2009-05-20 17:10:56 +0000

    Timestamp is a useless type in MySQL (it only supports UNIX times 1970-2035, and the JDBC driver can't even cope with selecting the default value for timestamp (which is 0000-00-00 00:00:00). We now forward engineer timestamp values as datetime, which is much more flexible.

--- trunk/src/ca/sqlpower/architect/ddl/MySqlDDLGenerator.java
+++ trunk/src/ca/sqlpower/architect/ddl/MySqlDDLGenerator.java
@@ -322,7 +322,7 @@ public class MySqlDDLGenerator extends GenericDDLGenerator {
                 null, DatabaseMetaData.columnNullable, true, false));
         typeMap.put(Integer.valueOf(Types.TIME), new GenericTypeDescriptor("TIME", Types.TIME, 0, "'", "'",
                 DatabaseMetaData.columnNullable, false, false));
-        typeMap.put(Integer.valueOf(Types.TIMESTAMP), new GenericTypeDescriptor("TIMESTAMP", Types.TIMESTAMP, 0, "'",
+        typeMap.put(Integer.valueOf(Types.TIMESTAMP), new GenericTypeDescriptor("DATETIME", Types.TIMESTAMP, 0, "'",
                 "'", DatabaseMetaData.columnNullable, false, false));
         typeMap.put(Integer.valueOf(Types.TINYINT), new GenericTypeDescriptor("TINYINT", Types.TINYINT, 38, null, null,
                 DatabaseMetaData.columnNullable, true, false));
