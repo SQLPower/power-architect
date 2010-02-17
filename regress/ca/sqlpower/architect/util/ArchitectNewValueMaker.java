c31618fdd2cab99556494f55e4444e72b7088bf0
thomasobrien95 <thomas@sqlpower.ca>
2010-02-17 22:19:51 +0000

    Updated some of the profile classes to conform with the persisters. Setters and child methods now fire expected events and the annotations exist for the ColumnProfileResult. The tests for the ColumnProfileResult exist and pass as well. An Architect specific new value maker exists for the new persister tests.

---
+++ regress/ca/sqlpower/architect/util/ArchitectNewValueMaker.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2010, SQL Power Group Inc.
+ *
+ * This file is part of Power*Architect.
+ *
+ * Power*Architect is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Power*Architect is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.architect.util;
+
+import ca.sqlpower.architect.profile.ColumnValueCount;
+import ca.sqlpower.architect.profile.ProfileSettings;
+import ca.sqlpower.architect.profile.TableProfileResult;
+import ca.sqlpower.object.SPObject;
+import ca.sqlpower.sql.DataSourceCollection;
+import ca.sqlpower.sql.SPDataSource;
+import ca.sqlpower.sqlobject.SQLTable;
+import ca.sqlpower.testutil.GenericNewValueMaker;
+
+public class ArchitectNewValueMaker extends GenericNewValueMaker {
+
+    public ArchitectNewValueMaker(SPObject root) {
+        super(root);
+    }
+    
+    public ArchitectNewValueMaker(SPObject root, DataSourceCollection<SPDataSource> dsCollection) {
+        super(root, dsCollection);
+    }
+    
+    @Override
+    public Object makeNewValue(Class<?> valueType, Object oldVal, String propName) {
+        if (valueType == ProfileSettings.class) {
+            ProfileSettings settings = new ProfileSettings();
+            getRootObject().addChild(settings, 0);
+            return settings;
+        } else if (valueType == TableProfileResult.class) {
+            TableProfileResult tpr = new TableProfileResult(
+                    (SQLTable) makeNewValue(SQLTable.class, null, ""), 
+                    (ProfileSettings) makeNewValue(ProfileSettings.class, null, ""));
+            getRootObject().addChild(tpr, 0);
+            return tpr;
+        } else if (valueType == ColumnValueCount.class) {
+            ColumnValueCount cvc = new ColumnValueCount(Integer.MAX_VALUE, 2, 42);
+            getRootObject().addChild(cvc, 0);
+            return cvc;
+        } else {
+            return super.makeNewValue(valueType, oldVal, propName);
+        }
+    }
+
+}
