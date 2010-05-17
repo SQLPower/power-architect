c09d6004ac8b1db34fe2298f28a5106fe4fef36a
terryyiu <terryyiu@gmail.com>
2010-05-17 21:07:04 +0000

    Fixed the SQLTypeTreeCellRenderer so that items will look selected when clicked on.

--- src/main/java/ca/sqlpower/architect/swingui/SQLTypeTreeCellRenderer.java
+++ src/main/java/ca/sqlpower/architect/swingui/SQLTypeTreeCellRenderer.java
@@ -51,6 +51,7 @@ public class SQLTypeTreeCellRenderer extends DefaultTreeCellRenderer {
     @Override
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded,
             boolean leaf, int row, boolean hasFocus) {
+        super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
         if (value instanceof ArchitectProject) {
             setIcon(null);
             
