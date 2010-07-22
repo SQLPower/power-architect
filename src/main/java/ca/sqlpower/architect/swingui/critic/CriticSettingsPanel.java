9589554f8dfbcca49b71d50d3d005d85e13d65bc
thomasobrien95 <thomas@sqlpower.ca>
2010-07-22 16:06:16 +0000

    Added the ability to add arbitrary settings to the critic settings. The critic settings panel can also display these settings and accept user changes.
    Currently only enums for settings are supported with a combo box but there is a place for boolean and string settings for the future.

--- src/main/java/ca/sqlpower/architect/swingui/critic/CriticSettingsPanel.java
+++ src/main/java/ca/sqlpower/architect/swingui/critic/CriticSettingsPanel.java
@@ -22,10 +22,13 @@ package ca.sqlpower.architect.swingui.critic;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import ca.sqlpower.architect.ddl.critic.CriticAndSettings;
@@ -65,6 +68,12 @@ public class CriticSettingsPanel implements DataEntryPanel {
      * on these changes can listen and be notified here.
      */
     private final List<PropertyChangeListener> listeners = new ArrayList<PropertyChangeListener>();
+
+    /**
+     * This map stores all of the property names to their combo boxes that a
+     * user can use to change the property of a setting with.
+     */
+    private final Map<String, JComboBox> criticPropertyChoosers = new HashMap<String, JComboBox>();
     
     /**
      * Updates the severity combo box due to changes to the model.
@@ -85,6 +94,18 @@ public class CriticSettingsPanel implements DataEntryPanel {
     public CriticSettingsPanel(CriticAndSettings settings) {
         this.settings = settings;
         
+        for (Map.Entry<String, Object> entry : settings.getProperties().entrySet()) {
+            if (entry.getValue() instanceof Boolean) {
+                //TODO display a check box with the parameter name to the left of it
+            } else if (entry.getValue() instanceof String) {
+                //TODO display a text field with a parameter name to the left of it
+            } else if (entry.getValue().getClass().isEnum()) {
+                JComboBox box = new JComboBox(settings.getEnumPropertyValues(entry.getKey()).toArray());
+                box.setSelectedItem(entry.getValue());
+                criticPropertyChoosers.put(entry.getKey(), box);
+            }
+        }
+        
         panel = new JPanel();
         severityCombo = new JComboBox(Severity.values());
         if (settings.getPlatformType().equals(StarterPlatformTypes.CONFIGURATION.getName())) {
@@ -93,15 +114,26 @@ public class CriticSettingsPanel implements DataEntryPanel {
         severityCombo.setSelectedItem(settings.getSeverity());
         //It would be nice if the layout used a pref:grow style for the first
         //column but it makes it difficult to set the preferred size correctly.
-        DefaultFormBuilder builder = new DefaultFormBuilder(new FormLayout("fill:pref:grow, 5dlu, pref"), panel);
-        builder.append(settings.getName());
+        DefaultFormBuilder builder = new DefaultFormBuilder(new FormLayout("pref, 10dlu, fill:pref:grow, 5dlu, pref"), panel);
+        builder.append(new JLabel(settings.getName()), 3);
         builder.append(severityCombo);
+        
+        for (Map.Entry<String, JComboBox> entry : criticPropertyChoosers.entrySet()) {
+            builder.nextLine();
+            builder.append("");
+            builder.append(settings.getPropertyDescription(entry.getKey()));
+            builder.append(entry.getValue());
+            
+        }
         
         settings.addSPListener(severitySettingListener);
     }
 
     public boolean applyChanges() {
         settings.setSeverity(((Severity) severityCombo.getSelectedItem()));
+        for (Map.Entry<String, JComboBox> enumProperty : criticPropertyChoosers.entrySet()) {
+            settings.setProperty(enumProperty.getKey(), enumProperty.getValue().getSelectedItem());
+        }
         return true;
     }
 
@@ -115,6 +147,12 @@ public class CriticSettingsPanel implements DataEntryPanel {
 
     public boolean hasUnsavedChanges() {
         if (!((Severity) severityCombo.getSelectedItem()).equals(settings.getSeverity())) return true;
+        for (Map.Entry<String, JComboBox> enumProperty : criticPropertyChoosers.entrySet()) {
+            if ((settings.getProperties().get(enumProperty.getKey()) == null && enumProperty.getValue().getSelectedItem() != null)
+                    || !settings.getProperties().get(enumProperty.getKey()).equals(enumProperty.getValue().getSelectedItem())) {
+                return true;
+            }
+        }
         return false;
     }
 
